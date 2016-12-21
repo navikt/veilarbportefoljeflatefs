f5cc8bff68482d99e72eebe73ce943e30e538b9f
Mathilde Kirkhus <mathilde.Kirkhus@nav.no>
2016-12-21 15:06:37 +0100

    url: sette korrekt miljø/domene

--- web/src/frontend/app/middleware/api.js
+++ web/src/frontend/app/middleware/api.js
@@ -4,7 +4,8 @@ const API_BASE_URL = '/veilarbportefoljeflatefs/tjenester';
 const MED_CREDENTIALS = { credentials: 'same-origin' };
 
 export function hentEnheter() {
-    return fetchToJson('http://a34duvw25985.devillo.no:9599/Portefolje-serverside/tjenester/hentenheter', MED_CREDENTIALS);
+    const url = 'http://' + window.location.hostname + ':9599/Portefolje-serverside/tjenester/hentenheter';
+    return fetchToJson(url, MED_CREDENTIALS);
 
 }
 
