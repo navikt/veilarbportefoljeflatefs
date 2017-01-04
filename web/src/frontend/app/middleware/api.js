baae8800e8a4ba596a19a40ed44ca5e180b76800
Johan Rusvik <johan.rusvik@nav.no>
2017-01-04 12:20:52 +0100

    Endrer dev-url til riktig url

--- web/src/frontend/app/middleware/api.js
+++ web/src/frontend/app/middleware/api.js
@@ -4,7 +4,7 @@ import { erDev } from './../utils/utils';
 const API_BASE_URL = '/veilarbportefoljeflatefs/tjenester';
 const MED_CREDENTIALS = { credentials: 'same-origin' };
 
-const VEILARBVEILEDERAPI_URL = erDev() ? ':9590/portefolje' : '/veilarbveilederapi';
+const VEILARBVEILEDERAPI_URL = erDev() ? ':9590/veilarbveilederapi' : '/veilarbveilederapi';
 
 export function hentEnheter(ident) {
     const url = `https://${window.location.hostname}${VEILARBVEILEDERAPI_URL}/tjenester/hentenheter/${ident}`;
