2702ce7cd489ac986b19c36afd99d805e414080d
Christian Finstad <christian.finstad@nav.no>
2017-01-09 09:07:45 +0100

    Apiet til veilarbveilederapi har endret navn til veilarbveileder

--- web/src/frontend/app/middleware/api.js
+++ web/src/frontend/app/middleware/api.js
@@ -4,10 +4,10 @@ import { erDev } from './../utils/utils';
 const API_BASE_URL = '/veilarbportefoljeflatefs/tjenester';
 const MED_CREDENTIALS = { credentials: 'same-origin' };
 
-const VEILARBVEILEDERAPI_URL = erDev() ? ':9590/veilarbveilederapi' : '/veilarbveilederapi';
+const VEILARBVEILEDER_URL = erDev() ? ':9590/veilarbveileder' : '/veilarbveileder';
 
 export function hentEnheter(ident) {
-    const url = `https://${window.location.hostname}${VEILARBVEILEDERAPI_URL}/tjenester/hentenheter/${ident}`;
+    const url = `https://${window.location.hostname}${VEILARBVEILEDER_URL}/tjenester/hentenheter/${ident}`;
     return fetchToJson(url, MED_CREDENTIALS);
 }
 
