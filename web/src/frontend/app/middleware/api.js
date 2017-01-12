4b90d4f29d448a99cc56dfe461b1869cc85db29f
Mathilde Kirkhus <mathilde.Kirkhus@nav.no>
2017-01-12 10:36:09 +0100

    Fikset tabell over brukere til enhet

--- web/src/frontend/app/middleware/api.js
+++ web/src/frontend/app/middleware/api.js
@@ -5,7 +5,7 @@ const API_BASE_URL = '/veilarbportefoljeflatefs/tjenester';
 const MED_CREDENTIALS = { credentials: 'same-origin' };
 
 const VEILARBVEILEDER_URL = erDev() ? ':9590/veilarbveileder' : '/veilarbveileder';
-const VEILARBPORTEFOLJE_URL = erDev() ? ':9592/portefolje' : '/veilarbportefolje';
+const VEILARBPORTEFOLJE_URL = erDev() ? ':9592/veilarbportefolje' : '/veilarbportefolje';
 
 export function hentEnheter(ident) {
     const url = `https://${window.location.hostname}${VEILARBVEILEDER_URL}/tjenester/hentenheter/${ident}`;
@@ -16,7 +16,7 @@ export function hentLedetekster() {
     return fetchToJson(`${API_BASE_URL}/tekster`, MED_CREDENTIALS);
 }
 
-export function hentPortefolje(enhet) {
-    const url = `https://${window.location.hostname}${VEILARBPORTEFOLJE_URL}/tjenester/hentportefolje/${enhet}`;
+export function hentPortefolje(enhet, ident) {
+    const url = `https://app-t4.adeo.no/veilarbportefolje/tjenester/hentportefolje/${enhet.enhetId}?ident=${ident}`;
     return fetchToJson(url, MED_CREDENTIALS);
 }
