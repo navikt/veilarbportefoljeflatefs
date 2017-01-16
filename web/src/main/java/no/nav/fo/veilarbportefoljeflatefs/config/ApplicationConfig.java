fee2a20ba5bc2ed8ebcf228fa86d9d3eb82f26ab
Mathilde Kirkhus <mathilde.Kirkhus@nav.no>
2017-01-16 15:52:39 +0100

    La til sjekk mot endpoints i selftest

--- web/src/main/java/no/nav/fo/veilarbportefoljeflatefs/config/ApplicationConfig.java
+++ web/src/main/java/no/nav/fo/veilarbportefoljeflatefs/config/ApplicationConfig.java
@@ -1,5 +1,7 @@
 package no.nav.fo.veilarbportefoljeflatefs.config;
 
+import no.nav.fo.veilarbportefoljeflatefs.config.endpoint.veilarbportefolje.PortefoljeEndpointConfig;
+import no.nav.fo.veilarbportefoljeflatefs.config.endpoint.veilarbveileder.VeilederEndpointConfig;
 import no.nav.fo.veilarbportefoljeflatefs.internal.HealthCheckService;
 import no.nav.fo.veilarbportefoljeflatefs.internal.IsAliveServlet;
 import org.springframework.context.annotation.Bean;
@@ -9,8 +11,10 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @Import({
+        PortefoljeEndpointConfig.class,
+        VeilederEndpointConfig.class,
         TeksterServiceConfig.class,
-        Pingables.class,
+        Pingables.class
 })
 public class ApplicationConfig {
 
