b1f74db455602a57daf1cb9dd2ef035ff810b02b
Mathilde Kirkhus <mathilde.Kirkhus@nav.no>
2017-01-20 10:47:33 +0100

    Går mot veilarbportefolje lokalt + fikset portnr

--- web/src/frontend/app/middleware/api.js
+++ web/src/frontend/app/middleware/api.js
@@ -5,7 +5,7 @@ const API_BASE_URL = '/veilarbportefoljeflatefs/tjenester';
 const MED_CREDENTIALS = { credentials: 'same-origin' };
 
 const VEILARBVEILEDER_URL = erDev() ? ':9590/veilarbveileder' : '/veilarbveileder';
-const VEILARBPORTEFOLJE_URL = erDev() ? ':9592/veilarbportefolje' : '/veilarbportefolje';
+const VEILARBPORTEFOLJE_URL = erDev() ? ':9594/veilarbportefolje' : '/veilarbportefolje';
 
 export function hentEnheter(ident) {
     const url = `https://${window.location.hostname}${VEILARBVEILEDER_URL}/tjenester/hentenheter/${ident}`;
@@ -17,6 +17,6 @@ export function hentLedetekster() {
 }
 
 export function hentPortefolje(enhet, ident, fra, antall) {
-    const url = `https://app-t4.adeo.no/veilarbportefolje/tjenester/hentportefoljeforenhet/${enhet}?ident=${ident}&fra=${fra}&antall=${antall}`;
+    const url = `https://${window.location.hostname}${VEILARBPORTEFOLJE_URL}/tjenester/hentportefoljeforenhet/${enhet}?ident=${ident}&fra=${fra}&antall=${antall}`;
     return fetchToJson(url, MED_CREDENTIALS);
 }
