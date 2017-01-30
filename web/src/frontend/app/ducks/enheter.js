9fe736c1e5d7c60f5c79863099ddb7bed3d7777b
Johan Rusvik <johan.rusvik@nav.no>
2017-01-30 16:26:21 +0100

    Fjerner redundant exports

--- web/src/frontend/app/ducks/enheter.js
+++ web/src/frontend/app/ducks/enheter.js
@@ -4,11 +4,11 @@ import { STATUS, doThenDispatch } from './utils';
 import { leggEnhetIUrl } from '../utils/utils';
 
 // Actions
-export const OK = 'veilarbportefolje/enheter/OK';
-export const FEILET = 'veilarbportefolje/enheter/FEILET';
-export const PENDING = 'veilarbportefolje/enheter/PENDING';
+const OK = 'veilarbportefolje/enheter/OK';
+const FEILET = 'veilarbportefolje/enheter/FEILET';
+const PENDING = 'veilarbportefolje/enheter/PENDING';
 
-export const VELG_ENHET = 'VELG_ENHET';
+const VELG_ENHET = 'VELG_ENHET';
 
 const initialState = {
     data: [],
