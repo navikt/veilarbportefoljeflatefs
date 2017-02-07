78e2e45413fe0d74542871d4306c8649cd26715d
Mathilde Kirkhus <mathilde.Kirkhus@nav.no>
2017-02-07 11:01:01 +0100

    PK-30197, PKULV-2203: Lagt til portefoljeside for annen veileder

--- web/src/frontend/app/veiledere/veiledere-tabell.js
+++ web/src/frontend/app/veiledere/veiledere-tabell.js
@@ -1,38 +1,64 @@
-import React, { PropTypes as PT } from 'react';
+/* eslint-disable jsx-a11y/onclick-has-focus*/
+/* eslint-disable jsx-a11y/onclick-has-role*/
+/* eslint-disable jsx-a11y/no-static-element-interactions*/
+import React, { Component, PropTypes as PT } from 'react';
+import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
+import history from '../history';
 import { veilederShape } from './../proptype-shapes';
+import { settValgtVeilederIdent } from '../ducks/portefolje';
 
-function VeilederTabell({ veiledere }) {
-    return (
-        <table className="tabell tabell-skillestrek">
-            <thead>
-                <tr>
-                    <th scope="col">
-                        <FormattedMessage id="enhet.veiledere.tabell.veiledere" />
-                    </th>
-                    <th scope="col">
-                        <FormattedMessage id="enhet.veiledere.tabell.ident" />
-                    </th>
-                    <th scope="col">
-                        <FormattedMessage id="enhet.veiledere.tabell.brukere" />
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {veiledere.map(veileder =>
-                    <tr key={veileder.ident}>
-                        <td>{`${veileder.navn}`}</td>
-                        <td>{`${veileder.ident}`}</td>
-                        <td>{`${veileder.brukere}`}</td>
+class VeilederTabell extends Component {
+
+    settValgtVeilederIdent(veileder) {
+        const { settVeilederident } = this.props;
+        settVeilederident(veileder.ident);
+        history.push('/portefolje');
+    }
+
+    render() {
+        const { veiledere } = this.props;
+
+        return (
+            <table className="tabell tabell-skillestrek">
+                <thead>
+                    <tr>
+                        <th scope="col">
+                            <FormattedMessage id="enhet.veiledere.tabell.veiledere" />
+                        </th>
+                        <th scope="col">
+                            <FormattedMessage id="enhet.veiledere.tabell.ident" />
+                        </th>
+                        <th scope="col">
+                            <FormattedMessage id="enhet.veiledere.tabell.brukere" />
+                        </th>
                     </tr>
+                </thead>
+                <tbody>
+                    {veiledere.map(veileder =>
+                        <tr key={veileder.ident}>
+                            <td><a onClick={() => this.settValgtVeilederIdent(veileder)}>{`${veileder.navn}`}</a></td>
+                            <td>{`${veileder.ident}`}</td>
+                            <td>{`${veileder.brukere}`}</td>
+                        </tr>
                 )}
-            </tbody>
-        </table>
-    );
+                </tbody>
+            </table>
+        );
+    }
 }
 
 VeilederTabell.propTypes = {
-    veiledere: PT.arrayOf(veilederShape)
+    veiledere: PT.arrayOf(veilederShape),
+    settVeilederident: PT.func.isRequired
 };
 
-export default VeilederTabell;
+const mapStateToProps = state => ({
+    veilederident: state.portefolje.veilederident
+});
+
+const mapDispatchToProps = dispatch => ({
+    settVeilederident: veilederident => dispatch(settValgtVeilederIdent(veilederident))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(VeilederTabell);
