2d621d0a85511eee0e405397d7208d5bdab16aa5
Christian Finstad <christian.finstad@nav.no>
2017-02-08 13:55:42 +0100

    Merge from master

--- web/src/frontend/app/portefolje/portefolje-visning.js
+++ web/src/frontend/app/portefolje/portefolje-visning.js
@@ -12,13 +12,13 @@ class PortefoljeVisning extends Component {
     componentWillMount() {
         const { valgtEnhet, hentPortefolje } = this.props;
         if (valgtEnhet) {
-            hentPortefolje(valgtEnhet.enhetId, this.props.ident);
+            hentPortefolje(valgtEnhet.enhetId);
         }
         this.settSorteringOgHentPortefolje = this.settSorteringOgHentPortefolje.bind(this);
     }
 
     settSorteringOgHentPortefolje() {
-        const { sorteringsrekkefolge, settSortering, fraIndex, valgtEnhet, ident, hentPortefolje } = this.props;
+        const { sorteringsrekkefolge, settSortering, fraIndex, valgtEnhet, hentPortefolje } = this.props;
         let valgtRekkefolge = '';
         if (sorteringsrekkefolge === 'ascending') {
             valgtRekkefolge = 'descending';
@@ -27,13 +27,13 @@ class PortefoljeVisning extends Component {
             valgtRekkefolge = 'ascending';
             settSortering('ascending');
         }
-        hentPortefolje(valgtEnhet.enhetId, ident, valgtRekkefolge, fraIndex);
+        hentPortefolje(valgtEnhet.enhetId, valgtRekkefolge, fraIndex);
     }
 
 
     render() {
-        const { portefolje, valgtEnhet, ident, hentPortefolje, sorteringsrekkefolge } = this.props;
-        const { antallTotalt, antallReturnert, fraIndex } = portefolje.data;
+        const { portefolje, valgtEnhet, hentPortefolje, sorteringsrekkefolge } = this.props;
+        const { antallTotalt, antallReturnert, fraIndex, brukere } = portefolje.data;
 
         return (
             <Innholdslaster avhengigheter={[portefolje]}>
@@ -103,23 +103,21 @@ class PortefoljeVisning extends Component {
 PortefoljeVisning.propTypes = {
     valgtEnhet: PT.object.isRequired,
     portefolje: PT.object.isRequired,
-    ident: PT.string.isRequired,
     hentPortefolje: PT.func.isRequired,
     settSortering: PT.func.isRequired,
     sorteringsrekkefolge: PT.string.isRequired,
-    fraIndex: PT.number.isRequired
+    fraIndex: PT.number
 };
 
 const mapStateToProps = state => ({
     portefolje: state.portefolje,
     valgtEnhet: state.enheter.valgtEnhet,
-    ident: state.enheter.ident,
     sorteringsrekkefolge: state.portefolje.sorteringsrekkefolge
 });
 
 const mapDispatchToProps = dispatch => ({
-    hentPortefolje: (enhet, ident, rekkefolge, fra = 0, antall = 20) =>
-        dispatch(hentPortefoljeForEnhet(enhet, ident, rekkefolge, fra, antall)),
+    hentPortefolje: (enhet, rekkefolge, fra = 0, antall = 20) =>
+        dispatch(hentPortefoljeForEnhet(enhet, rekkefolge, fra, antall)),
     settSortering: rekkefolge => dispatch(settSorterRekkefolge(rekkefolge))
 });
 
