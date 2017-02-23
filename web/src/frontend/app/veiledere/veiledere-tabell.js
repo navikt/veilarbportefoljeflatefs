bc10b726039a31d131d98f7a7d42fc766d34cb61
Mathilde Kirkhus <mathilde.Kirkhus@nav.no>
2017-02-23 13:09:51 +0100

    Refaktorering + eslint

--- web/src/frontend/app/veiledere/veiledere-tabell.js
+++ web/src/frontend/app/veiledere/veiledere-tabell.js
@@ -1,4 +1,8 @@
+/* eslint-disable jsx-a11y/onclick-has-focus*/
+/* eslint-disable jsx-a11y/onclick-has-role*/
+/* eslint-disable jsx-a11y/no-static-element-interactions*/
 import React, { Component, PropTypes as PT } from 'react';
+import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import history from '../history';
 import { veilederShape } from './../proptype-shapes';
@@ -7,39 +11,39 @@ import { settValgtVeileder } from '../ducks/portefolje';
 class VeilederTabell extends Component {
 
     settValgtVeileder(veileder) {
-        const {settVeileder} = this.props;
+        const { settVeileder } = this.props;
         settVeileder(veileder);
         history.push('/portefolje');
     }
 
     render() {
-        const {veiledere, portefoljestorrelser} = this.props;
+        const { veiledere, portefoljestorrelser } = this.props;
 
         return (
             <table className="tabell tabell-skillestrek">
                 <thead>
-                <tr>
-                    <th scope="col">
-                        <FormattedMessage id="enhet.veiledere.tabell.veiledere"/>
-                    </th>
-                    <th scope="col">
-                        <FormattedMessage id="enhet.veiledere.tabell.ident"/>
-                    </th>
-                    <th scope="col">
-                        <FormattedMessage id="enhet.veiledere.tabell.brukere"/>
-                    </th>
-                </tr>
+                    <tr>
+                        <th scope="col">
+                            <FormattedMessage id="enhet.veiledere.tabell.veiledere" />
+                        </th>
+                        <th scope="col">
+                            <FormattedMessage id="enhet.veiledere.tabell.ident" />
+                        </th>
+                        <th scope="col">
+                            <FormattedMessage id="enhet.veiledere.tabell.brukere" />
+                        </th>
+                    </tr>
                 </thead>
                 <tbody>
-                {veiledere.map(veileder =>
-                    <tr key={veileder.ident}>
-                        <td><a onClick={() => this.settValgtVeileder(veileder)}>{`${veileder.navn}`}</a></td>
-                        <td>{`${veileder.ident}`}</td>
+                    {veiledere.map(veileder =>
+                        <tr key={veileder.ident}>
+                            <td><a onClick={() => this.settValgtVeileder(veileder)}>{`${veileder.navn}`}</a></td>
+                            <td>{`${veileder.ident}`}</td>
 
-                        {/* Denne må endres til å se på veileder_id (storrelse.value === veileder.veileder_id) når
-                         når det er tilgjengelig fra portefølje*/}
-                        <td>{portefoljestorrelser.find(storrelse => storrelse.value === 'SKAFFEA').count}</td>
-                    </tr>
+                            {/* Denne må endres til å se på veileder_id (storrelse.value === veileder.veileder_id) når
+                                når det er tilgjengelig fra portefølje*/}
+                            <td>{portefoljestorrelser.find(storrelse => storrelse.value === 'SKAFFEA').count}</td>
+                        </tr>
                 )}
                 </tbody>
             </table>
@@ -50,11 +54,15 @@ class VeilederTabell extends Component {
 VeilederTabell.propTypes = {
     veiledere: PT.arrayOf(veilederShape),
     settVeileder: PT.func.isRequired,
-    portefoljestorrelser: PT.arrayOf(PT.object)
+    portefoljestorrelser: PT.arrayOf(PT.object).isRequired
 };
+
+const mapStateToProps = state => ({
+    portefoljestorrelser: state.portefoljestorrelser.data.facetResults
+});
 
 const mapDispatchToProps = dispatch => ({
     settVeileder: veileder => dispatch(settValgtVeileder(veileder))
 });
 
-export default (mapDispatchToProps)(VeilederTabell);
+export default connect(mapStateToProps, mapDispatchToProps)(VeilederTabell);
