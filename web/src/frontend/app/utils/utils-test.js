8781f9ba8b79975c8d69848cda7c10fb65f602a2
Oscar Eckbo <oscar.eckbo@nav.no>
2017-03-08 12:11:47 +0100

    Utils-tester

--- web/src/frontend/app/utils/utils-test.js
+++ web/src/frontend/app/utils/utils-test.js
@@ -3,7 +3,19 @@ import { expect } from 'chai';
 import { filterUrlBuilder } from '../utils/utils';
 
 describe('Utils', () => {
-    it('skal legge til nyeBrukere i url hvis finnes', () => {
+    it('skal legge til nyeBrukere i url hvis den er aktiv', () => {
         expect(filterUrlBuilder(true, false)).to.equal('&nyeBrukere=true');
+    });
+
+    it('skal legge til inaktiveBrukere i url hvis den er aktiv', () => {
+        expect(filterUrlBuilder(false, true)).to.equal('&inaktiveBrukere=true');
+    });
+
+    it('skal legge til nyeBrukere og inaktiveBrukere i url hvis de er aktive', () => {
+        expect(filterUrlBuilder(true, true)).to.equal('&nyeBrukere=true&inaktiveBrukere=true');
+    });
+
+    it('skal ikke legge til filtre i url hvis det ikke finnes filtre', () => {
+        expect(filterUrlBuilder(false, false)).to.equal('');
     });
 });
