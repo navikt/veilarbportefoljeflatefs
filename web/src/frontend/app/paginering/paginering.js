282909e2c5481836a5948c900031ade186098287
Mathilde Kirkhus <mathilde.Kirkhus@nav.no>
2017-03-10 15:24:06 +0100

    Refaktorering

--- web/src/frontend/app/paginering/paginering.js
+++ web/src/frontend/app/paginering/paginering.js
@@ -1,14 +1,18 @@
 import React, { PropTypes as PT } from 'react';
 
 function Paginering({ fraIndex, antallTotalt, hentListe, tekst, sideStorrelse }) {
-    function createLink(fraIndeks, tilIndeks, linkTekst) {
+    function createSimpleLink(fraIndeks, tilIndeks, linkTekst) {
         return (<a href="" style={{marginRight: '2px'}} onClick={(e) => {
             e.preventDefault();
             hentListe(fraIndeks, tilIndeks);
         }}>{linkTekst}</a>)
     }
 
-    const sisteFraIndex = fraIndex + sideStorrelse >= antallTotalt ? fraIndex : fraIndex + sideStorrelse;
+    const fraIndeksForrigeSide = fraIndex - sideStorrelse < 0 ? fraIndex : fraIndex - sideStorrelse;
+    const fraIndeksNesteSide = antallTotalt % sideStorrelse === 0 ? antallTotalt - sideStorrelse
+        : antallTotalt - (antallTotalt % sideStorrelse);
+    const fraIndeksSisteSide = fraIndex + sideStorrelse >= antallTotalt ? fraIndex : fraIndex + sideStorrelse;
+
 
     return (
         <div className="paginering">
@@ -18,25 +22,17 @@ function Paginering({ fraIndex, antallTotalt, hentListe, tekst, sideStorrelse })
             <div className="bytt-side">
                 {antallTotalt <= sideStorrelse ? null :
                     <div>
-                        {createLink(0, antallTotalt, 'Se alle')}
-                            <a href="" style={{margin: '0px 10px'}} className={fraIndex == 0 ? "not-active":""} onClick={(e) => {
+                        {createSimpleLink(0, antallTotalt, 'Se alle')}
+                        <a href="" style={{margin: '0px 10px'}} className={fraIndex == 0 ? "not-active":""} onClick={(e) => {
                             e.preventDefault();
-                            const fra = fraIndex - sideStorrelse < 0 ? fraIndex : fraIndex - sideStorrelse;
-                            hentListe(fra, sideStorrelse);
+                            hentListe(fraIndeksForrigeSide, sideStorrelse);
                         }}
                         >{'<'}</a>
-                        {createLink(0, sideStorrelse, '1')}
+                        {createSimpleLink(0, sideStorrelse, '1')}
                         {fraIndex == 0 ? <span style={{marginRight: '2px'}}>...</span>:
                         <span style={{marginRight: '2px'}}>...<b>{((fraIndex / sideStorrelse) + 1)}</b>...</span>}
-                        <a
-                            href="" style={{ marginRight: '10px' }} onClick={(e) => {
-                            e.preventDefault();
-                            const fra = antallTotalt % sideStorrelse === 0 ? antallTotalt - sideStorrelse
-                                : antallTotalt - (antallTotalt % sideStorrelse);
-                            hentListe(fra, sideStorrelse);
-                        }}
-                        >{ Math.ceil(antallTotalt/sideStorrelse)}</a>
-                        {createLink(sisteFraIndex, sideStorrelse, '>')}
+                        {createSimpleLink(fraIndeksNesteSide, sideStorrelse, Math.ceil(antallTotalt/sideStorrelse))}
+                        {createSimpleLink(fraIndeksSisteSide, sideStorrelse, '>')}
                     </div>
                 }
             </div>
