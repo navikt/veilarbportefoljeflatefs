a910ff210b44d3e5406a0413ae6d5a1e27654fbe
Nicklas Utgaard <nicklas.utgaard@nav.no>
2017-03-27 17:53:15 +0200

    Opprydding enhetsside

--- web/src/frontend/app/enhet/filtrering/filtrering-container.js
+++ web/src/frontend/app/enhet/filtrering/filtrering-container.js
@@ -1,10 +1,12 @@
 import React, { PropTypes as PT, Component } from 'react';
 import { connect } from 'react-redux';
 import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
-import { hentPortefoljeForEnhet } from '../../ducks/portefolje';
+import { veilederShape, brukerShape } from './../../proptype-shapes';
+import { hentPortefoljeForEnhet, tildelVeileder } from './../../ducks/portefolje';
 import FiltreringStatus from './filtrering-status';
 import FiltreringFilter from './filtrering-filter';
 import { eksporterEnhetsportefoljeTilLocalStorage } from '../../ducks/utils';
+import TildelVeilederVelger from './../tildel-veileder-velger';
 
 class FiltreringContainer extends Component {
     constructor(props) {
@@ -24,13 +26,25 @@ class FiltreringContainer extends Component {
     }
 
     render() {
+        const { veiledere, valgtVeileder, velgVeileder, brukere } = this.props;
         return (
             <div>
-                <Ekspanderbartpanel className="custom-ekspanderbartpanel" tittel="Status" tittelProps={{ type: 'systemtittel', tag: 'span' }}>
-                    <FiltreringStatus oppdaterDatagrunnlag={this.oppdaterDatagrunnlag} />
+                <Ekspanderbartpanel className="custom-ekspanderbartpanel" tittel="Status"
+                                    tittelProps={{ type: 'systemtittel', tag: 'span' }}>
+                    <FiltreringStatus oppdaterDatagrunnlag={this.oppdaterDatagrunnlag}/>
                 </Ekspanderbartpanel>
-                <Ekspanderbartpanel className="custom-ekspanderbartpanel" tittel="Filter" tittelProps={{ type: 'systemtittel', tag: 'span' }}>
-                    <FiltreringFilter oppdaterDatagrunnlag={this.oppdaterDatagrunnlag} />
+                <Ekspanderbartpanel className="custom-ekspanderbartpanel" tittel="Filter"
+                                    tittelProps={{ type: 'systemtittel', tag: 'span' }}>
+                    <FiltreringFilter oppdaterDatagrunnlag={this.oppdaterDatagrunnlag}/>
+                </Ekspanderbartpanel>
+                <Ekspanderbartpanel className="custom-ekspanderbartpanel" tittel="Tildel veileder"
+                                    tittelProps={{ type: 'systemtittel', tag: 'span' }}>
+                    <TildelVeilederVelger
+                        valgtVeileder={valgtVeileder}
+                        veiledere={veiledere}
+                        brukere={brukere}
+                        velgVeileder={(tildelinger, tilVeileder) => velgVeileder(tildelinger, tilVeileder)}
+                    />
                 </Ekspanderbartpanel>
             </div>
         );
@@ -43,7 +57,12 @@ FiltreringContainer.propTypes = {
     fraIndex: PT.number,
     antall: PT.number,
     valgtEnhet: PT.string,
-    hentPortefolje: PT.func.isRequired
+    hentPortefolje: PT.func.isRequired,
+    veiledere: PT.arrayOf(veilederShape).isRequired,
+    brukere: PT.arrayOf(brukerShape).isRequired,
+    routes: PT.arrayOf(PT.object),
+    valgtVeileder: PT.object,
+    velgVeileder: PT.func.isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -51,10 +70,14 @@ const mapStateToProps = state => ({
     sorteringsrekkefolge: state.portefolje.sorteringsrekkefolge,
     fraIndex: state.portefolje.data.fraIndex,
     antall: state.paginering.sideStorrelse,
-    valgtEnhet: state.enheter.valgtEnhet.enhet.enhetId
+    valgtEnhet: state.enheter.valgtEnhet.enhet.enhetId,
+    veiledere: state.veiledere.data.veilederListe,
+    brukere: state.portefolje.data.brukere,
+    valgtVeileder: state.enheter.valgtVeileder,
 });
 
 const mapDispatchToProps = dispatch => ({
+    velgVeileder: (tildelinger, tilVeileder) => dispatch(tildelVeileder(tildelinger, tilVeileder)),
     hentPortefolje: (enhet, rekkefolge, fra, antall, filtervalg) =>
         dispatch(hentPortefoljeForEnhet(enhet, rekkefolge, fra, antall, filtervalg))
 });
