e08ed2735754a16a3fd78e8c859fffc2c234607d
Oscar Eckbo <oscar.eckbo@nav.no>
2017-03-27 11:02:33 +0200

    Legger til statustall

--- web/src/frontend/app/enhet/filtrering/filtrering-status.js
+++ web/src/frontend/app/enhet/filtrering/filtrering-status.js
@@ -2,11 +2,18 @@ import React, { PropTypes as PT, Component } from 'react';
 import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import { endreFiltervalg } from '../../ducks/filtrering';
+import { hentStatusTall } from '../../ducks/statustall';
+import { statustallShape } from '../../proptype-shapes';
+import Innholdslaster from '../../innholdslaster/innholdslaster';
 
 class FiltreringStatus extends Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentWillMount() {
+        this.props.fetchStatusTall(this.props.enhet);
     }
 
     componentDidUpdate() {
@@ -30,7 +37,10 @@ class FiltreringStatus extends Component {
                 checked={nyeBrukere}
             />
             <label htmlFor="checkbox-filtrering-oversikt-nye-brukere">
-                <FormattedMessage id="enhet.filtrering.filtrering.oversikt.nye.brukere.checkbox" />
+                <FormattedMessage
+                    id="enhet.filtrering.filtrering.oversikt.nye.brukere.checkbox"
+                    values={{ antall: this.props.statustall.data.nyeBrukere }}
+                />
             </label>
         </div>);
 
@@ -43,14 +53,28 @@ class FiltreringStatus extends Component {
                 checked={inaktiveBrukere}
             />
             <label htmlFor="checkbox-filtrering-oversikt-inaktive-brukere">
-                <FormattedMessage id="enhet.filtrering.filtrering.oversikt.inaktive.brukere.checkbox" />
+                <FormattedMessage
+                    id="enhet.filtrering.filtrering.oversikt.inaktive.brukere.checkbox"
+                    values={{ antall: this.props.statustall.data.inaktiveBrukere }}
+                />
             </label>
         </div>);
 
+        const totaltAntallBrukere = (
+            <div className="typo-element blokk-xs">
+                <FormattedMessage
+                    id="filtrering.status.totalt-antall-brukere"
+                    values={{ antall: this.props.statustall.data.totalt }}
+                />
+            </div>
+        );
         return (
             <div className="filtrering-oversikt panel">
-                {checkboxNyeBrukere}
-                {checkboxInaktiveBrukere}
+                <Innholdslaster avhengigheter={[this.props.statustall]}>
+                    {totaltAntallBrukere}
+                    {checkboxNyeBrukere}
+                    {checkboxInaktiveBrukere}
+                </Innholdslaster>
             </div>
         );
     }
@@ -60,16 +84,22 @@ FiltreringStatus.propTypes = {
     endreFilter: PT.func.isRequired,
     oppdaterDatagrunnlag: PT.func.isRequired,
     nyeBrukere: PT.bool.isRequired,
-    inaktiveBrukere: PT.bool.isRequired
+    inaktiveBrukere: PT.bool.isRequired,
+    fetchStatusTall: PT.func.isRequired,
+    enhet: PT.string.isRequired,
+    statustall: PT.shape({ data: statustallShape.isRequired }).isRequired
 };
 
 const mapStateToProps = state => ({
     nyeBrukere: state.filtrering.filtervalg.nyeBrukere,
-    inaktiveBrukere: state.filtrering.filtervalg.inaktiveBrukere
+    inaktiveBrukere: state.filtrering.filtervalg.inaktiveBrukere,
+    enhet: state.enheter.valgtEnhet.enhet.enhetId,
+    statustall: state.statustall
 });
 
 const mapDispatchToProps = dispatch => ({
-    endreFilter: (filterId, filtervalg) => dispatch(endreFiltervalg(filterId, filtervalg))
+    endreFilter: (filterId, filtervalg) => dispatch(endreFiltervalg(filterId, filtervalg)),
+    fetchStatusTall: enhet => dispatch(hentStatusTall(enhet))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(FiltreringStatus);
