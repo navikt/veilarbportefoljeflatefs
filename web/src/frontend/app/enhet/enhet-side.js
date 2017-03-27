14a468a4d170f329d4b3a77c216496b68422e778
Nicklas Utgaard <nicklas.utgaard@nav.no>
2017-03-27 17:53:15 +0200

    Opprydding enhetsside

--- web/src/frontend/app/enhet/enhet-side.js
+++ web/src/frontend/app/enhet/enhet-side.js
@@ -1,61 +1,39 @@
 import React, { PropTypes as PT, Component } from 'react';
 import { connect } from 'react-redux';
-import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
 import { FormattedMessage } from 'react-intl';
 import Lenker from './../lenker/lenker';
 import { hentVeiledereForEnhet } from './../ducks/veiledere';
-import TildelVeilederVelger from './tildel-veileder-velger';
-import { veilederShape, brukerShape } from './../proptype-shapes';
 import EnhetsportefoljeVisning from '../enhetsportefolje/enhetsportefolje-visning';
 import FiltreringContainer from './filtrering/filtrering-container';
-import { tildelVeileder } from '../ducks/portefolje';
 import { eksporterEnhetsportefoljeTilLocalStorage } from '../ducks/utils';
 
 class EnhetSide extends Component {
     componentWillMount() {
         const { valgtEnhet, hentVeiledere } = this.props;
+
+        // TODO hent enhet, og veiledere for enhet kan skje i Application
         hentVeiledere(valgtEnhet.enhet.enhetId);
     }
+
     componentDidUpdate() {
+        // TODO dette burde skje som en del av redux sin oppdatering av filter
         const { filtervalg, valgtEnhet } = this.props;
         eksporterEnhetsportefoljeTilLocalStorage(filtervalg, valgtEnhet.enhet, location.pathname);
     }
 
     render() {
-        const {
-            valgtEnhet,
-            veiledere,
-            valgtVeileder,
-            velgVeileder,
-            brukere
-        } = this.props;
-
-
-        if (!valgtEnhet) {
-            return <noscript />;
+        // TODO man må alltid ha en valgtEnhet, denne sjekken kan derfor flyttes ut til Application
+        if (!this.props.valgtEnhet) {
+            return null;
         }
 
         return (
             <div className="enhet-side">
                 <Lenker />
                 <p className="typo-infotekst enhetsingress blokk-m">
-                    <FormattedMessage id="enhet.ingresstekst" />
+                    <FormattedMessage id="enhet.ingresstekst"/>
                 </p>
                 <FiltreringContainer />
-                <Ekspanderbartpanel
-                    className="custom-ekspanderbartpanel"
-                    tittel="Tildel veileder" tittelProps={{
-                        tag: 'span',
-                        type: 'systemtittel'
-                    }}
-                >
-                    <TildelVeilederVelger
-                        valgtVeileder={valgtVeileder}
-                        veiledere={veiledere}
-                        brukere={brukere}
-                        velgVeileder={(tildelinger, tilVeileder) => velgVeileder(tildelinger, tilVeileder)}
-                    />
-                </Ekspanderbartpanel>
                 <EnhetsportefoljeVisning />
             </div>
         );
@@ -63,25 +41,17 @@ class EnhetSide extends Component {
 }
 
 EnhetSide.propTypes = {
-    veiledere: PT.arrayOf(veilederShape).isRequired,
-    brukere: PT.arrayOf(brukerShape).isRequired,
     valgtEnhet: PT.object,
     filtervalg: PT.object,
-    valgtVeileder: PT.object,
-    velgVeileder: PT.func.isRequired,
     hentVeiledere: PT.func.isRequired
 };
 
 const mapStateToProps = state => ({
-    veiledere: state.veiledere.data.veilederListe,
-    brukere: state.portefolje.data.brukere,
-    valgtVeileder: state.enheter.valgtVeileder,
     valgtEnhet: state.enheter.valgtEnhet,
     filtervalg: state.filtrering
 });
 
 const mapDispatchToProps = dispatch => ({
-    velgVeileder: (tildelinger, tilVeileder) => dispatch(tildelVeileder(tildelinger, tilVeileder)),
     hentVeiledere: enhetId => dispatch(hentVeiledereForEnhet(enhetId))
 });
 
