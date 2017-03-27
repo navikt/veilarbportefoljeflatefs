604da3de1426860c96a00757479941ca3b26d20f
Nicklas Utgaard <nicklas.utgaard@nav.no>
2017-03-27 17:41:42 +0200

    Opprydding lenker

--- web/src/frontend/app/lenker/lenker.js
+++ web/src/frontend/app/lenker/lenker.js
@@ -2,25 +2,17 @@ import React, { PropTypes as PT } from 'react';
 import { Link } from 'react-router';
 import { FormattedMessage } from 'react-intl';
 
-function Lenker({ routes }) {
-    const valgtSide = routes[1] ? routes[1].path : '';
-    function erValgt(lenke) {
-        return lenke === valgtSide ? 'valgt' : 'ikke-valgt';
-    }
+function Lenker() {
     return (
         <div className="lenker blokk-m">
-            <Link to="enhet" className={`typo-undertittel ${erValgt('enhet')}`}>
+            <Link to="enhet" className="oversiktslenke typo-undertittel" activeClassName="oversiktslenke--valgt">
                 <FormattedMessage id="lenker.enhet.oversikt" />
             </Link>
-            <Link to="veiledere" className={`typo-undertittel ${erValgt('veiledere')}`}>
+            <Link to="veiledere" className="oversiktslenke typo-undertittel" activeClassName="oversiktslenke--valgt">
                 <FormattedMessage id="lenker.veiledere.oversikt" />
             </Link>
         </div>
     );
 }
-
-Lenker.propTypes = {
-    routes: PT.arrayOf(PT.object)
-};
 
 export default Lenker;
