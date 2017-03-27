218e3c8568d776b569527b6e6effc8fd7c8b7a72
Mathilde Kirkhus <mathilde.Kirkhus@nav.no>
2017-03-27 13:23:20 +0200

    Lagt til aria-selected på lenker

--- web/src/frontend/app/lenker/lenker-minoversikt.js
+++ web/src/frontend/app/lenker/lenker-minoversikt.js
@@ -1,15 +1,20 @@
 import React, { PropTypes as PT } from 'react';
 import { Link } from 'react-router';
 import { FormattedMessage } from 'react-intl';
+import classNames from 'classnames';
 
 function LenkerMinoversikt({ routes }) {
     const valgtSide = routes[1] ? routes[1].path : '';
     function erValgt(lenke) {
-        return lenke === valgtSide ? 'valgt' : 'ikke-valgt';
+        return lenke === valgtSide;
     }
     return (
         <div className="lenker blokk-m">
-            <Link to="portefolje" className={`typo-undertittel ${erValgt('portefolje')}`}>
+            <Link
+                to="portefolje"
+                className={classNames({ 'typo-undertittel': true, valgt: erValgt('portefolje') })}
+                aria-selected={erValgt('portefolje')}
+            >
                 <FormattedMessage id="lenker.min.oversikt" />
             </Link>
         </div>
--- web/src/frontend/app/lenker/lenker.js
+++ web/src/frontend/app/lenker/lenker.js
@@ -1,18 +1,27 @@
 import React, { PropTypes as PT } from 'react';
 import { Link } from 'react-router';
 import { FormattedMessage } from 'react-intl';
+import classNames from 'classnames';
 
 function Lenker({ routes }) {
     const valgtSide = routes[1] ? routes[1].path : '';
     function erValgt(lenke) {
-        return lenke === valgtSide ? 'valgt' : 'ikke-valgt';
+        return lenke === valgtSide;
     }
     return (
         <div className="lenker blokk-m">
-            <Link to="enhet" className={`typo-undertittel ${erValgt('enhet')}`}>
+            <Link
+                to="enhet"
+                className={classNames({ 'typo-undertittel': true, valgt: erValgt('enhet') })}
+                aria-selected={erValgt('enhet')}
+            >
                 <FormattedMessage id="lenker.enhet.oversikt" />
             </Link>
-            <Link to="veiledere" className={`typo-undertittel ${erValgt('veiledere')}`}>
+            <Link
+                to="veiledere"
+                className={classNames({ 'typo-undertittel': true, valgt: erValgt('veiledere') })}
+                aria-selected={erValgt('veiledere')}
+            >
                 <FormattedMessage id="lenker.veiledere.oversikt" />
             </Link>
         </div>
