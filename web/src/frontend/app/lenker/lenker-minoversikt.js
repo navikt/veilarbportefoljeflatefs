f7b98abb4616ed3398d3821c872b8848dab8afaf
Mathilde Kirkhus <mathilde.Kirkhus@nav.no>
2017-03-29 14:30:49 +0200

    Fiks etter merge-conflicts

--- web/src/frontend/app/lenker/lenker-minoversikt.js
+++ web/src/frontend/app/lenker/lenker-minoversikt.js
@@ -1,19 +1,23 @@
 import React, { PropTypes as PT } from 'react';
 import { Link } from 'react-router';
 import { FormattedMessage } from 'react-intl';
-import classNames from 'classnames';
+import { veilederShape } from '../proptype-shapes';
 
-function LenkerMinoversikt({ routes }) {
+function LenkerMinoversikt({ routes, veileder }) {
     const valgtSide = routes[1] ? routes[1].path : '';
     function erValgt(lenke) {
+        console.log(valgtSide);
         return lenke === valgtSide;
     }
     return (
         <div className="lenker blokk-m">
             <Link
-                to="portefolje"
-                className={classNames({ 'typo-undertittel': true, valgt: erValgt('portefolje') })}
-                aria-selected={erValgt('portefolje')}
+                to={{
+                    pathname: `portefolje/${veileder.ident}`
+                }}
+                className="oversiktslenke typo-undertittel"
+                activeClassName="oversiktslenke--valgt"
+                aria-selected={erValgt('portefolje(/:ident)')}
             >
                 <FormattedMessage id="lenker.min.oversikt" />
             </Link>
@@ -22,7 +26,8 @@ function LenkerMinoversikt({ routes }) {
 }
 
 LenkerMinoversikt.propTypes = {
-    routes: PT.arrayOf(PT.object)
+    routes: PT.arrayOf(PT.object),
+    veileder: veilederShape.isRequired
 };
 
 export default LenkerMinoversikt;
--- web/src/frontend/app/lenker/lenker.js
+++ web/src/frontend/app/lenker/lenker.js
@@ -1,34 +1,35 @@
-import React from 'react';
+import React, { PropTypes as PT } from 'react';
 import { Link } from 'react-router';
 import { FormattedMessage } from 'react-intl';
-import classNames from 'classnames';
 
 function Lenker({ routes }) {
     const valgtSide = routes[1] ? routes[1].path : '';
     function erValgt(lenke) {
         return lenke === valgtSide;
     }
-function Lenker() {
+
     return (
         <div className="lenker blokk-m">
-            <Link
-                to="enhet"
-                className={classNames({ 'typo-undertittel': true, valgt: erValgt('enhet') })}
-                aria-selected={erValgt('enhet')}
+            <Link to="enhet"
+                  className="oversiktslenke typo-undertittel"
+                  activeClassName="oversiktslenke--valgt"
+                  aria-selected={erValgt('enhet')}
             >
-            <Link to="enhet" className="oversiktslenke typo-undertittel" activeClassName="oversiktslenke--valgt">
                 <FormattedMessage id="lenker.enhet.oversikt" />
             </Link>
-            <Link
-                to="veiledere"
-                className={classNames({ 'typo-undertittel': true, valgt: erValgt('veiledere') })}
-                aria-selected={erValgt('veiledere')}
+            <Link to="veiledere"
+                  className="oversiktslenke typo-undertittel"
+                  activeClassName="oversiktslenke--valgt"
+                  aria-selected={erValgt('veiledere')}
             >
-            <Link to="veiledere" className="oversiktslenke typo-undertittel" activeClassName="oversiktslenke--valgt">
                 <FormattedMessage id="lenker.veiledere.oversikt" />
             </Link>
         </div>
     );
 }
+
+Lenker.propTypes = {
+    routes: PT.arrayOf(PT.object)
+};
 
 export default Lenker;
