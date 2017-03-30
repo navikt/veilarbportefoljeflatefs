a3a50453b9439b106fa545e124959a412a279cc2
Peder Korsveien <peder.korsveien@nav.no>
2017-03-30 09:12:44 +0200

    Endre diagram til en pure component

--- web/src/frontend/app/minoversikt/diagram.js
+++ web/src/frontend/app/minoversikt/diagram.js
@@ -1,75 +1,75 @@
-import React, { Component, PropTypes as PT } from 'react';
+import React, { PropTypes as PT } from 'react';
 import Chart from './chart';
 
-class Diagram extends Component {
-
-    render() {
-        const kategori = 'AAP Maxtid';
-        const chartsConfig = {
-            chart: {
-                type: 'column'
-            },
+const Diagram = ({ kategori }) => {
+    const chartsConfig = {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Stacked column chart'
+        },
+        xAxis: {
+            categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+        },
+        yAxis: {
+            min: 0,
             title: {
-                text: 'Stacked column chart'
+                text: 'Total fruit consumption'
             },
-            xAxis: {
-                categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'Total fruit consumption'
-                },
-                stackLabels: {
+            stackLabels: {
+                enabled: true,
+                style: {
+                    fontWeight: 'bold',
+                    color: 'gray'
+                }
+            }
+        },
+        legend: {
+            align: 'right',
+            x: -30,
+            verticalAlign: 'top',
+            y: 25,
+            floating: true,
+            backgroundColor: 'white',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            shadow: false
+        },
+        tooltip: {
+            headerFormat: '<b>{point.x}</b><br/>',
+            pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+        },
+        plotOptions: {
+            column: {
+                stacking: 'normal',
+                dataLabels: {
                     enabled: true,
-                    style: {
-                        fontWeight: 'bold',
-                        color: 'gray'
-                    }
+                    color: 'white'
                 }
-            },
-            legend: {
-                align: 'right',
-                x: -30,
-                verticalAlign: 'top',
-                y: 25,
-                floating: true,
-                backgroundColor: 'white',
-                borderColor: '#CCC',
-                borderWidth: 1,
-                shadow: false
-            },
-            tooltip: {
-                headerFormat: '<b>{point.x}</b><br/>',
-                pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
-            },
-            plotOptions: {
-                column: {
-                    stacking: 'normal',
-                    dataLabels: {
-                        enabled: true,
-                        color: 'white'
-                    }
-                }
-            },
-            series: [{
-                name: 'John',
-                data: [5, 3, 4, 7, 2]
-            }, {
-                name: 'Jane',
-                data: [2, 2, 3, 2, 1]
-            }, {
-                name: 'Joe',
-                data: [3, 4, 4, 2, 5]
-            }]
-        };
-        return (
-            <div>
-                <h1>{kategori}</h1>
-                <Chart type="Chart" options={chartsConfig} container="chart" />
-            </div>
-        );
-    }
-}
+            }
+        },
+        series: [{
+            name: 'John',
+            data: [5, 3, 4, 7, 2]
+        }, {
+            name: 'Jane',
+            data: [2, 2, 3, 2, 1]
+        }, {
+            name: 'Joe',
+            data: [3, 4, 4, 2, 5]
+        }]
+    };
+    return (
+        <div>
+            <h1>{kategori}</h1>
+            <Chart type="Chart" options={chartsConfig} container="chart" />
+        </div>
+    );
+};
+
+Diagram.propTypes = {
+    kategori: PT.string.isRequired
+};
 
 export default Diagram;
