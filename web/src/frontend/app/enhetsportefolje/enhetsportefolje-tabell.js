44a0da7c6925fdabfae442aaa334570a475ab621
Nicklas Utgaard <nicklas.utgaard@nav.no>
2017-03-30 16:01:32 +0200

    Tabell etiketter

--- web/src/frontend/app/enhetsportefolje/enhetsportefolje-tabell.js
+++ web/src/frontend/app/enhetsportefolje/enhetsportefolje-tabell.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes as PT } from 'react';
 import classNames from 'classnames';
 import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
+import Tabelletiketter from './../components/tabelletiketter/tabelletiketter';
 import { veilederShape, brukerShape, portefoljeShape } from '../proptype-shapes';
 import { markerAlleBrukere } from './../ducks/portefolje';
 import TomPortefoljeModal from '../modal/tom-portefolje-modal';
@@ -34,116 +35,118 @@ class EnhetsportefoljeTabell extends Component {
     }
 
     render() {
-        const { brukere, veiledere, settSomMarkertAlle,
-            settSomMarkert, portefolje, modalSkalVises, toggleSkjulModal, valgtEnhet } = this.props;
+        const {
+            brukere, veiledere, settSomMarkertAlle,
+            settSomMarkert, portefolje, modalSkalVises, toggleSkjulModal, valgtEnhet
+        } = this.props;
         const sorterEtternavn = portefolje.sorteringsfelt === 'etternavn';
         const sorterFodelsnummer = portefolje.sorteringsfelt === 'fodselsdato';
 
         const alleMarkert = brukere.length > 0 && brukere.every((bruker) => bruker.markert);
         return (
             <div>
-                <TomPortefoljeModal skjulModal={toggleSkjulModal} visModal={modalSkalVises} />
+                <TomPortefoljeModal skjulModal={toggleSkjulModal} visModal={modalSkalVises}/>
                 <table className="tabell portefolje-tabell typo-undertekst">
                     <thead className="extra-head">
-                        <tr>
-                            <th />
-                            <th>Bruker</th>
-                            <th />
-                            <th>Veileder</th>
-                            <th />
-                            <th />
-                        </tr>
+                    <tr>
+                        <th />
+                        <th colSpan="2">Bruker</th>
+                        <th colSpan="3">Veileder</th>
+                    </tr>
                     </thead>
                     <thead>
-                        <tr>
-                            <th>
-                                <div className="skjema__input">
-                                    <input
-                                        className="checkboks"
-                                        id="checkbox-alle-brukere"
-                                        type="checkbox"
-                                        checked={alleMarkert}
-                                        onClick={() => settSomMarkertAlle(!alleMarkert)}
-                                    />
-                                    <label className="skjema__label" htmlFor="checkbox-alle-brukere" />
-                                </div>
-                            </th>
-                            <th>
-                                <button
-                                    onClick={() => this.settSorteringOgHentPortefolje('etternavn')}
-                                    className={classNames({ 'sortering-link': true, valgt: sorterEtternavn })}
-                                >
-                                    <FormattedMessage id="enhet.veiledere.tabell.etternavn" />
-                                </button>
-                                <FormattedMessage id="enhet.veiledere.tabell.fornavn" />
-                            </th>
-                            <th>
-                                <button
-                                    onClick={() => this.settSorteringOgHentPortefolje('fodselsdato')}
-                                    className={classNames({ 'sortering-link': true, valgt: sorterFodelsnummer })}
-                                >
-                                    <FormattedMessage id="portefolje.tabell.fodselsnummer" />
-                                </button>
-                            </th>
-                            <th>
-                                <button
-                                    onClick={() => this.settSorteringOgHentPortefolje('etternavn')}
-                                    className="sortering-link"
-                                >
-                                    <FormattedMessage id="enhet.veiledere.tabell.etternavn" />
-                                </button>
-                                <FormattedMessage id="enhet.veiledere.tabell.fornavn" />
-                            </th>
-                            <th>
-                                <FormattedMessage id="portefolje.tabell.navident" />
-                            </th>
-                            <th />
-                        </tr>
+                    <tr>
+                        <th>
+                            { /* TODO hent checkbokser fra nav-frontend-skjema */}
+                            <div className="skjema__input">
+                                <input
+                                    className="checkboks"
+                                    id="checkbox-alle-brukere"
+                                    type="checkbox"
+                                    checked={alleMarkert}
+                                    onClick={() => settSomMarkertAlle(!alleMarkert)}
+                                />
+                                <label className="skjema__label" htmlFor="checkbox-alle-brukere"/>
+                            </div>
+                        </th>
+                        <th>
+                            <button
+                                onClick={() => this.settSorteringOgHentPortefolje('etternavn')}
+                                className={classNames({ 'sortering-link': true, valgt: sorterEtternavn })}
+                            >
+                                <FormattedMessage id="enhet.veiledere.tabell.etternavn"/>
+                            </button>
+                            <FormattedMessage id="enhet.veiledere.tabell.fornavn"/>
+                        </th>
+                        <th>
+                            <button
+                                onClick={() => this.settSorteringOgHentPortefolje('fodselsdato')}
+                                className={classNames({ 'sortering-link': true, valgt: sorterFodelsnummer })}
+                            >
+                                <FormattedMessage id="portefolje.tabell.fodselsnummer"/>
+                            </button>
+                        </th>
+                        <th>
+                            <button
+                                onClick={() => this.settSorteringOgHentPortefolje('etternavn')}
+                                className="sortering-link"
+                            >
+                                <FormattedMessage id="enhet.veiledere.tabell.etternavn"/>
+                            </button>
+                            <FormattedMessage id="enhet.veiledere.tabell.fornavn"/>
+                        </th>
+                        <th>
+                            <FormattedMessage id="portefolje.tabell.navident"/>
+                        </th>
+                        <th />
+                    </tr>
                     </thead>
                     <tbody>
-                        {brukere.map((bruker) => <tr key={bruker.fnr}>
-                            <td>
-                                <div className="skjema__input">
-                                    <input
-                                        className="checkboks"
-                                        id={`checkbox-${bruker.fnr}`}
-                                        type="checkbox"
-                                        checked={!!bruker.markert}
-                                        onClick={() => settSomMarkert(bruker.fnr, !bruker.markert)}
-                                    />
-                                    <label className="skjema__label" htmlFor={`checkbox-${bruker.fnr}`} />
-                                </div>
-                            </td>
-                            <th>
-                                <a
-                                    href={`https://${window.location.hostname}/veilarbpersonflatefs/` +
-                                    `${bruker.fnr}?enhet=${valgtEnhet}`}
-                                    className="til-bruker-link"
-                                >
-                                    {settSammenNavn(bruker)}
-                                </a>
-                            </th>
-                            <td>{bruker.fnr}</td>
-                            {
-                        bruker.veilederId ? <td className="veileder-td">{veiledere
-                            .filter((veileder) => veileder.ident === bruker.veilederId)
-                            .map((veileder) => (veileder.navn || veileder.ident))}</td>
-                            :
-                        <td className="ny-bruker-td"><span className="ny-bruker">Ny bruker</span></td>
-                    }
-                            <td />
-                            <td>
-                                {bruker.sikkerhetstiltak.length > 0 ?
-                                    <span className="etikett etikett--fokus">Sikkerhetstiltak</span> : null}
-                                {bruker.diskresjonskode != null ?
-                                    <span className="etikett etikett--fokus">{`Kode ${bruker.diskresjonskode}`}</span>
-                            : null}
-                                {bruker.egenAnsatt === true ?
-                                    <span className="etikett etikett--fokus">Egen ansatt</span> : null}
-                                {bruker.erDoed === true ?
-                                    <span className="etikett etikett--fokus">Død</span> : null}
-                            </td>
-                        </tr>)}
+                    {brukere.map((bruker) => <tr key={bruker.fnr}>
+                        <td>
+                            <div className="skjema__input">
+                                <input
+                                    className="checkboks"
+                                    id={`checkbox-${bruker.fnr}`}
+                                    type="checkbox"
+                                    checked={!!bruker.markert}
+                                    onClick={() => settSomMarkert(bruker.fnr, !bruker.markert)}
+                                />
+                                <label className="skjema__label" htmlFor={`checkbox-${bruker.fnr}`}/>
+                            </div>
+                        </td>
+                        <th>
+                            <a
+                                href={`https://${window.location.hostname}/veilarbpersonflatefs/` +
+                                `${bruker.fnr}?enhet=${valgtEnhet}`}
+                                className="til-bruker-link"
+                            >
+                                {settSammenNavn(bruker)}
+                            </a>
+                        </th>
+                        <td>{bruker.fnr}</td>
+                        {
+                            bruker.veilederId ? <td className="veileder-td">{veiledere
+                                    .filter((veileder) => veileder.ident === bruker.veilederId)
+                                    .map((veileder) => (veileder.navn || veileder.ident))}</td>
+                                :
+                                <td>
+                                    <Tabelletiketter type="nybruker">Ny bruker</Tabelletiketter>
+                                </td>
+                        }
+                        <td />
+                        <td>
+                            {bruker.sikkerhetstiltak.length > 0 ?
+                                <span className="etikett etikett--fokus">Sikkerhetstiltak</span> : null}
+                            {bruker.diskresjonskode != null ?
+                                <span className="etikett etikett--fokus">{`Kode ${bruker.diskresjonskode}`}</span>
+                                : null}
+                            {bruker.egenAnsatt === true ?
+                                <span className="etikett etikett--fokus">Egen ansatt</span> : null}
+                            {bruker.erDoed === true ?
+                                <span className="etikett etikett--fokus">Død</span> : null}
+                        </td>
+                    </tr>)}
                     </tbody>
                 </table>
             </div>
