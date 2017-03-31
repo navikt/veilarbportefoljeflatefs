8e7d252de697bc007c4999cd3e22c6f57327485b
Peder Korsveien <peder.korsveien@nav.no>
2017-03-31 13:30:37 +0200

    Parse til Date og vis dato i korrekt format

--- web/src/frontend/app/tabell/kolonne_utlopsdato.tsx
+++ web/src/frontend/app/tabell/kolonne_utlopsdato.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
-export default function (props: { utlopsdato: any}) {
-    const { dayOfMonth, monthValue, year } = props.utlopsdato;
-    const dato = `${dayOfMonth}.${monthValue}.${year}`;
-    return <td>{dato}</td>;
+export default function (props: { utlopsdato: string}) {
+    const dato: Date = new Date(props.utlopsdato);
+    const options = {day: '2-digit', month: '2-digit', year: 'numeric'};
+    return <td>{dato.toLocaleDateString(['nb-no', 'nn-no', 'en-gb', 'en-us'], options)}</td>;
 }
