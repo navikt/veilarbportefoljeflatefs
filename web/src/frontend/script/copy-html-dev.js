3ed8bceaa968cb13603e1631c36a2f82cf2f6b12
Christian Finstad <christian.finstad@nav.no>
2017-03-31 12:10:43 +0200

    Egen copy-html-dev for å unngå at dev-html blir brukt i prod.

--- web/src/frontend/script/copy-html-dev.js
+++ web/src/frontend/script/copy-html-dev.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const dest = '../main/webapp/index.html';
 
 /* eslint-disable no-var*/
-var src = 'index.html';
+var src = 'index-dev.html';
+
 
 fs.createReadStream(src).pipe(fs.createWriteStream(dest));
