92a42f6fa15599c23367eb1a6ae380f6fbbf4b68
Oscar Eckbo <oscar.eckbo@nav.no>
2017-04-03 12:07:47 +0200

    Sletter (forhåpentligvis) unødvendig styling

--- web/src/frontend/app/minoversikt/minoversikt-portefolje-visning.js
+++ web/src/frontend/app/minoversikt/minoversikt-portefolje-visning.js
@@ -189,7 +189,7 @@ class VeilederPortefoljeVisning extends Component {
                                             <Utlopsdatokolonne utlopsdato={bruker.utlopsdato} />
                                         : null
                                     }
-                                    <td className="sikkerhetstiltak-td">
+                                    <td>
                                         {bruker.sikkerhetstiltak.length > 0 ?
                                             <Tabelletiketter type="sikkerhetstiltak">
                                                 Sikkerhetstiltak
