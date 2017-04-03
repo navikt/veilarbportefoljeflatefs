b60eef3ea5e3d511a2414ed31c5abb613e55a2d3
Peder Korsveien <peder.korsveien@nav.no>
2017-04-03 10:14:53 +0200

    Parse LocalDateTime, endre til js pga linting

--- web/src/frontend/app/tabell/kolonne_utlopsdato.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export default function (props: { utlopsdato: string}) {
-    const dato: Date = new Date(props.utlopsdato);
-    const options = {day: '2-digit', month: '2-digit', year: 'numeric'};
-    return <td>{dato.toLocaleDateString(['nb-no', 'nn-no', 'en-gb', 'en-us'], options)}</td>;
-}
