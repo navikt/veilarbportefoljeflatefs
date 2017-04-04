a71bbd45a1e0431c34fd4b2fa240edfebfb9e675
Oscar Eckbo <oscar.eckbo@nav.no>
2017-04-04 10:13:49 +0200

    Bruker en Guid istedenfor fnr--index som key i porteføljetabell.

--- web/src/frontend/app/enhetsportefolje/enhetsportefolje-tabell.js
+++ web/src/frontend/app/enhetsportefolje/enhetsportefolje-tabell.js
@@ -119,7 +119,7 @@ class EnhetsportefoljeTabell extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {brukere.map((bruker, index) => <tr key={`${bruker.fnr}--${index}`}>
+                        {brukere.map((bruker) => <tr key={bruker.guid}>
                             <td>
                                 <div className="skjema__input">
                                     <input
