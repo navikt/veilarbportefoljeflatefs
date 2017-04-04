8c35ad93c190b20298ba44199ec0766fb9b6b6eb
Oscar Eckbo <oscar.eckbo@nav.no>
2017-04-04 13:30:15 +0200

    sletter testfilen

--- web/src/frontend/app/paginering/paginering-test.js
+++
@@ -1,7 +0,0 @@
-import { expect } from 'chai';
-
-describe('paginering', () => {
-    it('Når man er på første side skal ikke forrigeknapp være aktiv', () => {
-        expect(1).to.be.equal(1);
-    });
-});
