4992104e186da46594397335bfe3d3e9a7065342
Oscar Eckbo <oscar.eckbo@nav.no>
2017-04-04 10:11:34 +0200

    Drar ut createSimpleLinks i egen komponent

--- web/src/frontend/app/paginering/paginering.js
+++ web/src/frontend/app/paginering/paginering.js
@@ -1,30 +1,18 @@
 import React, { PropTypes as PT } from 'react';
 import classNames from 'classnames';
 import { Element } from 'nav-frontend-typografi';
+import createSimpleLink from '../components/simple-link';
 
 function Paginering({ fraIndex, antallTotalt, hentListe, tekst, sideStorrelse, antallReturnert }) {
-    function createSimpleLink(fraIndeks, tilIndeks, child, className) {
-        return (
-            <button
-                className={className}
-                onClick={(e) => {
-                    e.preventDefault();
-                    hentListe(fraIndeks, tilIndeks);
-                }}
-            >
-                {child}
-            </button>);
-    }
-
-    const fraIndeksForrigeSide = fraIndex - sideStorrelse < 0 ? fraIndex : fraIndex - sideStorrelse;
-    const fraIndeksNesteSide = antallTotalt % sideStorrelse === 0 ? antallTotalt - sideStorrelse
-        : antallTotalt - (antallTotalt % sideStorrelse);
-    const fraIndeksSisteSide = fraIndex + sideStorrelse >= antallTotalt ? fraIndex : fraIndex + sideStorrelse;
+    const fraIndeksForrigeSide = (fraIndex - sideStorrelse < 0) ? fraIndex : (fraIndex - sideStorrelse);
+    const fraIndeksNesteSide = (antallTotalt % sideStorrelse === 0) ? (antallTotalt - sideStorrelse)
+        : (antallTotalt - (antallTotalt % sideStorrelse));
+    const fraIndeksSisteSide = (fraIndex + sideStorrelse >= antallTotalt) ? fraIndex : (fraIndex + sideStorrelse);
 
     const ikkeAktiv = classNames({ 'not-active': antallTotalt === antallReturnert });
-    const seAlleKnapp = createSimpleLink(0, antallTotalt, 'Se alle');
-    const seFaerreKnapp = createSimpleLink(0, sideStorrelse, 'Se færre');
-    const seAlleKnappInactive = createSimpleLink(0, antallTotalt, 'Se alle', 'not-active');
+    const seAlleKnapp = createSimpleLink(0, antallTotalt, 'Se alle', hentListe);
+    const seFaerreKnapp = createSimpleLink(0, sideStorrelse, 'Se færre', hentListe);
+    const seAlleKnappInactive = createSimpleLink(0, antallTotalt, 'Se alle', hentListe, 'not-active');
 
     function lagChevron(isLeft) {
         if (isLeft) {
@@ -58,12 +46,12 @@ function Paginering({ fraIndex, antallTotalt, hentListe, tekst, sideStorrelse, a
                 </button>
             );
         }
-        return (createSimpleLink(fraIndeksForrigeSide, sideStorrelse, lagChevron(true), 'prev'));
+        return (createSimpleLink(fraIndeksForrigeSide, sideStorrelse, lagChevron(true), hentListe, 'prev'));
     }
 
     function visSideEnKnapp() {
         if (fraIndex !== 0) {
-            return createSimpleLink(0, sideStorrelse, '1', ikkeAktiv);
+            return createSimpleLink(0, sideStorrelse, '1', hentListe, ikkeAktiv);
         }
         return null;
     }
@@ -73,7 +61,7 @@ function Paginering({ fraIndex, antallTotalt, hentListe, tekst, sideStorrelse, a
     function visSisteSideKnapp() {
         if (fraIndex < fraIndeksSisteSide && antallTotalt !== antallReturnert) {
             return createSimpleLink(
-                fraIndeksNesteSide, sideStorrelse, Math.ceil(antallTotalt / sideStorrelse), ikkeAktiv
+                fraIndeksNesteSide, sideStorrelse, Math.ceil(antallTotalt / sideStorrelse), hentListe, ikkeAktiv
             );
         }
         return null;
@@ -87,7 +75,7 @@ function Paginering({ fraIndex, antallTotalt, hentListe, tekst, sideStorrelse, a
                 </button>
             );
         }
-        return createSimpleLink(fraIndeksSisteSide, sideStorrelse, lagChevron(false), ikkeAktiv);
+        return createSimpleLink(fraIndeksSisteSide, sideStorrelse, lagChevron(false), hentListe, ikkeAktiv);
     }
 
     return (
