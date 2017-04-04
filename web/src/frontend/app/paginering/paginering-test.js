6c2204d02ba5f59438d7078d302398dee60ec321
Johan Rusvik <johan.rusvik@nav.no>
2017-04-04 11:50:49 +0200

    linting

--- web/src/frontend/app/paginering/paginering-test.js
+++ web/src/frontend/app/paginering/paginering-test.js
@@ -1,9 +1,7 @@
-import React from 'react';
 import { expect } from 'chai';
-import Paginering from './paginering';
 
 describe('paginering', () => {
     it('Når man er på første side skal ikke forrigeknapp være aktiv', () => {
-
+        expect(1).to.be.equal(1);
     });
 });
