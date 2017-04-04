3745636227ea2da3b20d3e6ed11640dbb928fb77
Oscar Eckbo <oscar.eckbo@nav.no>
2017-04-04 15:03:58 +0200

    tmp

---
+++ web/src/frontend/app/paginering/paginering-test.js
@@ -0,0 +1,16 @@
+import React from '../../test/test-helpers';
+import { shallow } from 'enzyme';
+import ReactTestUtils from 'react-addons-test-utils';
+
+describe('paginering', () => {
+    it('skal ikke gjøre en dritt', () => {
+        // const renderer = ReactTestUtils.createRenderer();
+        console.log("hei");
+    });
+    it('Skal rendre 2 bokser', () => {
+        const valgteYrkesgrupper = ["1", "2"];
+        const wrapper = shallow(<div>HEI</div>);
+    });
+
+});
+
---
+++ web/src/frontend/test/test-helpers.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import chai from 'chai';
+import TestUtils from 'react-addons-test-utils';
+import _ from './DOM-setup'; // eslint-disable-line no-unused-vars
+
+// const { assert, expect } = chai;
+
+export {
+    React,
+    chai,
+    // assert,
+    // expect,
+    TestUtils
+}
