cc76e56618fcccb5653739e97d1cedc63a1907ff
Peder Korsveien <peder.korsveien@nav.no>
2017-04-05 11:43:56 +0200

    Refaktorer nyttefunksjoner

--- web/src/frontend/app/minoversikt/diagram/util.js
+++ web/src/frontend/app/minoversikt/diagram/util.js
@@ -2,14 +2,28 @@ import { headertekst, legendtekst } from './diagram-konstanter'
 import moment from 'moment';
 import { ytelsevalg } from '../../filtrering/filter-konstanter';
 
+export function runningTotal(arr) {
+    const runningTotal = new Array(arr.length);
+    arr.reduce((acc, val, i) => {
+        return runningTotal[i] = acc + val;
+    }, 0);
+    return runningTotal;
+}
+
+export function harYtelseSerie(lopendeSum, antallBrukere) {
+    return new Array(lopendeSum.length)
+                    .fill(antallBrukere)
+                    .map((antall, i) => antall - lopendeSum[i]);
+}
+
 export function maned(brukere) {
     moment.locale('nb_no');
 
-    const labels = new Array(12).fill(0).map((_, i) => moment().add(i + 1, 'month').format('MMMM'));
+    const len = 12;
+    const labels = new Array(len).fill(0).map((_, i) => moment().add(i + 1, 'month').format('MMMM'));
+    const maaneder = new Array(len).fill(0).map((_, i) => `MND${i + 1}`);
 
-    const maaneder = new Array(12).fill(0).map((_, i) => `MND${i + 1}`);
-
-    let antallMisterYtelse = new Array(12).fill(0);
+    let antallMisterYtelse = new Array(len).fill(0);
     brukere
         .filter(bruker => bruker.utlopsdatoFasett)
         .map(bruker => {
@@ -18,34 +32,26 @@ export function maned(brukere) {
             antallMisterYtelse[index] = value + 1;
         });
 
-
-    let runningSum = new Array(12);
-    antallMisterYtelse.reduce((acc, val, i) => {
-        return runningSum[i] = acc + val;
-    }, 0);
-
-    const antallMedYtelse = new Array(12)
-        .fill(brukere.length)
-        .map((antall, i) => antall - runningSum[i]);
+    const lopendeSum = runningTotal(antallMisterYtelse);
 
     return {
         labels,
         antallMisterYtelse,
-        antallMedYtelse
+        antallMedYtelse: harYtelseSerie(lopendeSum, brukere.length)
     }
 }
 
 export function kvartal(brukere) {
     moment.locale('nb_no');
 
-    const labels = new Array(16).fill(0).map((_, i) => {
+    const len = 16;
+    const labels = new Array(len).fill(0).map((_, i) => {
         const quarter = moment().add(i, 'quarter');
         return `Q${quarter.quarter()}.${quarter.year()}`
     });
+    const kvartaler = new Array(len).fill(0).map((_, i) => `KV${i + 1}`);
 
-    const kvartaler = new Array(16).fill(0).map((_, i) => `KV${i + 1}`);
-
-    let antallMisterYtelse = new Array(16).fill(0);
+    let antallMisterYtelse = new Array(len).fill(0);
     brukere
         .filter(bruker => bruker.aapMaxtidFasett)
         .map(bruker => {
@@ -54,19 +60,12 @@ export function kvartal(brukere) {
             antallMisterYtelse[index] = value + 1;
         });
 
-    let runningSum = new Array(16);
-    antallMisterYtelse.reduce((acc, val, i) => {
-        return runningSum[i] = acc + val;
-    }, 0);
-
-    const antallMedYtelse = new Array(16)
-        .fill(brukere.length)
-        .map((antall, i) => antall - runningSum[i]);
+    const lopendeSum = runningTotal(antallMisterYtelse);
 
     return {
         labels,
         antallMisterYtelse,
-        antallMedYtelse
+        antallMedYtelse: harYtelseSerie(lopendeSum)
     };
 }
 
@@ -106,5 +105,7 @@ export function ledetekster(filtreringvalg) {
 export default {
     maned,
     kvartal,
-    ledetekster
+    ledetekster,
+    runningTotal,
+    harYtelseSerie
 }
