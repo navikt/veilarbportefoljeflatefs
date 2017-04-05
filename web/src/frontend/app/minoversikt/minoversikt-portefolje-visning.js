2a12ad86ef8c3dae74dd93347a00ac0e2c2ad975
Christian Finstad <christian.finstad@nav.no>
2017-04-05 15:33:37 +0200

    Legger til ident i veilederlista

--- web/src/frontend/app/minoversikt/minoversikt-portefolje-visning.js
+++ web/src/frontend/app/minoversikt/minoversikt-portefolje-visning.js
@@ -94,7 +94,7 @@ class VeilederPortefoljeVisning extends Component {
                 (<FormattedMessage
                     id="enhet.portefolje.paginering.tekst"
                     values={{ fraIndex: `${fraIndex + 1}`, tilIndex: fraIndex + antallReturnert, antallTotalt }}
-                />):
+                />) :
                 (<FormattedMessage
                     id="enhet.portefolje.paginering.tekst"
                     values={{ fraIndex: '0', tilIndex: '0', antallTotalt: '0' }}
