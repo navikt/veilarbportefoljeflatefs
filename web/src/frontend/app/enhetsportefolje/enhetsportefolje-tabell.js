e353ecdb8407b3c97fc2118a7fa2777f5c5d39dc
Oscar Eckbo <oscar.eckbo@nav.no>
2017-04-05 14:08:21 +0200

    linting

--- web/src/frontend/app/enhetsportefolje/enhetsportefolje-tabell.js
+++ web/src/frontend/app/enhetsportefolje/enhetsportefolje-tabell.js
@@ -87,7 +87,7 @@ class EnhetsportefoljeTabell extends Component {
                             </th>
                             <th>
                                 <button
-                                    onClick={() => { this.settSorteringOgHentPortefolje('etternavn'); }}
+                                    onClick={() => this.settSorteringOgHentPortefolje('etternavn')}
                                     className={classNames({ 'sortering-link': true, valgt: sorterEtternavn })}
                                 >
                                     <FormattedMessage id="enhet.veiledere.tabell.etternavn" />
@@ -96,7 +96,7 @@ class EnhetsportefoljeTabell extends Component {
                             </th>
                             <th>
                                 <button
-                                    onClick={() => { this.settSorteringOgHentPortefolje('fodselsdato'); }}
+                                    onClick={() => this.settSorteringOgHentPortefolje('fodselsdato')}
                                     className={classNames({ 'sortering-link': true, valgt: sorterFodelsnummer })}
                                 >
                                     <FormattedMessage id="portefolje.tabell.fodselsnummer" />
@@ -105,7 +105,7 @@ class EnhetsportefoljeTabell extends Component {
                             {utlopsdatoHeader}
                             <th>
                                 <button
-                                    onClick={() => { this.settSorteringOgHentPortefolje('etternavn'); }}
+                                    onClick={() => this.settSorteringOgHentPortefolje('etternavn')}
                                     className="sortering-link"
                                 >
                                     <FormattedMessage id="enhet.veiledere.tabell.etternavn" />
