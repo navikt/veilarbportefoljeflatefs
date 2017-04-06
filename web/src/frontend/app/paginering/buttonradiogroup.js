cbc6832c24c7c237f8f9df62ef8f3f0c66213ae8
Peder Korsveien <peder.korsveien@nav.no>
2017-04-06 14:49:01 +0200

    Reset visningsmodus ved fjerning av filter

--- web/src/frontend/app/paginering/buttonradiogroup.js
+++ web/src/frontend/app/paginering/buttonradiogroup.js
@@ -1,44 +1,54 @@
-import React, { PropTypes as PT } from 'react';
+import React, { Component, PropTypes as PT } from 'react';
 import { connect } from 'react-redux';
 import { settVisningsmodus } from '../ducks/veilederpaginering';
 import { DIAGRAMVISNING, TABELLVISNING } from '../minoversikt/minoversikt-konstanter';
 
-const ButtonRadiogroup = ({ visningsmodus, handleClick }) => (
-    <div className="visningsgruppe">
-        <input
-            id="diagramvisning"
-            name="visningsmodus"
-            type="radio"
-            onChange={() => handleClick(DIAGRAMVISNING)}
-            value="diagramvisning"
-            checked={visningsmodus === DIAGRAMVISNING}
-            aria-selected={visningsmodus === DIAGRAMVISNING}
-        />
-        <label htmlFor="diagramvisning" className="typo-undertekst">Vis som diagram</label>
-        <input
-            id="tabellvisning"
-            name="visningsmodus"
-            type="radio"
-            onChange={() => handleClick(TABELLVISNING)}
-            value="tabellvisning"
-            checked={visningsmodus === TABELLVISNING}
-            aria-selected={visningsmodus === TABELLVISNING}
-        />
-        <label htmlFor="tabellvisning" className="typo-undertekst">Vis som tabell</label>
-    </div>
-    );
+class ButtonRadiogroup extends Component {
+
+    componentWillUnmount() {
+        this.props.endreVisningsmodus(TABELLVISNING);
+    }
+
+    render() {
+        const { visningsmodus, endreVisningsmodus } = this.props;
+        return (
+            <div className="visningsgruppe">
+                <input
+                    id="diagramvisning"
+                    name="visningsmodus"
+                    type="radio"
+                    onChange={() => endreVisningsmodus(DIAGRAMVISNING)}
+                    value="diagramvisning"
+                    checked={visningsmodus === DIAGRAMVISNING}
+                    aria-selected={visningsmodus === DIAGRAMVISNING}
+                />
+                <label htmlFor="diagramvisning" className="typo-undertekst">Vis som diagram</label>
+                <input
+                    id="tabellvisning"
+                    name="visningsmodus"
+                    type="radio"
+                    onChange={() => endreVisningsmodus(TABELLVISNING)}
+                    value="tabellvisning"
+                    checked={visningsmodus === TABELLVISNING}
+                    aria-selected={visningsmodus === TABELLVISNING}
+                />
+                <label htmlFor="tabellvisning" className="typo-undertekst">Vis som tabell</label>
+            </div>
+        );
+    }
+}
 
 const mapStateToProps = (state) => ({
     visningsmodus: state.veilederpaginering.visningsmodus
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    handleClick: (modus) => { dispatch(settVisningsmodus(modus)); }
+    endreVisningsmodus: (modus) => { dispatch(settVisningsmodus(modus)); },
 });
 
 ButtonRadiogroup.propTypes = {
     visningsmodus: PT.string.isRequired,
-    handleClick: PT.func.isRequired
+    endreVisningsmodus: PT.func.isRequired
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ButtonRadiogroup);
