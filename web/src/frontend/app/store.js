3eca3e6592e2a218ad4a0eeee17215adaf28f8f7
Hageland, Steffen <steffen.hageland@nav.no>
2017-04-11 13:52:05 +0200

    Fjerne innebygd devtools fra prosjektet.
    Når man bruker chrome-extension trenger man ikke bundle med devtools.
    Ved å ikke bygge den ved reduserer man bundle-size betydelig, i tillegg
    er de innebygde devtoolene hovedårsaken til at applikasjonen er utrolig
    treg når vi kjører den opp lokalt.
    
    La også inn en kort forklaring i readme om dette.

--- web/src/frontend/app/store.js
+++ web/src/frontend/app/store.js
@@ -1,38 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { persistState } from 'redux-devtools';
 import thunkMiddleware from 'redux-thunk';
 import { routerMiddleware } from 'react-router-redux';
-import DevTools from './devtools';
 import reducer from './reducer';
-import { erDev } from './utils/utils';
 
-
-function getDebugSessionKey() {
-    const matches = window.location.href.match(/[?&]debug_session=([^&]+)\b/);// eslint-disable-line no-undef
-    return (matches && matches.length > 0) ? matches[1] : null;
-}
-
-function getDevStoreCompose(history) {
+export default function create(history) {
     /* eslint-disable no-underscore-dangle */
     const useExtension = window.__REDUX_DEVTOOLS_EXTENSION__ !== undefined;
     const composer = useExtension ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
     /* eslint-enable */
 
-    return composer(
-        applyMiddleware(thunkMiddleware, routerMiddleware(history)),
-        useExtension ? c => c : DevTools.instrument(),
-        persistState(getDebugSessionKey())
-    );
-}
-
-function getStoreCompose(history) {
-    return compose(
+    const composed = composer(
         applyMiddleware(thunkMiddleware, routerMiddleware(history))
     );
-}
-
-export default function create(history) {
-    const composed = erDev() ? getDevStoreCompose(history) : getStoreCompose(history);
 
     return composed(createStore)(reducer, {});
 }
