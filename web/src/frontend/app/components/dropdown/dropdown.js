d67be565ac567f430b05cbdd83b20f934916e19b
Nicklas Utgaard <nicklas.utgaard@nav.no>
2017-06-06 14:21:23 +0200

    Setter fokus på første fokuserbare element i dropdown, og fokus tilbake på knappen når den lukkes.

--- web/src/frontend/app/components/dropdown/dropdown.js
+++ web/src/frontend/app/components/dropdown/dropdown.js
@@ -1,4 +1,4 @@
-import React, { PropTypes as PT, Children, Component, cloneElement } from 'react';
+import React, { Children, cloneElement, Component, PropTypes as PT } from 'react';
 import classNames from 'classnames';
 
 const btnCls = (erApen, className) => classNames('dropdown', className, {
@@ -25,6 +25,7 @@ class Dropdown extends Component {
         this.toggleDropdown = this.toggleDropdown.bind(this);
         this.lukkDropdown = this.lukkDropdown.bind(this);
         this.bindComponent = this.bindComponent.bind(this);
+        this.settFokus = this.settFokus.bind(this);
         this.handler = (e) => {
             if (this.state.apen && !isChildOf(this.component, e.target)) {
                 this.lukkDropdown();
@@ -55,7 +56,15 @@ class Dropdown extends Component {
     lukkDropdown() {
         const { onLukk = () => {} } = this.props;
         this.setState({ apen: false });
+        this.btn.focus();
         onLukk();
+    }
+
+    settFokus(element) {
+        if (element !== null) {
+            const elementer = element.querySelector('button, a, input, select');
+            elementer.focus();
+        }
     }
 
     render() {
@@ -66,12 +75,18 @@ class Dropdown extends Component {
             closeDropdown: this.lukkDropdown
         }));
         const innhold = !apen ? null : (
-            <div className="dropdown__innhold" id={`${name}-dropdown__innhold`}>{augmentedChild}</div>
-            );
+            <div className="dropdown__innhold" id={`${name}-dropdown__innhold`} ref={this.settFokus}>
+                {augmentedChild}
+            </div>
+        );
+
         return (
             <div className={btnCls(apen, className)} ref={this.bindComponent}>
                 <div className="dropdown__btnwrapper">
                     <button
+                        ref={(btn) => {
+                            this.btn = btn;
+                        }}
                         type="button"
                         className="dropdown__btn"
                         onClick={this.toggleDropdown}
