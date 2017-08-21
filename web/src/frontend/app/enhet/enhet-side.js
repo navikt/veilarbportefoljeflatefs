5e0a9a1046a4677114d3b3047995e0af74b0eeb8
S151399 <marthe.skogen@nav.no>
2017-08-21 10:09:31 +0200

    Henter tiltak for enhet fra backend

--- web/src/frontend/app/enhet/enhet-side.js
+++ web/src/frontend/app/enhet/enhet-side.js
@@ -10,6 +10,7 @@ import FiltreringContainer from '../filtrering/filtrering-container';
 import FiltreringLabelContainer from '../filtrering/filtrering-label-container';
 import { leggEnhetIUrl } from '../utils/utils';
 import { hentStatusTall } from './../ducks/statustall';
+import { hentEnhetTiltak } from './../ducks/enhettiltak';
 import TomPortefoljeModal from '../modal/tom-portefolje-modal';
 
 
@@ -21,6 +22,7 @@ class EnhetSide extends Component {
 
     componentDidMount() {
         this.props.hentStatusTall(this.props.valgtEnhet.enhet.enhetId);
+        this.props.hentEnhetTiltak(this.props.valgtEnhet.enhet.enhetId);
     }
 
     render() {
@@ -29,7 +31,7 @@ class EnhetSide extends Component {
             return null;
         }
         const { formatMessage } = this.props.intl;
-        const { filtervalg, veilederliste, statustall } = this.props;
+        const { filtervalg, veilederliste, statustall, enhettiltak } = this.props;
 
         const leggTilNavn = (identer, veiledere) => identer.map((ident) => {
             const veileder = veiledere.find((v) => v.ident === ident);
@@ -40,7 +42,7 @@ class EnhetSide extends Component {
             <DocumentTitle title={formatMessage({ id: 'lenker.enhet.oversikt' })}>
                 <div className="enhet-side blokk-xl">
                     <Lenker />
-                    <Innholdslaster avhengigheter={[statustall]}>
+                    <Innholdslaster avhengigheter={[statustall, enhettiltak]}>
                         <div id="oversikt-sideinnhold" role="tabpanel">
                             <p className="typo-infotekst begrensetbredde blokk-l">
                                 <FormattedMessage id="enhet.ingresstekst.enhetoversikt" />
@@ -68,7 +70,9 @@ EnhetSide.propTypes = {
     filtervalg: filtervalgShape.isRequired,
     veilederliste: PT.arrayOf(veilederShape).isRequired,
     hentStatusTall: PT.func.isRequired,
+    hentEnhetTiltak: PT.func.isRequired,
     statustall: PT.shape({ data: statustallShape }),
+    enhettiltak: PT.arrayOf(PT.string),
     intl: intlShape.isRequired
 };
 
@@ -76,11 +80,13 @@ const mapStateToProps = (state) => ({
     valgtEnhet: state.enheter.valgtEnhet,
     filtervalg: state.filtrering,
     veilederliste: state.veiledere.data.veilederListe,
-    statustall: state.statustall
+    statustall: state.statustall,
+    enhettiltak: state.enhettiltak
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    hentStatusTall: (enhet) => dispatch(hentStatusTall(enhet))
+    hentStatusTall: (enhet) => dispatch(hentStatusTall(enhet)),
+    hentEnhetTiltak: (enhet) => dispatch(hentEnhetTiltak(enhet))
 });
 
 export default injectIntl(connect(mapStateToProps, mapDispatchToProps)(EnhetSide));
