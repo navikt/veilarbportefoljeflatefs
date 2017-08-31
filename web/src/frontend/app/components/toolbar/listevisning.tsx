1f5f1d026d4c0b7e6658365259f17d72452d7c9a
Steffen Hageland <steffen.hageland@nav.no>
2017-08-31 14:28:50 +0200

    PK-44440 Flytter ut state på listevisning i redux, maks 5 valgt om gangen

--- web/src/frontend/app/components/toolbar/listevisning.tsx
+++ web/src/frontend/app/components/toolbar/listevisning.tsx
@@ -1,26 +1,32 @@
 import * as React from 'react';
-import {ChangeEvent, FormEvent, SyntheticEvent} from "react";
+import {connect} from 'react-redux';
+import {ChangeEvent} from "react";
 import Dropdown from "../dropdown/dropdown";
 import { Checkbox } from 'nav-frontend-skjema';
 import {FormattedMessage} from "react-intl";
+import {AppState} from "../../reducer";
+import {Action, Dispatch} from "redux";
+import {avvelgAlternativ, velgAlternativ} from "../../ducks/ui/listevisning";
 
 interface alternativ {
     value: string;
     tekstid: string;
+    allwaysDisabled?: boolean;
 }
 
 const alternativer: alternativ[] = [
-    {value: 'bruker', tekstid: 'listevisning.valg.bruker'},
-    {value: 'fodselsnr', tekstid: 'listevisning.valg.fodselsnr'},
+    {value: 'bruker', tekstid: 'listevisning.valg.bruker', allwaysDisabled: true},
+    {value: 'fodselsnr', tekstid: 'listevisning.valg.fodselsnr', allwaysDisabled: true},
     {value: 'veileder', tekstid: 'listevisning.valg.veileder'},
     {value: 'navident', tekstid: 'listevisning.valg.navident'},
     {value: 'ventersvar', tekstid: 'listevisning.valg.ventersvar'},
     {value: 'utlopytelse', tekstid: 'listevisning.valg.utlopytelse'},
-    {value: 'utlopaktivitet', tekstid: 'listevisning.valg.utlopaktivitet'},
+    {value: 'utlopaktivitet', tekstid: 'listevisning.valg.utlopaktivitet'}
 ];
 
 interface ListevisningRadProps extends alternativ {
-    disabled?: boolean;
+    disabled: boolean;
+    valgt: boolean;
     onChange: (name: string, checked: boolean) => void;
 }
 
@@ -29,31 +35,56 @@ const ListevisningRad = (props: ListevisningRadProps) => (
         <Checkbox
             label={<FormattedMessage id={props.tekstid} />}
             value={props.value}
-            disabled={props.disabled}
-            onChange={((e: ChangeEvent<HTMLInputElement>) => props.onChange(props.value, e.target.value === props.value))}
+            checked={props.valgt}
+            disabled={props.disabled || props.allwaysDisabled}
+            onChange={((e: ChangeEvent<HTMLInputElement>) => props.onChange(props.value, e.target.checked))}
         />
     </li>
 );
 
 interface ListevisningProps {
-
+    valgteAlternativ: string[];
+    dispatch: Dispatch<Action>;
 }
 
 const Listevisning = (props: ListevisningProps) => {
     function handleChange(name, checked) {
-        console.log("pow", name, checked);
+        if(checked) {
+            props.dispatch(velgAlternativ(name));
+        } else {
+            props.dispatch(avvelgAlternativ(name));
+        }
+    }
+
+    function erValgt(alternativ) {
+        return props.valgteAlternativ.some(a => a === alternativ.value);
     }
 
     return (
         <Dropdown name="Listevisning" className="dropdown--fixed dropdown--toolbar">
             <section className="radio-filterform__valg">
-                <FormattedMessage id="listevisning.ingress" />
+                <div className="blokk-s">
+                    <FormattedMessage id="listevisning.ingress" />
+                </div>
                 <ul className="ustilet">
-                    { alternativer.map(alternativ => <ListevisningRad {...alternativ} onChange={handleChange} />) }
+                    { alternativer.map(alternativ => (
+                        <ListevisningRad
+                            {...alternativ}
+                            valgt={erValgt(alternativ)}
+                            disabled={props.valgteAlternativ.length >= 5 && !erValgt(alternativ)}
+                            onChange={handleChange}
+                        />
+                    )) }
                 </ul>
             </section>
         </Dropdown>
     );
 };
 
-export default Listevisning;
+function mapStateToProps(state: AppState) {
+    return {
+        valgteAlternativ: state.ui.listevisning.valgte
+    }
+}
+
+export default connect(mapStateToProps)(Listevisning);
