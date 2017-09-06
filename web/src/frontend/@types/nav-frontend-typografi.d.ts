8923dfab6218b6afe238c56eb3df69271271fa5f
Peder Korsveien <peder.korsveien@nav.no>
2017-09-06 08:12:35 +0200

    Konverter overskrift-med-hjelpetekst til ts

--- web/src/frontend/@types/nav-frontend-typografi.d.ts
+++ web/src/frontend/@types/nav-frontend-typografi.d.ts
@@ -5,9 +5,14 @@ declare module "nav-frontend-typografi" {
 		className?: string;
 	}
 
+	export interface ElementProps {
+	    tag: String
+    }
+
 	export class Sidetittel extends React.Component<Props, {}> {}
 	export class Undertittel extends React.Component<Props, {}> {}
 	export class Normaltekst extends React.Component<Props, {}> {}
 	export class EtikettLiten extends React.Component<Props, {}> {}
 	export class UndertekstBold extends React.Component<Props, {}> {}
+	export class Element extends React.Component<ElementProps, {}> {}
 }
