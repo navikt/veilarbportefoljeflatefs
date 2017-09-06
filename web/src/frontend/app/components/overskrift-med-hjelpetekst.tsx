387f4b47dffc33e50f51a547577d119db9db82bf
Peder Korsveien <peder.korsveien@nav.no>
2017-09-06 08:12:35 +0200

    Konverter overskrift-med-hjelpetekst til ts

--- web/src/frontend/app/components/overskrift-med-hjelpetekst.tsx
+++ web/src/frontend/app/components/overskrift-med-hjelpetekst.tsx
@@ -1,23 +1,25 @@
-import React, { PropTypes as PT } from 'react';
+import * as React from 'react';
 
 import { FormattedMessage } from 'react-intl';
 import { Element } from 'nav-frontend-typografi';
 import { HjelpetekstAuto } from 'nav-frontend-hjelpetekst';
+import {StatelessComponent} from "react";
 
-const OverskriftMedHjelpeTekst = ({ overskriftId, hjelpetekstId }) => (
+interface OverskriftMedHjelpeTekstProps {
+    overskriftId: string,
+    hjelpetekstId: string
+    children?: React.ReactNode,
+}
+
+const OverskriftMedHjelpeTekst : StatelessComponent = ({ overskriftId, hjelpetekstId } : OverskriftMedHjelpeTekstProps) => (
     <div className="blokk-xxs filtrering--overskrift-med-hjelpetekst">
         <Element tag="h3">
             <FormattedMessage id={overskriftId} />
         </Element>
-        <HjelpetekstAuto size="16px">
+        <HjelpetekstAuto>
             <FormattedMessage id={hjelpetekstId} />
         </HjelpetekstAuto>
     </div>
 );
-
-OverskriftMedHjelpeTekst.propTypes = {
-    overskriftId: PT.string.isRequired,
-    hjelpetekstId: PT.string.isRequired
-};
 
 export default OverskriftMedHjelpeTekst;
