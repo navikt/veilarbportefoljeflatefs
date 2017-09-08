1827c45269e01961e0209d22fb060b0b3f58c541
S151399 <marthe.skogen@nav.no>
2017-09-08 14:37:42 +0200

    Styling på fjern-fra-arbeidsliste-form

--- web/src/frontend/app/modal/arbeidsliste-modal.js
+++ web/src/frontend/app/modal/arbeidsliste-modal.js
@@ -1,13 +1,13 @@
-import React, { Component, PropTypes as PT } from 'react';
-import { connect } from 'react-redux';
+import React, {Component, PropTypes as PT} from 'react';
+import {connect} from 'react-redux';
 import NavFrontendModal from 'nav-frontend-modal';
-import { Normaltekst, Innholdstittel } from 'nav-frontend-typografi';
-import { FormattedMessage } from 'react-intl';
-import { skjulModal } from '../ducks/modal';
-import { markerAlleBrukere } from '../ducks/portefolje';
+import {Normaltekst, Innholdstittel} from 'nav-frontend-typografi';
+import {FormattedMessage} from 'react-intl';
+import {skjulModal} from '../ducks/modal';
+import {markerAlleBrukere} from '../ducks/portefolje';
 import LeggTilArbeidslisteForm from './legg-til-arbeidsliste-form';
 import FjernFraArbeidslisteForm from './fjern-fra-arbeidsliste-form';
-import { brukerShape } from '../proptype-shapes';
+import {brukerShape} from '../proptype-shapes';
 
 NavFrontendModal.setAppElement('#applikasjon');
 
@@ -23,12 +23,12 @@ class ArbeidslisteModal extends Component {
 
     componentWillReceiveProps(nextProps) {
         if (nextProps.isOpen !== this.state.isOpen) {
-            this.setState({ isOpen: nextProps.isOpen });
+            this.setState({isOpen: nextProps.isOpen});
         }
     }
 
     lukkModal() {
-        this.setState({ isOpen: false });
+        this.setState({isOpen: false});
         this.props.skjulArbeidslisteModal();
         this.props.fjernMarkerteBrukere();
     }
@@ -43,20 +43,21 @@ class ArbeidslisteModal extends Component {
             </div>
         );
     }
+
     leggTilModal(valgteBrukere) {
         return (
             <div className="arbeidsliste__modal">
                 <div className="arbeidsliste-info-tekst">
-                <Innholdstittel tag="h1" className="blokk-xs">
-                    <FormattedMessage id="modal.legg.til.arbeidsliste.tittel" />
-                </Innholdstittel>
-                <Normaltekst className="blokk-s">
-                    <FormattedMessage
-                        id="modal.legg.til.arbeidsliste.infotekst"
-                        values={{ antall: valgteBrukere.length }}
-                    />
-                </Normaltekst>
-            </div>
+                    <Innholdstittel tag="h1" className="blokk-xs">
+                        <FormattedMessage id="modal.legg.til.arbeidsliste.tittel"/>
+                    </Innholdstittel>
+                    <Normaltekst className="blokk-s">
+                        <FormattedMessage
+                            id="modal.legg.til.arbeidsliste.infotekst"
+                            values={{antall: valgteBrukere.length}}
+                        />
+                    </Normaltekst>
+                </div>
                 <LeggTilArbeidslisteForm
                     valgteBrukere={valgteBrukere}
                     lukkModal={this.lukkModal}
@@ -71,15 +72,11 @@ class ArbeidslisteModal extends Component {
 
         return (
             <div className="arbeidsliste__modal">
-                <Innholdstittel tag="h1" className="blokk-xs">
-                    <FormattedMessage id="modal.fjern.fra.arbeidsliste.tittel" />
-                </Innholdstittel>
-                <Normaltekst className="blokk-s">
-                    <FormattedMessage
-                        id="modal.fjern.fra.arbeidsliste.infotekst"
-                        values={{ antall: brukereSomSkalFjernes.length }}
-                    />
-                </Normaltekst>
+                <div className="arbeidsliste-info-tekst">
+                    <Innholdstittel tag="h1" className="blokk-xs">
+                        <FormattedMessage id="modal.fjern.fra.arbeidsliste.tittel"/>
+                    </Innholdstittel>
+                </div>
                 <FjernFraArbeidslisteForm
                     valgteBrukere={brukereSomSkalFjernes}
                     lukkModal={this.lukkModal}
@@ -89,7 +86,7 @@ class ArbeidslisteModal extends Component {
     }
 
     render() {
-        const { valgteBrukere } = this.props;
+        const {valgteBrukere} = this.props;
         const fjerne = valgteBrukere.some((bruker) => bruker.arbeidsliste.arbeidslisteAktiv);
         return (
             <NavFrontendModal
@@ -101,7 +98,7 @@ class ArbeidslisteModal extends Component {
                 closeButton
             >
                 {this.leggTilHeader()}
-                { fjerne ? this.fjernFraModal(valgteBrukere) : this.leggTilModal(valgteBrukere) }
+                {fjerne ? this.fjernFraModal(valgteBrukere) : this.leggTilModal(valgteBrukere)}
             </NavFrontendModal>);
     }
 }
