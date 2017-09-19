ef14d613108a9ada41c5c65edb709995ac23c51d
Steffen Hageland <steffen.hageland@nav.no>
2017-09-19 12:17:13 +0200

    PK-44440 Utløp_aktivitet skal kun være mulig datokolonne om 1 aktivitet er valgt i filteret

--- web/src/frontend/app/ducks/ui/listevisning-selectors.ts
+++ web/src/frontend/app/ducks/ui/listevisning-selectors.ts
@@ -1,10 +1,12 @@
 import {AppState} from '../../reducer';
 import {Kolonne} from './listevisning';
-import {AktiviteterValg, FiltreringState} from '../filtrering';
+import {FiltreringAktiviteterValg, AktiviteterValg, FiltreringState} from '../filtrering';
 import {
+    aktiviteter,
     I_AVTALT_AKTIVITET, UTLOPTE_AKTIVITETER, VENTER_PA_SVAR_FRA_BRUKER,
     VENTER_PA_SVAR_FRA_NAV
 } from '../../filtrering/filter-konstanter';
+import {FiltervalgModell} from '../../model-interfaces';
 
 export function selectMuligeAlternativer(state: AppState): Kolonne[] {
     return state.ui.listevisning.mulige;
@@ -18,6 +20,16 @@ function addHvis(kolonne: Kolonne, add: boolean): Kolonne[] {
     return add ? [kolonne] : [];
 }
 
+function harValgtKunEnAktivitet(aktiviteter: FiltreringAktiviteterValg): boolean {
+    return Object.entries(aktiviteter)
+        .filter(([key, value]) => value === AktiviteterValg.JA)
+        .length === 1;
+}
+
+function harIkkeValgtTiltakstype(tiltakstyper: string[]): boolean {
+    return tiltakstyper.length === 0;
+}
+
 export function getMuligeKolonner(state: AppState): Kolonne[] {
     const filtervalg: FiltreringState = state.filtrering;
     return [Kolonne.BRUKER, Kolonne.FODSELSNR, Kolonne.VEILEDER, Kolonne.NAVIDENT]
@@ -25,6 +37,5 @@ export function getMuligeKolonner(state: AppState): Kolonne[] {
         .concat(addHvis(Kolonne.AVTALT_AKTIVITET, filtervalg.brukerstatus === I_AVTALT_AKTIVITET))
         .concat(addHvis(Kolonne.VENTER_SVAR, filtervalg.brukerstatus === VENTER_PA_SVAR_FRA_BRUKER || filtervalg.brukerstatus === VENTER_PA_SVAR_FRA_NAV))
         .concat(addHvis(Kolonne.UTLOP_YTELSE, filtervalg.ytelse !== null))
-        .concat(addHvis(Kolonne.UTLOP_AKTIVITET, Object.entries(filtervalg.aktiviteter)
-            .some(([key, value]) => value === AktiviteterValg.JA)));
+        .concat(addHvis(Kolonne.UTLOP_AKTIVITET, harValgtKunEnAktivitet(filtervalg.aktiviteter) && harIkkeValgtTiltakstype(filtervalg.tiltakstyper)));
 }
