3eb9acade62ecf4fe9ce63a4e9426613a9b5cb78
Steffen Hageland <steffen.hageland@nav.no>
2017-09-27 09:47:37 +0200

    Konverterer tabell til typescript

---
+++ web/src/frontend/app/components/tabell/etikett.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Tabelletiketter, {TabelletiketterTypes} from '../tabelletiketter/tabelletiketter';
+
+interface EtikettProps {
+    type: TabelletiketterTypes;
+    child: React.ReactChild;
+    skalVises: boolean;
+}
+
+function Etikett({ type, child, skalVises }: EtikettProps) {
+    if (!skalVises) {
+        return null;
+    }
+    return (
+        <Tabelletiketter type={type}>
+            {child}
+        </Tabelletiketter>
+    );
+}
+
+export default Etikett;
