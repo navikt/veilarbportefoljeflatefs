6edd6e13c80f120da07c2bdf591bd8f66f6a656e
Steffen Hageland <steffen.hageland@nav.no>
2017-10-17 16:54:46 +0200

    Legger på riktige headers på fetch-kallene til context-api

--- web/src/frontend/app/components/enhet-context/context-api.ts
+++ web/src/frontend/app/components/enhet-context/context-api.ts
@@ -1,11 +1,18 @@
 import { fetchToJson } from '../../ducks/utils';
 
+const MED_CREDENTIALS: RequestInit = {
+    credentials: "same-origin",
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
+
 interface AktivenhetModell {
     aktivEnhet: string;
 }
 
 export function hentAktivEnhet(): Promise<string> {
-    return fetchToJson<AktivenhetModell>(`/modiacontextholder/api/context/aktivenhet`)
+    return fetchToJson<AktivenhetModell>(`/modiacontextholder/api/context/aktivenhet`, MED_CREDENTIALS)
         .then((data) => {
                 return data.aktivEnhet;
             }
@@ -14,10 +21,8 @@ export function hentAktivEnhet(): Promise<string> {
 
 export function oppdaterAktivEnhet(enhet: string): Promise<Response> {
     return fetch(`/modiacontextholder/api/context`, {
+        ...MED_CREDENTIALS,
         method: 'post',
-        headers: {
-            'Content-Type': 'application/json'
-        },
         body: JSON.stringify({
             verdi: enhet,
             eventType: 'NY_AKTIV_ENHET'
