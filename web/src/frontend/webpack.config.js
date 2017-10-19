a9cd191c25996e03dc91da6c76ef6d796c61db74
Peder Korsveien <peder.korsveien@nav.no>
2017-10-19 12:42:29 +0200

    Bitteliten navneendring i webpack-config

--- web/src/frontend/webpack.config.js
+++ web/src/frontend/webpack.config.js
@@ -13,22 +13,22 @@ const PATHS_TO_CLEAN = [
     'js/head.min.js'
 ]
 
-const FILES_TO_CPY_DEV = [
+const FILES_TO_COPY_DEV = [
     { from: './index.dev.html', to: path.join(PATHS.WEBAPP, 'index.html') },
     { from: './head.min.js', to: path.join(PATHS.WEBAPP, 'js/') },
 ]
 
-const FILES_TO_CPY_PROD = [
+const FILES_TO_COPY_PROD = [
     { from: './index.html', to: path.join(PATHS.WEBAPP, 'index.html') },
 ]
 
 function plugins(isDev) {
-    const FILES_TO_CPY = isDev ? FILES_TO_CPY_DEV : FILES_TO_CPY_PROD
+    const FILES_TO_COPY = isDev ? FILES_TO_COPY_DEV : FILES_TO_COPY_PROD
 
     return [
         new ExtractTextPlugin('css/index.css'),
         new OptimizeCssAssetsPlugin(),
-        new CopyWebpackPlugin(FILES_TO_CPY),
+        new CopyWebpackPlugin(FILES_TO_COPY),
         new CleanWebpackPlugin(PATHS_TO_CLEAN, { root: PATHS.WEBAPP })
     ]
 }
