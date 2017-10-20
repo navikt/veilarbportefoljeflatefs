07dbde5649b3a4e8d1a532b716f3f23494cb9ad3
Johan Rusvik <johan.rusvik@nav.no>
2017-10-20 14:52:06 +0200

    Fjerner ubrukt kode

--- web/src/frontend/app/ducks/veilederpaginering.js
+++ web/src/frontend/app/ducks/veilederpaginering.js
@@ -1,26 +1,12 @@
 import { DEFAULT_PAGINERING_STORRELSE } from '../konstanter';
 import { TABELLVISNING } from '../minoversikt/minoversikt-konstanter';
-import { sorter } from '../utils/sortering';
 import { Sorteringsfelt, Sorteringsrekkefolge } from '../model-interfaces';
 
 // Actions
-export const SETT_LISTE = 'veilederpaginering/settliste/OK';
-export const SETT_SUBLISTE = 'veilederpaginering/settsubliste/OK';
-export const KLARER = 'veilederpaginering/klarer/OK';
-export const SETT_FRA_INDEKS_FOR_SUBLISTE = 'veilederpaginering/settfraindeksforsubliste/OK';
-export const SETT_NY_SORTERING = 'veilederpaginering/settNySortering/OK';
-export const SORTER_PAA_ETTERNAVN = 'veilederpaginering/sorterPaaEtternavn/OK';
-export const SORTER_PAA_PORTEFOLJESTORRELSE = 'veilederpaginering/sorterPaaPortefoljestorrelse/OK';
-export const SETT_VISNINGSMODUS = 'veilederpaginering/settvisningsmodus/OK';
-
-// Utils
-export function sorterPaaEtternavn(veiledere, rekkefolge) {
-    return veiledere.sort(sorter('etternavn', rekkefolge));
-}
-
-export function sorterPaaPortefoljestorrelse(veiledere, rekkefolge) {
-    return veiledere.sort(sorter('portefoljestorrelse', rekkefolge));
-}
+const SETT_LISTE = 'veilederpaginering/settliste/OK';
+const SETT_SUBLISTE = 'veilederpaginering/settsubliste/OK';
+const KLARER = 'veilederpaginering/klarer/OK';
+const SETT_VISNINGSMODUS = 'veilederpaginering/settvisningsmodus/OK';
 
 const initialState = {
     liste: [],
@@ -47,23 +33,6 @@ export default function reducer(state = initialState, action) {
             };
         case KLARER:
             return { ...state, liste: [], subListe: [], fraIndeksForSubListe: 0 };
-        case SETT_FRA_INDEKS_FOR_SUBLISTE:
-            return { ...state, fraIndeksForSubListe: action.fraIndeks };
-        case SETT_NY_SORTERING:
-            return { ...state, currentSortering: action.nySortering };
-        case SORTER_PAA_ETTERNAVN:
-            return {
-                ...state,
-                liste: sorterPaaEtternavn(state.liste, action.nySortering.rekkefolge),
-                currentSortering: action.nySortering
-            };
-        case SORTER_PAA_PORTEFOLJESTORRELSE:
-            return {
-                ...state,
-                liste: sorterPaaPortefoljestorrelse(state.liste, action.nySortering.rekkefolge),
-                currentSortering: action.nySortering
-            };
-
         case SETT_VISNINGSMODUS:
             return {
                 ...state,
@@ -96,24 +65,9 @@ export function settSubListeForPaginering(fraIndeks, antall) {
     };
 }
 
-export function sorterListePaaEtternavn(nySortering) {
-    return {
-        type: SORTER_PAA_ETTERNAVN,
-        nySortering
-    };
-}
-
-export function sorterListePaaPortefoljestorrelse(nySortering) {
-    return {
-        type: SORTER_PAA_PORTEFOLJESTORRELSE,
-        nySortering
-    };
-}
-
 export function settVisningsmodus(visningsmodus) {
     return {
         type: SETT_VISNINGSMODUS,
         visningsmodus
     };
 }
-
