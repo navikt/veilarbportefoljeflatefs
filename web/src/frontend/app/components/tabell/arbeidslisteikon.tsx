0ecfe6150223b17e3e0109add00dcda8272f38cf
Peder Korsveien <peder.korsveien@gmail.com>
2017-10-29 16:03:00 +0100

    Fjern tomme <span>-er i kolonnene til arbeidslisteikonet

--- web/src/frontend/app/components/tabell/arbeidslisteikon.tsx
+++ web/src/frontend/app/components/tabell/arbeidslisteikon.tsx
@@ -6,8 +6,8 @@ interface ArbeidslisteikonProps {
     skalVises: boolean;
 }
 
-const cls = (className?: string) => classNames('arbeidsliste--ikon', className);
+const cls = (className?: string) => className ? classNames('arbeidsliste--ikon', className) : 'arbeidsliste--ikon';
 
 export default ({ className, skalVises}: ArbeidslisteikonProps) => {
-    return <span className={ skalVises ? cls(className) : className}>&nbsp;</span>;
+    return <span className={ skalVises ? cls(className) : className}/>
 }
