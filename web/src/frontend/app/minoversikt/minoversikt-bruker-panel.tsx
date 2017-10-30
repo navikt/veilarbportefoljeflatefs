ded29b0e926e0dd764d043657f7cb7a92d1e57da
Peder Korsveien <peder.korsveien@gmail.com>
2017-10-30 10:24:25 +0100

    Trekk arbeidslistepanel ut i egen komponent

--- web/src/frontend/app/minoversikt/minoversikt-bruker-panel.tsx
+++ web/src/frontend/app/minoversikt/minoversikt-bruker-panel.tsx
@@ -1,17 +1,14 @@
 import * as React from 'react';
 import {MouseEvent} from 'react';
-
-import {FormattedMessage} from 'react-intl';
-import {UndertekstBold} from 'nav-frontend-typografi';
 import ArbeidslisteButton from '../components/tabell/arbeidslistebutton';
 import CheckBox from '../components/tabell/checkbox';
 import ArbeidslisteIkon from '../components/tabell/arbeidslisteikon';
 import Etiketter from '../components/tabell/etiketter';
 import {brukerShape, filtervalgShape} from './../proptype-shapes';
-import ArbeidslisteModalRediger from '../modal/arbeidsliste-modal-rediger';
 import {BrukerModell, FiltervalgModell} from '../model-interfaces';
 import Collapse from 'react-collapse';
 import MinOversiktKolonner from "./minoversikt-kolonner";
+import ArbeidslistePanel from "./minoversikt-arbeidslistepanel";
 
 interface MinOversiktBrukerPanelProps {
     bruker: BrukerModell;
@@ -57,45 +54,9 @@ class MinoversiktBrukerPanel extends React.Component<MinOversiktBrukerPanelProps
     }
 
     render() {
-        const {bruker, enhetId, filtervalg, innloggetVeileder, settMarkert, onClick} = this.props;
-        const sistEndretDato = new Date(bruker.arbeidsliste.endringstidspunkt);
-        const sistEndretAv = bruker.arbeidsliste.sistEndretAv.veilederId;
-        const arbeidslisteAktiv = bruker.arbeidsliste.arbeidslisteAktiv;
+        const {bruker, enhetId, filtervalg, innloggetVeileder, settMarkert} = this.props;
 
-        const arbeidslisteBody =
-            (<span className="flex">
-                    <span className="brukerliste__gutter-left brukerliste--min-width-3"/>
-                    <span className="brukerliste__innhold brukerliste__arbeidslisteinnhold flex--grow">
-                        <UndertekstBold>
-                            <FormattedMessage id="arbeidsliste.kommentar.header"/>
-                        </UndertekstBold>
-                        <p>{bruker.arbeidsliste.kommentar}</p>
-                        <p className="brukerliste__arbeidslisteinnhold-footer typo-undertekst">
-                            <FormattedMessage
-                                id="arbeidsliste.kommentar.footer"
-                                values={{
-                                    dato: sistEndretDato.toLocaleDateString(),
-                                    veileder: sistEndretAv
-                                }}
-                            />
-                            <button
-                                className="lenke lenke--frittstående arbeidsliste--rediger-lenke"
-                                onClick={this.redigerOnClickHandler}
-                            >
-                                <FormattedMessage id="arbeidsliste.kommentar.footer.knapp"/>
-                            </button>
-                            <ArbeidslisteModalRediger
-                                bruker={bruker}
-                                isOpen={this.state.redigerArbeidslisteModalIsOpen}
-                                lukkModal={this.lukkRedigerArbeidslisteModal}
-                                innloggetVeileder={innloggetVeileder}
-                                sistEndretDato={sistEndretDato}
-                                sistEndretAv={sistEndretAv}
-                            />
-                        </p>
-                    </span>
-                </span>
-            );
+        const arbeidslisteAktiv = bruker.arbeidsliste.arbeidslisteAktiv;
 
         return (
             <li className="brukerliste--border-bottom-thin">
@@ -111,12 +72,22 @@ class MinoversiktBrukerPanel extends React.Component<MinOversiktBrukerPanelProps
                         enhetId={enhetId}
                     />
                     <div className="brukerliste__gutter-right">
-                        <ArbeidslisteButton skalVises={arbeidslisteAktiv} apen={this.state.apen} onClick={this.handleArbeidslisteButtonClick} />
+                        <ArbeidslisteButton
+                            skalVises={arbeidslisteAktiv}
+                            apen={this.state.apen}
+                            onClick={this.handleArbeidslisteButtonClick}
+                        />
                         <Etiketter bruker={bruker}/>
                     </div>
                 </div>
                 <Collapse isOpened={this.state.apen}>
-                    <article className="brukerliste__arbeidslistepanel">{arbeidslisteBody}</article>
+                    <ArbeidslistePanel
+                        bruker={bruker}
+                        innloggetVeileder={innloggetVeileder}
+                        redigerArbeidslisteModalIsOpen={this.state.redigerArbeidslisteModalIsOpen}
+                        lukkRedigerArbeidslisteModal={this.lukkRedigerArbeidslisteModal}
+                        redigerOnClickHandler={this.redigerOnClickHandler}
+                    />
                 </Collapse>
             </li>
         );
