65057b2432aba8b9f4f0ec85cd62ca9b1e061d96
Johan Rusvik <johan.rusvik@nav.no>
2017-11-06 14:11:34 +0100

    Endrer alertstripe

--- web/src/frontend/app/components/enhet-context/context-feilmodal.tsx
+++ web/src/frontend/app/components/enhet-context/context-feilmodal.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 import NavFrontendModal from 'nav-frontend-modal';
 import { Innholdstittel } from 'nav-frontend-typografi';
-import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
+import { AlertStripeAdvarselSolid } from 'nav-frontend-alertstriper';
 import { Hovedknapp } from 'nav-frontend-knapper';
 
 interface ContextFeilmodalProps {
@@ -25,9 +25,9 @@ class ContextFeilmodal extends React.Component<ContextFeilmodalProps> {
                         <FormattedMessage id="nyenhet.feilmodal.overskrift" />
                     </Innholdstittel>
                     <div className="modal-content modal-test">
-                        <AlertStripeAdvarsel className="blokk-s">
+                        <AlertStripeAdvarselSolid className="blokk-s">
                             <FormattedMessage id="nyenhet.feilmodal.tekst" />
-                        </AlertStripeAdvarsel>
+                        </AlertStripeAdvarselSolid>
                     </div>
                     <div className="modal-footer" >
                         <Hovedknapp onClick={this.props.onClose}>
