fa2cfec9f33fe4dc53b8b5bd9d4a9b0df91e70c0
Oscar Eckbo <oscar.eckbo@nav.no>
2017-11-28 17:36:38 +0100

    [AAP2018-105] fjerner camel-casing da solr-feltene er lowercase

--- web/src/frontend/app/model-interfaces.ts
+++ web/src/frontend/app/model-interfaces.ts
@@ -13,7 +13,7 @@ export enum Sorteringsfelt {
     DAGPENGER_UTLOP_UKE = 'dagputlopuke',
     DAGPENGER_PERM_UTLOP_UKE = 'permutlopuke',
     AAP_MAXTID_UKE = 'aapmaxtiduke',
-    AAP_UNNTAK_UKE = 'aapUnntakDagerIgjen',
+    AAP_UNNTAK_UKE = 'aapunntakdagerigjen',
     ARBEIDSLISTE_FRIST = 'arbeidslistefrist',
     VENTER_PA_SVAR_FRA_NAV = 'venterpasvarfranav',
     VENTER_PA_SVAR_FRA_BRUKER = 'venterpasvarfrabruker',
