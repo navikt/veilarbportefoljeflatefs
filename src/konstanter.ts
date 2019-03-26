864166f754098cebecdb679f1bd66a56c5688fe3
Alexander Gård <alexander.gard@nav.no>
2019-03-26 10:48:01 +0100

    fads

--- src/konstanter.ts
+++ src/konstanter.ts
@@ -7,7 +7,7 @@ export const IKKE_SATT = 'ikke_satt';
 export const DEFAULT_PAGINERING_STORRELSE = 20;
 
 export const SPOR_OM_TILBAKEMELDING = 'portefolje.spor_om_tilbakemelding';
-
+// comment
 export const PORTEFOLJE_FEATURES = [
     SPOR_OM_TILBAKEMELDING
 ];
