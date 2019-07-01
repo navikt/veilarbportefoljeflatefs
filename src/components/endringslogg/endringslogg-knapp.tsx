449504e4103ca7c4e06c1a1165f525f8602434c0
Stian Horn Stensli <stian.hs@hotmail.com>
2019-07-01 14:59:47 +0200

    absolute on dropdown and deleted unused files

--- src/components/endringslogg/endringslogg-knapp.tsx
+++ src/components/endringslogg/endringslogg-knapp.tsx
@@ -5,22 +5,13 @@ import './endringslogg.less';
 export function EndringsloggKnapp(props) {
     const [open, setOpen] = useState(false);
     return (
-        <div className="endringslogg-container">
-
+        <div className="endringslogg-dropDown endringslogg-container">
             <div className="endringslogg-info">
             </div>
-            <Ekspanderbartpanel tittel="Klikk her for å åpne/lukke panelet" tittelProps="normaltekst" border>
+            <Ekspanderbartpanel tittel="Oppdateringer" tittelProps="normaltekst" border>
                 Panelet vil da ekspandere og vise innholdet.
             </Ekspanderbartpanel>
-
         </div>
     );
 }
-/*
-function classnameOpen(open: boolean): string {
-    if (open) {
-        return 'h2__lenke oversiktslenke typo-undertittel oversiktslenke--valgt';
-    }
-    return 'h2__lenke oversiktslenke typo-undertittel';
-}
-*/
+
