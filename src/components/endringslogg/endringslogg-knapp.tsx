6d50eb9e926a006e429ca29e5d516ae4c3928507
Michael Gundersen <michael.gundersen@nav.no>
2019-07-02 10:34:18 +0200

    Ny komponent, EndringsloggInnhold og noe endring i styling

--- src/components/endringslogg/endringslogg-knapp.tsx
+++ src/components/endringslogg/endringslogg-knapp.tsx
@@ -3,7 +3,30 @@ import Ekspanderbartpanel, { EkspanderbartpanelProps } from 'nav-frontend-ekspan
 import './endringslogg.less';
 import { Innholdstittel } from 'nav-frontend-typografi';
 import { FormattedMessage } from 'react-intl';
+import { UnmountClosed, Collapse, CollapseProps } from 'react-collapse';
 import NavFrontendChevron from 'nav-frontend-chevron';
+
+function EndringsloggInnhold(props) {
+    const [open, setOpen] = useState(false);
+    return (
+        <>
+        <div className="endringslogg-innhold">
+            <label className="label" style={{marginRight: '1em'}}>
+                <input className="input"
+                       type="checkbox"
+                       checked={open}
+                       onChange={({target: {checked}}) => setOpen(checked)} />
+            </label>
+            <h4>{props.innholdsOverskrift}</h4>
+        </div>
+        <Collapse isOpened={open}>
+            <div className="text">
+            {props.children}
+            </div>
+        </Collapse>
+        </>
+    );
+}
 
 export function EndringsloggKnapp(props) {
     const [open, setOpen] = useState(false);
@@ -12,17 +35,23 @@ export function EndringsloggKnapp(props) {
             <div className="endringslogg-info">
             </div>
             <Ekspanderbartpanel tittel="Oppdateringer" tittelProps="normaltekst" border>
-                <h4>
-                    <NavFrontendChevron type={'ned'}/>Min-CV er nå printbar
-                </h4>
-                <h4>
-                    <NavFrontendChevron type={'ned'}/>Coachmarks er lansert!
-                </h4>
-                <h4>
-                    <NavFrontendChevron type={'ned'}/>Nytt filter på 'har sykepenger'
-                </h4>
+               <EndringsloggInnhold innholdsOverskrift="NY ENDRING!">
+                       Dette er en test på hvordan innhold kan se ut! Det er flere nye endringer og det kommer
+                       mange flere om ikke så lenge.
+               </EndringsloggInnhold>
+                <EndringsloggInnhold innholdsOverskrift="Min-CV er nå printbar">
+                    Dette er en test på hvordan innhold kan se ut! Det er flere nye endringer og det kommer
+                    mange flere om ikke så lenge.
+                </EndringsloggInnhold>
+                <EndringsloggInnhold innholdsOverskrift="Coachmarks er lansert!">
+                    Dette er en test på hvordan innhold kan se ut! Det er flere nye endringer og det kommer
+                    mange flere om ikke så lenge.
+                </EndringsloggInnhold>
+                <EndringsloggInnhold innholdsOverskrift="Nytt filter på 'har sykepenger'">
+                    Dette er en test på hvordan innhold kan se ut! Det er flere nye endringer og det kommer
+                    mange flere om ikke så lenge.
+                </EndringsloggInnhold>
             </Ekspanderbartpanel>
         </div>
     );
 }
-
