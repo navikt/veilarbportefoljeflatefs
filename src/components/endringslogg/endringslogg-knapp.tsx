fa36ece03ae81ba84f78448cf93e49a2c0c6acaf
Michael Gundersen <michael.gundersen@nav.no>
2019-07-03 13:46:21 +0200

    Implementert slik at localstorage sjekkes og ikon oppdateres etter dette. Noe refaktorering

--- src/components/endringslogg/endringslogg-knapp.tsx
+++ src/components/endringslogg/endringslogg-knapp.tsx
@@ -1,58 +1,62 @@
-import { default as React, useState } from 'react';
+import { default as React, useEffect, useState } from 'react';
 import Ekspanderbartpanel, { EkspanderbartpanelBase, EkspanderbartpanelProps } from 'nav-frontend-ekspanderbartpanel';
 import './endringslogg.less';
 import { Innholdstittel, Element, Normaltekst, EtikettLiten } from 'nav-frontend-typografi';
-import { FormattedMessage } from 'react-intl';
-import { UnmountClosed, Collapse, CollapseProps } from 'react-collapse';
-import { ToggleKnapp } from 'nav-frontend-toggle';
 import classNames from 'classnames/dedupe';
 
 function EndringsloggInnhold(props) {
-    let linkTag: any
-    if(props.url !== undefined){
-        if(props.linkTekst !== undefined){
+    let linkTag: any;
+    if (props.url !== undefined) {
+        if (props.linkTekst !== undefined) {
             linkTag = <a href={props.url}>{props.linkTekst}</a>
-        }else{
+        } else {
             linkTag = <a href={props.url}>{props.url}</a>
         }
     }
-    return (
-        <div className="endringslogg-innhold">
-            <EtikettLiten>{props.dato}</EtikettLiten>
-            <div className="endringslogg-indent">
-                <Element> {props.innholdsOverskrift} </Element>
-                <Normaltekst> {props.innholdTekst} </Normaltekst>
-                {linkTag}
+        return (
+            <div className="endringslogg-innhold">
+                <hr style={{color: 'black'}}/>
+                <EtikettLiten>{props.dato}</EtikettLiten>
+                <div className="endringslogg-indent">
+                    <Element> {props.innholdsOverskrift} </Element>
+                    <Normaltekst> {props.innholdTekst} </Normaltekst>
+                    {linkTag}
+                </div>
             </div>
-        </div>
-    );
+        );
+}
+
+function getDatoStreng(dato: Date){
+    let dd = String(dato.getDate()).padStart(2, '0');
+    let mm = "jul"; // TODO: Endre dette til å ikke være hardkodet
+    let yyyy = dato.getFullYear();
+    return `${dd}. ${mm}. ${yyyy}`;
 }
 
 export function EndringsloggKnapp(props) {
-    const [nyeNotifikasjoner, setNyeNotifikasjoner] = useState(false);
-    const [open, setOpen] = useState(false);
-    let today = new Date();
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = "jul";
-    let yyyy = today.getFullYear();
-    let todayString = `${dd}. ${mm}. ${yyyy}`;
+    const versjonsnummer = '0.1.6';
+    const [nyeNotifikasjoner, setNyeNotifikasjoner] = useState(!harSettEndringsinlegg(versjonsnummer));
+    const datoStreng = getDatoStreng(new Date());
+    let navURL = 'https://design.nav.no';
+    let navURLTeskt = 'Teknisk Informasjon';
 
-    let navURL = "https://design.nav.no";
-    let navURLTeskt = "Teknisk Informasjon"
     return (
         <div className="endringslogg-dropDown endringslogg-container">
-            <EkspanderbartpanelBase heading={<div className={classNames({'endringslogg-info-ingen-notifikasjoner': !nyeNotifikasjoner,
+            <EkspanderbartpanelBase onClick={() => {
+                handleSettEndring(versjonsnummer);
+                setNyeNotifikasjoner(false);
+            }} heading={<div className={classNames({'endringslogg-info-ingen-notifikasjoner': !nyeNotifikasjoner,
                 'endringslogg-info-nye-notifikasjoner': nyeNotifikasjoner})}/>}>
-                <EndringsloggInnhold dato={todayString}
+                <EndringsloggInnhold dato={datoStreng}
                                      innholdsOverskrift="NY ENDRING!"
                                      innholdTekst="Dette er en test på hvordan innhold kan se ut! Det er flere nye endringer og det kommer mange flere om ikke så lenge."
                 />
-                <EndringsloggInnhold dato={todayString}
+                <EndringsloggInnhold dato={datoStreng}
                                      innholdsOverskrift="Min-CV er nå printbar"
                                      innholdTekst="Dette er en test på hvordan innhold kan se ut! Det er flere nye endringer og det kommer mange flere om ikke så lenge."
                 />
                 <EndringsloggInnhold
-                    dato={todayString}
+                    dato={datoStreng}
                     innholdsOverskrift="Coachmarks er lansert!"
                     innholdTekst="Dette er en test på hvordan innhold kan se ut! Det er flere nye endringer og det kommer
                     mange flere om ikke så lenge."
@@ -61,7 +65,7 @@ export function EndringsloggKnapp(props) {
                 />
 
                 <EndringsloggInnhold
-                    dato={todayString}
+                    dato={datoStreng}
                     innholdsOverskrift="Nytt filter på 'har sykepenger'"
                     innholdTekst="Dette er en test på hvordan innhold kan se ut! Det er flere nye endringer og det kommer
                     mange flere om ikke så lenge."
@@ -73,10 +77,11 @@ export function EndringsloggKnapp(props) {
 }
 
 const ENDRING_PREFIX = 'Endringslogg';
-function harSettEndringsinlegg(id: string) {
-    return window.localStorage.getItem(id) != null;
+function harSettEndringsinlegg(versjon: string) {
+    const senesteVersjonSett = window.localStorage.getItem(ENDRING_PREFIX);
+    return senesteVersjonSett != null && senesteVersjonSett === versjon;
 }
 
-function handleSettEndring(id) {
-    window.localStorage.setItem(id, 'true');
+function handleSettEndring(versjon) {
+    window.localStorage.setItem(ENDRING_PREFIX, versjon);
 }
