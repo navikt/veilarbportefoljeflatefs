9cb8a2b9e0e83bb131f0f886dff4f8083bec134f
Stian Horn Stensli <stian.hs@hotmail.com>
2019-07-03 15:53:21 +0200

    spacing og blå dot

--- src/components/endringslogg/endringslogg-knapp.tsx
+++ src/components/endringslogg/endringslogg-knapp.tsx
@@ -8,19 +8,24 @@ function EndringsloggInnhold(props) {
     let linkTag: any;
     if (props.url !== undefined) {
         if (props.linkTekst !== undefined) {
-            linkTag = <a href={props.url}>{props.linkTekst}</a>
+            linkTag = <><a href={props.url} target="_blank" >{props.linkTekst}</a><i> (Åpnes i ny fane)</i></>
         } else {
-            linkTag = <a href={props.url}>{props.url}</a>
+            linkTag = <><a href={props.url} target="_blank" >{props.url}</a><i> (Åpnes i ny fane)</i></>
         }
     }
         return (
-            <div className="endringslogg-innhold">
-                <hr style={{color: 'black'}}/>
-                <EtikettLiten>{props.dato}</EtikettLiten>
-                <div className="endringslogg-indent">
+            <div className="endringslogg-rad">
+                <div className={classNames({'endringslogg-info-ingen-notifikasjoner endringslogg-kolonne ': !props.nyeNotifikasjoner,
+                        'endringslogg-info-nye-notifikasjoner endringslogg-kolonne endringslogg-info-kolonne ': props.nyeNotifikasjoner})}/>
+                <div className="endringslogg-innhold endringslogg-kolonne">
+                    <EtikettLiten>{props.dato}</EtikettLiten>
+                    <div className="endringslogg-indent">
                     <Element> {props.innholdsOverskrift} </Element>
                     <Normaltekst> {props.innholdTekst} </Normaltekst>
+
                     {linkTag}
+                    </div>
+                    <hr style={{width: "-webkit-fill-available"}}/>
                 </div>
             </div>
         );
@@ -36,6 +41,7 @@ function getDatoStreng(dato: Date){
 export function EndringsloggKnapp(props) {
     const versjonsnummer = '0.1.6';
     const [nyeNotifikasjoner, setNyeNotifikasjoner] = useState(!harSettEndringsinlegg(versjonsnummer));
+    const [open, setOpen] = useState(false);
     const datoStreng = getDatoStreng(new Date());
     let navURL = 'https://design.nav.no';
     let navURLTeskt = 'Teknisk Informasjon';
@@ -43,23 +49,29 @@ export function EndringsloggKnapp(props) {
     return (
         <div className="endringslogg-dropDown endringslogg-container">
             <EkspanderbartpanelBase onClick={() => {
-                handleSettEndring(versjonsnummer);
-                setNyeNotifikasjoner(false);
-            }} heading={<div className={classNames({'endringslogg-info-ingen-notifikasjoner': !nyeNotifikasjoner,
-                'endringslogg-info-nye-notifikasjoner': nyeNotifikasjoner})}/>}>
+                if(open){
+                    handleSettEndring(versjonsnummer);
+                    setNyeNotifikasjoner(false);
+                }
+                setOpen(!open);
+            }} heading={<><div className={classNames({'endringslogg-info-ingen-notifikasjoner': !nyeNotifikasjoner,
+                'endringslogg-info-nye-notifikasjoner': nyeNotifikasjoner})}/><div className="endringslogg-title">Oppdateringer</div></>}>
                 <EndringsloggInnhold dato={datoStreng}
                                      innholdsOverskrift="NY ENDRING!"
                                      innholdTekst="Dette er en test på hvordan innhold kan se ut! Det er flere nye endringer og det kommer mange flere om ikke så lenge."
+                                     nyeNotifikasjoner={nyeNotifikasjoner}
                 />
                 <EndringsloggInnhold dato={datoStreng}
                                      innholdsOverskrift="Min-CV er nå printbar"
                                      innholdTekst="Dette er en test på hvordan innhold kan se ut! Det er flere nye endringer og det kommer mange flere om ikke så lenge."
+                                     nyeNotifikasjoner={nyeNotifikasjoner}
                 />
                 <EndringsloggInnhold
                     dato={datoStreng}
                     innholdsOverskrift="Coachmarks er lansert!"
                     innholdTekst="Dette er en test på hvordan innhold kan se ut! Det er flere nye endringer og det kommer
                     mange flere om ikke så lenge."
+                    nyeNotifikasjoner={false}
                     url={navURL}
                     linkTekst={navURLTeskt}
                 />
@@ -69,6 +81,7 @@ export function EndringsloggKnapp(props) {
                     innholdsOverskrift="Nytt filter på 'har sykepenger'"
                     innholdTekst="Dette er en test på hvordan innhold kan se ut! Det er flere nye endringer og det kommer
                     mange flere om ikke så lenge."
+                    nyeNotifikasjoner={false}
                     url={navURL}
                />
             </EkspanderbartpanelBase>
