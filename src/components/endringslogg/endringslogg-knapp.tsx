12fbe21520d5b5cf6f7384068a2b079ee24d2a50
Michael Gundersen <michael.gundersen@nav.no>
2019-07-05 10:46:23 +0200

    Merge-konflikt fikset. Typesatt filen noe mer

--- src/components/endringslogg/endringslogg-knapp.tsx
+++ src/components/endringslogg/endringslogg-knapp.tsx
@@ -1,41 +1,52 @@
-import { default as React, useEffect, useState} from 'react';
+import { default as React, useEffect, useState } from 'react';
 import Ekspanderbartpanel, { EkspanderbartpanelBase, EkspanderbartpanelProps } from 'nav-frontend-ekspanderbartpanel';
 import './endringslogg.less';
 import { Innholdstittel, Element, Normaltekst, EtikettLiten } from 'nav-frontend-typografi';
 import classNames from 'classnames/dedupe';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { UnmountClosed, Collapse, CollapseProps } from 'react-collapse';
 
-function EndringsloggInnhold(props) {
+interface EndringsloggInnholdProps {
+    url?: string;
+    linkTekst?: string;
+    dato: string;
+    innholdsTekst: string;
+    innholdsOverskrift: string;
+    nyeNotifikasjoner: boolean;
+}
+
+function EndringsloggInnhold(props: EndringsloggInnholdProps) {
     let linkTag: any;
     if (props.url !== undefined) {
         if (props.linkTekst !== undefined) {
-            linkTag = <><a href={props.url} target="_blank" >{props.linkTekst}</a><i> (Åpnes i ny fane)</i></>
+            linkTag = <><a href={props.url} target="_blank">{props.linkTekst}</a><i> (Åpnes i ny fane)</i></>;
         } else {
-            linkTag = <><a href={props.url} target="_blank" >{props.url}</a><i> (Åpnes i ny fane)</i></>
+            linkTag = <><a href={props.url} target="_blank">{props.url}</a><i> (Åpnes i ny fane)</i></>;
         }
     }
-        return (
-            <div className="endringslogg-rad">
-                <div className="endringslogg-skille">
-                    <div className={classNames({'endringslogg-info-ingen-notifikasjoner endringslogg-kolonne ': !props.nyeNotifikasjoner,
-                            'endringslogg-info-nye-notifikasjoner endringslogg-kolonne endringslogg-info-kolonne ': props.nyeNotifikasjoner})}/>
-                    <div className="endringslogg-innhold endringslogg-kolonne">
-                        <EtikettLiten>{props.dato}</EtikettLiten>
-                        <div className="endringslogg-indent">
+    return (
+        <div className="endringslogg-rad">
+            <div className="endringslogg-skille">
+                <div className={classNames({
+                    'endringslogg-info-ingen-notifikasjoner ': !props.nyeNotifikasjoner,
+                    'endringslogg-info-nye-notifikasjoner endringslogg-info-kolonne ': props.nyeNotifikasjoner
+                })}/>
+                <div className="endringslogg-innhold endringslogg-kolonne">
+                    <EtikettLiten>{props.dato}</EtikettLiten>
+                    <div className="endringslogg-indent">
                         <Element> {props.innholdsOverskrift} </Element>
-                        <Normaltekst> {props.innholdTekst} </Normaltekst>
-
+                        <Normaltekst> {props.innholdsTekst} </Normaltekst>
                         {linkTag}
-                        </div>
                     </div>
                 </div>
             </div>
-        );
+        </div>
+    );
 }
 
-function getDatoStreng(dato: Date){
+function getDatoStreng(dato: Date) {
     let dd = String(dato.getDate()).padStart(2, '0');
-    let mm = "jul"; // TODO: Endre dette til å ikke være hardkodet
+    let mm = 'jul'; // TODO: Endre dette til å ikke være hardkodet
     let yyyy = dato.getFullYear();
     return `${dd}. ${mm}. ${yyyy}`;
 }
@@ -51,40 +62,33 @@ export function EndringsloggKnapp(props) {
             <button className="endringslogg-dropDown endringslogg-container" onClick={() => setOpen(!open)}>
                 Oppdateringer
             </button>
-
             <TransitionGroup component={null}>
                 {open && (
-                <CSSTransition classNames="collapse-container" timeout={300}>
-                    <div className='collapse-container'>
-                        <div className={'collapse-header'}>
-                            Oppdateringer
+                    <CSSTransition classNames="collapse-container" timeout={300}>
+                        <div className='collapse-container'>
+                            <div className={'collapse-header'}>
+                                Oppdateringer
+                            </div>
+                            <EndringsloggInnhold dato={datoStreng}
+                                                 innholdsOverskrift="NY ENDRING!"
+                                                 innholdsTekst="Dette er en test på hvordan innhold kan se ut! Det er flere nye endringer og det kommer mange flere om ikke så lenge."
+                                                 nyeNotifikasjoner={nyeNotifikasjoner}
+                            />
+                            <EndringsloggInnhold dato={datoStreng}
+                                                 innholdsOverskrift="Min-CV er nå printbar"
+                                                 innholdsTekst="Dette er en test på hvordan innhold kan se ut! Det er flere nye endringer og det kommer mange flere om ikke så lenge."
+                                                 nyeNotifikasjoner={nyeNotifikasjoner}
+                            />
                         </div>
-                        <EndringsloggInnhold dato={datoStreng}
-                                            innholdsOverskrift="NY ENDRING!"
-                                            innholdTekst="Dette er en test på hvordan innhold kan se ut! Det er flere nye endringer og det kommer mange flere om ikke så lenge."
-                                            nyeNotifikasjoner={nyeNotifikasjoner}
-                        />
-                        <EndringsloggInnhold dato={datoStreng}
-                                            innholdsOverskrift="Min-CV er nå printbar"
-                                            innholdTekst="Dette er en test på hvordan innhold kan se ut! Det er flere nye endringer og det kommer mange flere om ikke så lenge."
-                                            nyeNotifikasjoner={nyeNotifikasjoner}
-                        />
-                    </div>
-                </CSSTransition>
+                    </CSSTransition>
                 )}
             </TransitionGroup>
         </>
     );
 }
 
-interface ContainerProps {
-    apen: boolean;
-    nyeNotifikasjoner: boolean;
-    datoStreng: string;
-}
-
-
 const ENDRING_PREFIX = 'Endringslogg';
+
 function harSettEndringsinlegg(versjon: string) {
     const senesteVersjonSett = window.localStorage.getItem(ENDRING_PREFIX);
     return senesteVersjonSett != null && senesteVersjonSett === versjon;
