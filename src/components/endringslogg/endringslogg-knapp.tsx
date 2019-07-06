3e4af54cf93e84fde4e2867c2baa5f17749b31d6
theagilepadawan <michael.nesodden@gmail.com>
2019-07-07 01:13:14 +0200

    FO-123: Klikk utenfor komponenten lukker den, endring i styling i henhold til mock-up og placeholder-ikon.

--- src/components/endringslogg/endringslogg-knapp.tsx
+++ src/components/endringslogg/endringslogg-knapp.tsx
@@ -1,5 +1,4 @@
-import { default as React, useEffect, useState } from 'react';
-import Ekspanderbartpanel, { EkspanderbartpanelBase, EkspanderbartpanelProps } from 'nav-frontend-ekspanderbartpanel';
+import { default as React, useEffect, useRef, useState } from 'react';
 import './endringslogg.less';
 import { Innholdstittel, Element, Normaltekst, EtikettLiten } from 'nav-frontend-typografi';
 import classNames from 'classnames/dedupe';
@@ -27,12 +26,14 @@ function EndringsloggInnhold(props: EndringsloggInnholdProps) {
     return (
         <div className="endringslogg-rad">
             <div className="endringslogg-skille">
+                <div className="endringslogg-datolinje">
                 <div className={classNames({
                     'endringslogg-info-ingen-notifikasjoner endringslogg-info-kolonne': !props.nyeNotifikasjoner,
                     'endringslogg-info-nye-notifikasjoner endringslogg-info-kolonne ': props.nyeNotifikasjoner
                 })}/>
-                <div className="endringslogg-innhold endringslogg-kolonne">
                     <EtikettLiten>{props.dato}</EtikettLiten>
+                </div>
+                <div className="endringslogg-innhold endringslogg-kolonne">
                     <div className="endringslogg-indent">
                         <Element> {props.innholdsOverskrift} </Element>
                         <Normaltekst> {props.innholdsTekst} </Normaltekst>
@@ -49,11 +50,35 @@ export function EndringsloggKnapp(props) {
     let nyeNotifikasjoner = !harSettEndringsinlegg(versjonsnummer);
     const [open, setOpen] = useState(false);
 
+    // Referranse til ytre div
+    const ytreNode = useRef(null);
+
+    const handleClickOutside = e => {
+        // @ts-ignore
+        if (ytreNode.current.contains(e.target)) {
+            // Klikket er inne i komponenten
+            return;
+        }
+        // Klikket er utenfor, oppdater staten
+        setOpen(false);
+    };
+
+    useEffect(() => {
+        if (open) {
+            document.addEventListener("mousedown", handleClickOutside);
+        } else {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [open]);
     return (
-        <>
-            <button className={`endringslogg-dropDown ${open && "endringslogg-dropDown-active"}`} onClick={
+        <div ref={ytreNode}>
+            <button className={`endringslogg-dropDown ${open && 'endringslogg-dropDown-active'}`} onClick={
                 () => {
-                    if(open){
+                    if(open) {
                         handleSettEndring(versjonsnummer);
                         nyeNotifikasjoner = false;
                     }
@@ -63,21 +88,21 @@ export function EndringsloggKnapp(props) {
             <TransitionGroup component={null}>
                 {open && (
                     <CSSTransition classNames="collapse-container" timeout={400}>
-                        <div className='collapse-container'>
+                        <div className="collapse-container">
                             <div className={'collapse-header'}>
                                 Oppdateringer
                             </div>
-                            <EndringsloggInnhold dato={"18. JUN. 2019"}
+                            <EndringsloggInnhold dato={'18. JUN. 2019'}
                                                  innholdsOverskrift="Laste ned og skrive ut CV"
                                                  innholdsTekst="Når du går inn på en bruker kan du nå laste ned CV-en under fanen «Detaljer». Da får du en bedre utskrift."
                                                  nyeNotifikasjoner={nyeNotifikasjoner}
                             />
-                            <EndringsloggInnhold dato={"06. JUN. 2019"}
+                            <EndringsloggInnhold dato={'06. JUN. 2019'}
                                                  innholdsOverskrift="Visning av profilering i Detaljer"
                                                  innholdsTekst="Nå kan du se profileringsresultatet fra brukerens registrering. Du finner det under «Registrering» i fanen «Detaljer» når du går inn på en bruker."
                                                  nyeNotifikasjoner={nyeNotifikasjoner}
                             />
-                            <EndringsloggInnhold dato={"29. MAR. 2019"}
+                            <EndringsloggInnhold dato={'29. MAR. 2019'}
                                                  innholdsOverskrift="Manuell registrering"
                                                  innholdsTekst="Ny løsning for å registrere brukere manuelt i Modia. Når du går inn på en bruker finner du det i Veilederverktøy (tannhjulet). Arena-oppgaven «Motta person» skal ikke lenger benyttes. "
                                                  nyeNotifikasjoner={nyeNotifikasjoner}
@@ -86,7 +111,7 @@ export function EndringsloggKnapp(props) {
                     </CSSTransition>
                 )}
             </TransitionGroup>
-        </>
+        </div>
     );
 }
 
