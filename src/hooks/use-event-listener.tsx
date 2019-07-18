aa892c18b6b43e4ffd4ac0da97db26e60489c2bd
Michael Gundersen <michael.gundersen@nav.no>
2019-07-18 16:08:05 +0200

    FO-123: Forrige bygg feilet pga missing whitespace, rettet opp dette

--- src/hooks/use-event-listener.tsx
+++ src/hooks/use-event-listener.tsx
@@ -3,7 +3,7 @@ import { DependencyList, useEffect } from 'react';
 export function useEventListener(eventName: string, handleEvent: (event?: any) => void, deps?: DependencyList) {
    useEffect(() => {
        const app = document.getElementById('applikasjon');
-       if(app){
+       if(app) {
         app.addEventListener(eventName, handleEvent, false);
         return () => {
             app.removeEventListener(eventName, handleEvent, false);
