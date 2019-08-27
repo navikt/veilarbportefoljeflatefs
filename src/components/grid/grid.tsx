8719f1dbd119950790558acd968e6c7c6d3e48e9
Tania Holst <tania.holst@nav.no>
2019-08-27 09:04:17 +0200

    FO-2448 Grid er nå funksjon, fjernet row og column og endret rekkefølge på alder

--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -6,15 +6,17 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
     gap?: string;
 }
 
-class Grid extends React.Component<Props> {
-    public render() {
-        const columns = new Array(this.props.columns)
+function Grid(props: Props) {
+/*
+ extends React.Component<Props> {
+*/
+        const columns = new Array(props.columns)
             .fill('1fr')
-            .join(this.props.gap ? ` ${this.props.gap} ` : ' ');
+            .join(props.gap ? ` ${props.gap} ` : ' ');
 
-        const rows = new Array(Math.ceil(React.Children.count(this.props.children) / this.props.columns))
+        const rows = new Array(Math.ceil(React.Children.count(props.children) / props.columns))
             .fill('auto')
-            .join(this.props.gap ? ` ${this.props.gap} ` : ' ');
+            .join(props.gap ? ` ${props.gap} ` : ' ');
 
         const style = {
             gridTemplateColumns: columns,
@@ -23,24 +25,20 @@ class Grid extends React.Component<Props> {
             msGridRows: rows
         };
 
-        const placedChildren = React.Children.map(this.props.children, (child: any, index: number) => {
-            const rawRow = Math.floor(index / this.props.columns) + 1;
-            const rawColumn = (index % this.props.columns) + 1;
+        const placedChildren = React.Children.map(props.children, (child: any, index: number) => {
+            const rawRow = Math.floor(index / props.columns) + 1;
+            const rawColumn = (index % props.columns) + 1;
 
-            const row = rawRow + (rawRow - 1);
-            const column = rawColumn + (rawColumn - 2);
-
-            const childStyle = { gridColumn: column, msGridColumn: column, gridRow: row, msGridRow: row };
+            const childStyle = { gridColumn: rawColumn, msGridColumn: rawColumn, gridRow: rawRow, msGridRow: rawColumn };
 
             return React.cloneElement(child, { style: Object.assign({}, child.props.style, childStyle) });
         });
 
         return (
-            <div className={`grid ${this.props.className ? this.props.className : ''}`} style={style}>
+            <div className={`grid ${props.className ? props.className : ''}`} style={style}>
                 {placedChildren}
             </div>
         );
-    }
 }
 
 export default Grid;
