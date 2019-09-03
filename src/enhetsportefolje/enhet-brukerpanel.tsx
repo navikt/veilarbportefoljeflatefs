b6ae56d9098ec7fe9b4b88d563f08676e32b0bbf
alexandraback <alexandra1.back@gmail.com>
2019-09-03 08:21:12 +0200

    KAIZEN-0: Refaktorering og scroll to

--- src/enhetsportefolje/enhet-brukerpanel.tsx
+++ src/enhetsportefolje/enhet-brukerpanel.tsx
@@ -1,29 +1,42 @@
 import * as React from 'react';
 import classNames from 'classnames';
 import Etiketter from '../components/tabell/etiketter';
-import { FiltervalgModell, VeilederModell } from '../model-interfaces';
+import { VeilederModell } from '../model-interfaces';
 import { Kolonne } from '../ducks/ui/listevisning';
 import CheckBox from '../components/tabell/checkbox';
 import EnhetKolonner from './enhet-kolonner';
+import { getFraBrukerFraUrl } from '../utils/url-utils';
+import { useLayoutEffect, useRef } from 'react';
+import { FiltreringState } from '../ducks/filtrering';
 
 interface EnhetBrukerpanelProps {
     bruker: any;
     settMarkert: (bruker: string, markert: boolean) => void;
     enhetId: string;
-    filtervalg: FiltervalgModell;
+    filtervalg: FiltreringState;
     brukersVeileder?: VeilederModell;
     valgteKolonner: Kolonne[];
     varForrigeBruker?: boolean;
 }
 
 function EnhetBrukerpanel({ bruker, settMarkert, enhetId, filtervalg, brukersVeileder, valgteKolonner, varForrigeBruker }: EnhetBrukerpanelProps) {
+    const liRef = useRef<HTMLLIElement>(null);
 
     const classname  = classNames('brukerliste__element brukerliste--border-bottom-thin', {
         'brukerliste--forrigeBruker': varForrigeBruker,
     });
 
+    const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop);
+
+    useLayoutEffect(() => {
+        const forrigeBruker = getFraBrukerFraUrl();
+        if (bruker.fnr === forrigeBruker) {
+            scrollToRef(liRef);
+        }
+    }, [liRef.current]);
+
     return (
-        <li className={classname}>
+        <li className={classname} ref={liRef}>
                 <div className="brukerliste__gutter-left brukerliste--min-width-enhet">
                     <CheckBox bruker={bruker} settMarkert={settMarkert} />
                 </div>
