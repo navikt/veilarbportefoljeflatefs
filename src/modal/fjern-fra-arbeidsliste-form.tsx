5509f24aeea6863dd8da72a572ebbb4b4bd92cc5
alexandraback <alexandra1.back@gmail.com>
2019-09-13 13:27:36 +0200

    KAIZEN-0: fiks fjernarbeidsliste prevent default

--- src/modal/fjern-fra-arbeidsliste-form.tsx
+++ src/modal/fjern-fra-arbeidsliste-form.tsx
@@ -30,7 +30,10 @@ interface FjernFraArbeidslisteFormProps {
 function FjernFraArbeidslisteForm({ lukkModal, valgteBrukere, onSubmit, slettFraArbeidslisteStatus }: FjernFraArbeidslisteFormProps) {
     const laster = slettFraArbeidslisteStatus !== undefined && slettFraArbeidslisteStatus !== STATUS.OK;
     return (
-        <form onSubmit={()=> onSubmit(valgteBrukere,lukkModal )}>
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            onSubmit(valgteBrukere, lukkModal);
+        }}>
             <div className="arbeidsliste-listetekst">
                 <ul>
                     {valgteBrukere.map((bruker) => brukerLabel(bruker))}
