d1a0f53c104c73e7114f49f343de9f000f1c1c98
Alexandra <alexandra1.back@gmail.com>
2019-10-29 15:14:03 +0100

    Fo 2530 (#110)
    * FO-2530: Fiks fokusfärg. Bump avhengigheter
    
    * FO-2530: Fiks advarsel-modaler
    
    * FO-2530: Fiks alertstriper

--- src/components/enhet-context/ny-context-modal.tsx
+++ src/components/enhet-context/ny-context-modal.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
-import NavFrontendModal from 'nav-frontend-modal';
 import { Innholdstittel, Normaltekst } from 'nav-frontend-typografi';
-import { AlertStripeInfoSolid } from 'nav-frontend-alertstriper';
 import Knapp, { Hovedknapp } from 'nav-frontend-knapper';
+import {AdvarselModal} from "../advarselmodal/advarsel-modal";
 
 interface NyContextModalProps {
     isOpen: boolean;
@@ -12,38 +11,36 @@ interface NyContextModalProps {
     aktivEnhet: string;
 }
 
-class NyContextModal extends React.Component<NyContextModalProps> {
-    render() {
-        return (
-            <NavFrontendModal
-                contentLabel="Brukercontext"
-                isOpen={this.props.isOpen}
-                closeButton={false}
-                onRequestClose={() => true}
-            >
-                <div className="brukercontext__modal">
-                    <Innholdstittel tag="h1" className="blokk-s">
-                        Du har endret Enhet
-                    </Innholdstittel>
-                    <AlertStripeInfoSolid className="blokk-s">
-                        Du har endret enhet i et annet vindu. Du kan ikke jobbe i 2 enheter samtidig.
-                        Velger du 'endre' mister du arbeidet du ikke har lagret.
-                    </AlertStripeInfoSolid>
-                    <Normaltekst className="blokk-s">
-                        {`Ønsker du å endre enhet til ${this.props.aktivEnhet}?`}
-                    </Normaltekst>
-                    <div className="modal-footer" >
-                        <Hovedknapp disabled={this.props.isPending} onClick={() => this.props.doEndreAktivEnhet()}>
-                            Endre
-                        </Hovedknapp>
-                        <Knapp onClick={this.props.doBeholdAktivEnhet} type="standard" spinner={this.props.isPending} autoDisableVedSpinner>
-                            Behold
-                        </Knapp>
-                    </div>
-                </div>
-            </NavFrontendModal>
-        );
-    }
+function NyContextModal ({isOpen, isPending, doBeholdAktivEnhet, doEndreAktivEnhet, aktivEnhet}: NyContextModalProps) {
+    return (
+        <AdvarselModal
+            contentLabel="Du har endret enhet advarsel"
+            isOpen={isOpen}
+            closeButton={false}
+            onRequestClose={() => true}
+            portalClassName="brukercontext-modal"
+            className="brukercontext-modal__content"
+        >
+            <Innholdstittel className="blokk-s">
+                Du har endret enhet
+            </Innholdstittel>
+            <Normaltekst className="blokk-s">
+                Du har endret enhet i et annet vindu. Du kan ikke jobbe i 2 enheter samtidig.
+                Velger du 'endre' mister du arbeidet du ikke har lagret.
+            </Normaltekst>
+            <Normaltekst className="blokk-s">
+                {`Ønsker du å endre enhet til ${aktivEnhet}?`}
+            </Normaltekst>
+            <div className="blokk-s knappe-rad">
+                <Hovedknapp disabled={isPending} onClick={doEndreAktivEnhet}>
+                    Endre
+                </Hovedknapp>
+                <Knapp onClick={doBeholdAktivEnhet} type="standard" spinner={isPending} autoDisableVedSpinner>
+                    Behold
+                </Knapp>
+            </div>
+        </AdvarselModal>
+    );
 }
 
 export default NyContextModal;
--- src/enhetsportefolje/velg-filter-melding.tsx
+++ src/enhetsportefolje/velg-filter-melding.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { AlertStripeInfoSolid } from 'nav-frontend-alertstriper';
+import Alertstripe from 'nav-frontend-alertstriper';
 
 function VelgFilterMelding() {
     return (
-
-        <AlertStripeInfoSolid className="blokk-m" >
-            <div className="listevisning--infopanel" aria-live="assertive" role="alert" aria-atomic="true">
+        <Alertstripe type="info" className="blokk-m" aria-live="assertive" role="alert" aria-atomic="true">
                 Du må gjøre en filtrering for å se brukere i listen.
-            </div>
-        </AlertStripeInfoSolid>
+        </Alertstripe>
     );
 }
 
