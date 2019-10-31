3d2342104369b127ad4019fd188645e97e3564d1
Tania Holst <tania.holst@nav.no>
2019-10-31 08:45:55 +0100

    FO-2370 endret dato-objekt til string og endret på mocken. la også til endringsloggen

--- src/components/endringslogg/utils/endringslogg-custom.tsx
+++ src/components/endringslogg/utils/endringslogg-custom.tsx
@@ -17,12 +17,12 @@ export interface EndringsloggInnleggMedSettStatus extends EndringsloggInnlegg {
 }
 
 const endringslogginnhold: EndringsloggInnlegg[] = [
-    // {
-    //     dato: '29. OKT. 2019',
-    //     tittel: 'Startdato for oppfølgingsperioden',
-    //     tekst: 'Oversikten har nå en kolonne som viser startdato for oppfølgingsperioden. Brukere registrert før aktivitetsplanen ble lansert 4. des. 2017 vises uten dato. Du kan sortere listen etter startdato.',
-    //     versjonId: '0.2.3',
-    // },
+    {
+        dato: '31. OKT. 2019',
+        tittel: 'Startdato for oppfølgingsperioden',
+        tekst: 'Oversikten har nå en kolonne som viser startdato for oppfølgingsperioden. Brukere registrert før aktivitetsplanen ble lansert 4. des. 2017 vises uten dato. Du kan sortere listen etter startdato.',
+        versjonId: '0.2.3',
+    },
     {
         dato: '22. OKT. 2019',
         tittel: 'Rydding i oversikten',
@@ -36,7 +36,7 @@ const endringslogginnhold: EndringsloggInnlegg[] = [
         dato: '26. SEP. 2019',
         tittel: 'Endring i arbeidslisten',
         tekst: 'Tittelfeltet i arbeidslisten er utvidet til 30 tegn. Kolonnetittelen i oversikten er endret fra "Kommentar" til "Tittel".',
-        versjonId: '0.2.1',
+        versjonId: '0.2.1'
     },
     {
         dato: '05. SEP. 2019',
