d986ce73acd7b0dd7d461e12cad4f2c432afce17
alexandraback <alexandra1.back@gmail.com>
2019-10-31 13:24:18 +0100

    KAIZEN-0: test att sätta env variabeln i app.json

--- src/alertstripeheroku/alertstripe-heroku.tsx
+++ src/alertstripeheroku/alertstripe-heroku.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { AlertStripeInfoSolid } from 'nav-frontend-alertstriper';
+import AlertStripe from 'nav-frontend-alertstriper';
 
 function AlertStripeHeroku() {
         return (
 
-            <AlertStripeInfoSolid>
+            <AlertStripe type="advarsel">
                 <div className="listevisning--infopanel" aria-live="assertive" role="alert" aria-atomic="true">
                     Dette er en testapplikasjon. Alle data er fiktive og ingen endring vil bli lagret.
                 </div>
-            </AlertStripeInfoSolid>
+            </AlertStripe>
         );
 }
 
