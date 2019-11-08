3160f56b42b357cffb50f37bd254ac3e6e33201b
Peder Korsveien <peder.korsveien@nav.no>
2019-11-08 10:26:03 +0100

    KZ-0 Oppdater feilmelding

--- src/innholdslaster/innholdslaster.tsx
+++ src/innholdslaster/innholdslaster.tsx
@@ -17,7 +17,7 @@ const medFeil = (avhengigheter) => avhengigheter.find(harStatus(STATUS.ERROR));
 function getFeilmeldingForReducer(feilendeReducer) {
     const status = feilendeReducer.data.response.status;
     if (status >= 500) {
-        return 'Beklager, systemet er nede. Prøv igjen senere.';
+        return 'Vi har dessverre tekniske problemer. Vi jobber med å løse disse, følg med på driftsmeldinger på Navet: https://navno.sharepoint.com/sites/intranett';
     } else if (status === 403) {
         return 'Beklager, du har ikke tilgang.';
     }
