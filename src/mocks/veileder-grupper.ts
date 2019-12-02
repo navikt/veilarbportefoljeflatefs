e4606d40650c62f3f67fea48d39e352814e5ba87
alexandraback <alexandra1.back@gmail.com>
2019-12-02 12:21:49 +0100

    KAIZEN-0: fjern obrukte variabler

--- src/mocks/veileder-grupper.ts
+++ src/mocks/veileder-grupper.ts
@@ -7,30 +7,12 @@ export const veilederGrupper = (veiledere: VeilederModell []) => {
     const veilederGruppe2 = veiledere.slice(5, 10).map((v) => v.ident);
     const veilederGruppe3 = veiledere.slice(11, 15).map((v) => v.ident);
     const veilederGruppe4 = veiledere.slice(16, 22).map((v) => v.ident);
-    const veilederGruppe5 = veiledere.slice(23, 24).map((v) => v.ident);
-    const veilederGruppe6 = veiledere.slice(25, 28).map((v) => v.ident);
-    const veilederGruppe7 = veiledere.slice(29, 31).map((v) => v.ident);
-    const veilederGruppe8 = veiledere.slice(32, 33).map((v) => v.ident);
-    const veilederGruppe9 = veiledere.slice(3, 12).map((v) => v.ident);
-    const veilederGruppe10 = veiledere.slice(5, 9).map((v) => v.ident);
-    const veilederGruppe11 = veiledere.slice(11, 15).map((v) => v.ident);
-    const veilederGruppe12 = veiledere.slice(1, 6).map((v) => v.ident);
-    const veilederGruppe13 = veiledere.slice(12, 16).map((v) => v.ident);
     return (
         [
             {filterNavn: 'Fantastic Four', filterId: 12, filterValg: {...initialState, veiledere: veilederGruppe1}},
             {filterNavn: 'Prinsess Gruppen', filterId: 13, filterValg: {...initialState, veiledere: veilederGruppe2}},
             {filterNavn: 'Hallo', filterId: 14, filterValg: {...initialState, veiledere: veilederGruppe3}},
             {filterNavn: 'På do', filterId: 15, filterValg: {...initialState, veiledere: veilederGruppe4}},
-            // {filterNavn: 'Et team som har et veldig, veldig, veldig, veldig, veldig, veldig langt navn', filterId: 16, filterValg: {...initialState, veiledere: veilederGruppe5}},
-            // {filterNavn: 'Prøver å finne maks an', filterId: 17, filterValg: {...initialState, veiledere: veilederGruppe6}},
-            // {filterNavn: 'Prinsess Gruppen', filterId: 18, filterValg: {...initialState, veiledere: veilederGruppe7}},
-            // {filterNavn: 'Prinsess Gruppen', filterId: 19, filterValg: {...initialState, veiledere: veilederGruppe8}},
-            // {filterNavn: 'Prinsess Gruppen', filterId: 20, filterValg: {...initialState, veiledere: veilederGruppe9}},
-            // {filterNavn: 'Prinsess Gruppen', filterId: 21, filterValg: {...initialState, veiledere: veilederGruppe10}},
-            // {filterNavn: 'Prinsess Gruppen', filterId: 22, filterValg: {...initialState, veiledere: veilederGruppe11}},
-            // {filterNavn: 'Prinsess Gruppen', filterId: 23, filterValg: {...initialState, veiledere: veilederGruppe12}},
-            // {filterNavn: 'Prinsess Gruppen', filterId: 24, filterValg: {...initialState, veiledere: veilederGruppe13}}
 
         ] as LagretFilter []
     );
