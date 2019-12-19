c3ce94fd9b431c4a6c1b96ca217165b124a34d28
Tania Holst <tania.holst@nav.no>
2019-12-19 15:27:53 +0100

    KAIZEN-0 la til filer

---
+++ src/components/modal/feilmelding-tildeling-modal.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
+import { VarselModal, VarselModalType } from '../varselmodal/varselmodal';
+import { Fnr, FnrList } from '../fnr-list';
+
+interface FeilmeldingBrukereModalProps {
+    isOpen: boolean;
+    fnrFeil: Fnr[];
+    fnrSuksess: Fnr[];
+    onClose: () => void;
+}
+
+function FeilmeldingTildelingModal(props: FeilmeldingBrukereModalProps) {
+    return (
+        <VarselModal
+            contentLabel="Modal tildeling feilet"
+            isOpen={props.isOpen}
+            onRequestClose={props.onClose}
+            closeButton={false}
+            type={VarselModalType.FEIL}
+            portalClassName="feiletbrukere-modal"
+            className="feiletbrukere-modal__content"
+        >
+            <Undertittel tag="h1" className="blokk-xxs">
+                Handling kan ikke utføres
+            </Undertittel>
+            <Normaltekst className="blokk-s">
+                Tildeling av veileder til følgende bruker feilet:
+            </Normaltekst>
+            <FnrList listeMedFnr={props.fnrFeil}/>
+            <Normaltekst className="blokk-s">
+                Det kan skyldes manglende tilgang på bruker, eller at veilederen allerede er tildelt brukeren.
+            </Normaltekst>
+            {props.fnrSuksess.length > 0 &&
+            <>
+                <Undertittel tag="h1" className="blokk-xxs">
+                    Handling utført
+                </Undertittel>
+
+                <FnrList listeMedFnr={props.fnrSuksess}/>
+                <Normaltekst className="blokk-s">
+                    Det kan ta noe tid før oversikten blir oppdatert med tildelt veileder
+                </Normaltekst>
+            </>
+
+            }
+            <button className="knapp knapp--hoved" onClick={props.onClose}>
+                Ok
+            </button>
+        </VarselModal>
+    );
+}
+
+export default FeilmeldingTildelingModal;
