4482df04ee5c97cdbc3fb05641038206efbf9e90
Tania Holst <tania.holst@nav.no>
2020-01-08 12:36:04 +0100

    FO-2675 la til border rundt ekspanderbart panel

--- src/components/toolbar/metrikk-ekspanderbartpanel.tsx
+++ src/components/toolbar/metrikk-ekspanderbartpanel.tsx
@@ -32,6 +32,7 @@ function MetrikkEkspanderbartpanel(props: PropsWithChildren<AllProps>) {
     return (
         <div className="blokk-xxs portefolje__ekspanderbarpanel">
             <Ekspanderbartpanel
+                border={true}
                 onClick={handleOnClick}
                 {...rest}
             >
