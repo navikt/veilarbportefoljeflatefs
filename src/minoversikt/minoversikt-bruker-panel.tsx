ccad69f492e4894d878e8cac99c497a65de9ea2e
Tania Holst <tania.holst@nav.no>
2020-01-09 10:28:13 +0100

    FO-2676 min oversikt fikset

--- src/minoversikt/minoversikt-bruker-panel.tsx
+++ src/minoversikt/minoversikt-bruker-panel.tsx
@@ -70,7 +70,7 @@ function MinoversiktBrukerPanel(props: MinOversiktBrukerPanelProps) {
                         apen={apen}
                         onClick={handleArbeidslisteButtonClick}
                     />
-                    <div className={"brukerliste__etiketter"}>
+                    <div className="brukerliste__etiketter">
                         <Etiketter bruker={bruker}/>
                         <Etikett
                             type={EtikettType.NYBRUKER}
