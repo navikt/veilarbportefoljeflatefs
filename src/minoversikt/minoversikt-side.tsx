6120178a0ff421232f15257591c6277b3f6e7c0a
Tania Holst <tania.holst@nav.no>
2020-01-10 11:47:36 +0100

    KAIZEN-0 fjernet ubrukt

--- src/minoversikt/minoversikt-side.tsx
+++ src/minoversikt/minoversikt-side.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import DocumentTitle from 'react-document-title';
 import { Normaltekst } from 'nav-frontend-typografi';
 import Innholdslaster from './../innholdslaster/innholdslaster';
