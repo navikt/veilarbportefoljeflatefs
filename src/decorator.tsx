328c385a05dbaac1bf0664e904021ed25c422f20
alexandraback <alexandra1.back@gmail.com>
2020-01-28 13:23:50 +0100

    KAIZEN-0: Nullstil aktivbrukere i context

--- src/decorator.tsx
+++ src/decorator.tsx
@@ -1,22 +1,23 @@
-import React, { useCallback } from 'react';
+import React, {useCallback, useState} from 'react';
 import NAVSPA from "@navikt/navspa";
 import { DecoratorProps } from "./utils/types/decorator-props";
 import { useDispatch } from "react-redux";
 import {OrNothing} from "./utils/types/types";
 import {oppdaterValgtEnhet} from "./ducks/valgt-enhet";
 import {useEnhetSelector} from "./hooks/redux/use-enhet-selector";
+import {useOnMount} from "./hooks/use-on-mount";
 
 
 const InternflateDecorator = NAVSPA.importer<DecoratorProps>('internarbeidsflatefs');
 
 function getConfig (
-    initielEnhet: OrNothing<string>,
-    settValgtEnhet: (enhet) => void
+    enhet: OrNothing<string>,
+    settValgtEnhet: (enhet) => void,
 ): DecoratorProps {
     return {
         appname: 'Arbeidsrettet oppfølging',
         fnr: null,
-        enhet: initielEnhet,
+        enhet,
         toggles: {
             visEnhet: false,
             visEnhetVelger: true,
@@ -32,15 +33,33 @@ function getConfig (
     }
 }
 
+function useNullStillContextholder() {
+    const [klar, setKlar] = useState(false);
+    useOnMount(() => {
+            // Manuell nullstilling av bruker i context
+            fetch('/modiacontextholder/api/context/aktivbruker', {
+                method: 'DELETE',
+                credentials: 'include'
+            }).then(() => setKlar(true));
+        });
+
+    return klar;
+}
+
 export function Decorator() {
     const dispatch = useDispatch();
     const enhetId = useEnhetSelector();
+    const klar = useNullStillContextholder();
 
-    function velgEnhet(enhet) {
+    function velgEnhet(enhet: string) {
         dispatch(oppdaterValgtEnhet(enhet));
     }
 
     const config = useCallback(getConfig, [enhetId, velgEnhet])(enhetId, velgEnhet);
+
+    if(!klar) {
+        return null;
+    }
 
     return (
         <InternflateDecorator {...config}/>
