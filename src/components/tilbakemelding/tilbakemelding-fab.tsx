731592b831b4858232606b10b6486fbd6221a8ff
alexandraback <alexandra1.back@gmail.com>
2020-02-10 14:20:47 +0100

    KAIZEN-0: Tilbakemelding dialog

--- src/components/tilbakemelding/tilbakemelding-fab.tsx
+++ src/components/tilbakemelding/tilbakemelding-fab.tsx
@@ -22,7 +22,7 @@ interface TilbakemeldingFabState {
 class TilbakemeldingFab extends React.Component<StateProps, TilbakemeldingFabState> {
 
     private readonly TILBAKEMELDING_PREFIX = 'har_sendt_tilbakemelding';
-    private readonly TILBAKEMELDING_FEATURE_TAG = 'veiledergrupper'; // NB: Husk å endre for hver nye feature
+    private readonly TILBAKEMELDING_FEATURE_TAG = 'dialog_tilfredshet'; // NB: Husk å endre for hver nye feature
 
     private wrapperRef;
 
