128f75695971d5e11a31197f286a33cd8ab7d8fc
Tania Holst <tania.holst@nav.no>
2020-02-25 15:40:08 +0100

    FO-2698 la inn noe greier jeg trenger hjelp av alex til å fikse

--- src/components/tabell/arbeidslisteikon.tsx
+++ src/components/tabell/arbeidslisteikon.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import classNames from 'classnames';
-import './tabell.less';
 import '../../minoversikt/minoversikt.less';
+import { ReactComponent as ArbeidslisteikonBla } from '../modal/arbeidsliste/arbeidslisteikon/arbeidslisteikon_bla.svg';
+// import { ReactComponent as ArbeidslisteikonRod } from '../modal/arbeidsliste/arbeidslisteikon/arbeidslisteikon_rod.svg';
+// import { ReactComponent as ArbeidslisteikonGronn } from '../modal/arbeidsliste/arbeidslisteikon/arbeidslisteikon_gronn.svg';
+// import { ReactComponent as ArbeidslisteikonGul } from '../modal/arbeidsliste/arbeidslisteikon/arbeidslisteikon_gul.svg';
 
 interface ArbeidslisteikonProps {
     className?: string;
@@ -11,5 +14,30 @@ interface ArbeidslisteikonProps {
 const cls = (className?: string) => className ? classNames('arbeidsliste--ikon', className) : 'arbeidsliste--ikon';
 
 export default ({className, skalVises}: ArbeidslisteikonProps) => {
-    return <span className={skalVises ? cls(className) : className}/>;
+
+    //hvis value fra radiobutton er det samme som value her, så skal de forskjellige ikonene vises.
+    // const value =
+    // const velgArbeidslisteIkon = () => {
+    //     if (skalVises) {
+    //         switch (value) {
+    //             case 'BLA':
+    //                 return <ArbeidslisteikonBla/>;
+    //             case 'ROD':
+    //                 return <ArbeidslisteikonRod/>;
+    //             case 'GRONN':
+    //                 return <ArbeidslisteikonGronn/>;
+    //             case 'GUL':
+    //                 return <ArbeidslisteikonGul/>;
+    //         }
+    //     }
+    // };
+
+    const visIkon = skalVises ? <ArbeidslisteikonBla/> : null;
+
+    return <span className={skalVises ? cls(className) : className}>
+
+        {visIkon}
+        {/*{velgArbeidslisteIkon}*/}
+
+        </span>;
 };
