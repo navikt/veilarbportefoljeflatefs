a2f90eb51c1a668564fce96feb36bb397c08b221
alexandraback <alexandra1.back@gmail.com>
2020-03-03 15:23:35 +0100

    KAIZEN-0: Legg tilbake reset

--- src/decorator.tsx
+++ src/decorator.tsx
@@ -1,9 +1,10 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useState} from 'react';
 import NAVSPA from "@navikt/navspa";
 import {DecoratorProps, EnhetDisplay, FnrDisplay} from "./utils/types/decorator-props";
 import {useDispatch} from "react-redux";
 import {oppdaterValgtEnhet} from "./ducks/valgt-enhet";
 import {useEnhetSelector} from "./hooks/redux/use-enhet-selector";
+import {useOnMount} from "./hooks/use-on-mount";
 
 const RESET_VALUE = '\u0000';
 const InternflateDecorator = NAVSPA.importer<DecoratorProps>('internarbeidsflatefs');
@@ -15,7 +16,7 @@ function getConfig (
     return {
         appname: 'Arbeidsrettet oppfølging',
         fnr: {
-            initialValue: RESET_VALUE,
+            initialValue: null,
             display: FnrDisplay.SOKEFELT,
             ignoreWsEvents: true,
             onChange: (value) => {
@@ -39,15 +40,33 @@ function getConfig (
     }
 }
 
+function useNullStillContextholder() {
+    const [klar, setKlar] = useState(false);
+    useOnMount(() => {
+        // Manuell nullstilling av bruker i context
+        fetch('/modiacontextholder/api/context/aktivbruker', {
+            method: 'DELETE',
+            credentials: 'include'
+        }).then(() => setKlar(true));
+    });
+
+    return klar;
+}
+
+
 export function Decorator() {
     const dispatch = useDispatch();
     const enhetId = useEnhetSelector();
-
+    const klar = useNullStillContextholder();
     function velgEnhet(enhet: string) {
         dispatch(oppdaterValgtEnhet(enhet));
     }
 
     const config = useCallback(getConfig, [enhetId, velgEnhet])(enhetId, velgEnhet);
+
+    if(!klar) {
+        return null;
+    }
 
     return (
         <InternflateDecorator {...config}/>
