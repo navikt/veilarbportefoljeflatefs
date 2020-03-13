1c2331b3534e076c198a5c0a4918604a5bce6ff2
Tania Holst <tania.holst@nav.no>
2020-03-13 15:00:58 +0100

    FO-2797 endringsloggmelding lagt til i dev

--- src/components/endringslogg/utils/endringslogg-custom.tsx
+++ src/components/endringslogg/utils/endringslogg-custom.tsx
@@ -28,6 +28,12 @@ const endringslogginnhold: EndringsloggInnlegg[] = [
                 brukeren til arbeidslisten første gang, eller ved å redigere eksisterende notat.</>
     },
     {
+        dato: '13. MAR. 2020',
+        tittel: 'Endre møteform på et avtalt møte',
+        versjonId: '13.03.20',
+        tekst: 'På aktiviteten "møte" i aktivitetsplanen kan du nå endre møteform, selv om møtet er satt til «Avtalt med NAV». Husk å sende en dialogmelding til brukeren om endringen du gjør.'
+    },
+    {
         dato: '05. MAR. 2020',
         tittel: 'Nytt design på dialog',
         versjonId: '05.03.20',
