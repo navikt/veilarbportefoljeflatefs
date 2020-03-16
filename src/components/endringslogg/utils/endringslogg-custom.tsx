35016a40240bb684d48939c00858f5acca2a4501
Tania Holst <tania.holst@nav.no>
2020-03-16 14:33:24 +0100

    FO-2698 fikset alt-tekst og endringslogg

--- src/components/endringslogg/utils/endringslogg-custom.tsx
+++ src/components/endringslogg/utils/endringslogg-custom.tsx
@@ -22,10 +22,10 @@ const endringslogginnhold: EndringsloggInnlegg[] = [
         dato: '16. MAR. 2020',
         tittel: 'Velg farge på arbeidslisteikonet',
         versjonId: '16.03.20',
-        tekst: 'Du kan nå merke notatene i arbeidslisten med fire ulike farger. Det kan være nyttig for å organisere og prioritere arbeidslisten. Du kan sortere listen etter farge.',
+        tekst: 'Du kan nå merke kommentaren i arbeidslisten med fire ulike farger. Det kan være nyttig for å organisere og prioritere arbeidslisten. Du kan sortere listen etter farge.',
         children:
-            <>Standardfargen på ikonet er blå, men du kan endre dette til lilla, grønn eller gul når du legger
-                brukeren til arbeidslisten første gang, eller ved å redigere eksisterende notat.</>
+            <>Standardfargen på ikonet er blå, men du kan endre dette til lilla, grønn eller gul når du legger brukeren
+                til arbeidslisten første gang, eller ved å redigere eksisterende kommenatar.</>
     },
     {
         dato: '13. MAR. 2020',
