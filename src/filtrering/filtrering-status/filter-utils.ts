992d087f5de1a20b02f2c09eeb1a6e12fef53acd
Tania Holst <tania.holst@nav.no>
2020-03-26 14:00:07 +0100

    FO-2804 en helt sjukt bra endring

--- src/filtrering/filtrering-status/filter-utils.ts
+++ src/filtrering/filtrering-status/filter-utils.ts
@@ -5,15 +5,12 @@ import { IKKE_PERMITTERTE_ETTER_NIENDE_MARS, PERMITTERTE_ETTER_NIENDE_MARS } fro
 const CHECKBOX_FILTER = ['UFORDELTE_BRUKERE', 'NYE_BRUKERE_FOR_VEILEDER', 'PERMITTERTE_ETTER_NIENDE_MARS', 'IKKE_PERMITTERTE_ETTER_NIENDE_MARS'];
 
 export function leggTilFerdigFilter(filterListe: string[], filter: string): string[] {
+    const newList = (filter) => filterListe.filter(v => v !== filter);
     if (filter === IKKE_PERMITTERTE_ETTER_NIENDE_MARS && filterListe.includes(PERMITTERTE_ETTER_NIENDE_MARS)) {
-        const newList = filterListe.filter(v => v !== PERMITTERTE_ETTER_NIENDE_MARS);
-        return [...newList, filter];
+        return [...newList(PERMITTERTE_ETTER_NIENDE_MARS), filter];
     } else if (filter === PERMITTERTE_ETTER_NIENDE_MARS && filterListe.includes(IKKE_PERMITTERTE_ETTER_NIENDE_MARS)) {
-        const newList = filterListe.filter(v => v !== IKKE_PERMITTERTE_ETTER_NIENDE_MARS);
-        return [...newList, filter];
-    }
-
-    else if (CHECKBOX_FILTER.includes(filter)) {
+        return [...newList(IKKE_PERMITTERTE_ETTER_NIENDE_MARS), filter];
+    } else if (CHECKBOX_FILTER.includes(filter)) {
         return [...filterListe, filter];
     } else if (!filterListe.includes(filter)) {
         const checkboxFilter = filterListe
