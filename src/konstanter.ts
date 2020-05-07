65129afdc54a2136aaab2a68c49f4b389dcca776
alexandraback <alexandra1.back@gmail.com>
2020-05-07 12:47:30 +0200

    KAIZEN-0: Bytt featurenavn

--- src/konstanter.ts
+++ src/konstanter.ts
@@ -7,7 +7,7 @@ export const IKKE_SATT = 'ikke_satt';
 export const DEFAULT_PAGINERING_STORRELSE = 20;
 
 export const SPOR_OM_TILBAKEMELDING = 'portefolje.spor_om_tilbakemelding';
-export const VEDTAKSTOTTE = 'veilarbportfolje-hent-data-fra-vedtakstotte';
+export const VEDTAKSTOTTE = 'pto.vedtaksstotte.pilot';
 export const GJEM_HOVEDMAL = 'veilarbportfolje.gjem_hovedmal';
 export const ARBEIDSLISTEKATEGORI = 'veilarbportefolje.arbeidslistekategori_filtrering';
 
