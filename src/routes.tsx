7336069f139b3e2bab605ec14e358fd0de4925ac
Tania Holst <tania.holst@nav.no>
2020-06-10 12:37:04 +0200

    FO-2888 feature-toggle lagt på. alle filer som starter med gammel skal bort når vi er ferdige med piloten

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,37 +4,66 @@ import { Route, Switch } from 'react-router-dom';
 import EnhetSide from './enhetsportefolje/enhet-side';
 import VeiledereSide from './veiledere/veiledere-side';
 import MinOversiktSide from './minoversikt/minoversikt-side';
+import EnhetSideGammel from './enhetsportefolje/gammel-enhet-side';
+import VeiledereSideGammel from './veiledere/gammel-veiledere-side';
+import MinOversiktSideGammel from './minoversikt/gammel-minoversikt-side';
 import TilbakemeldingFab from './components/tilbakemelding/tilbakemelding-fab';
 import { loggBrowserMetrikker } from './utils/metrikker/browser-metrikker';
 import { useFetchPortefoljeData } from './hooks/portefolje/use-fetch-portefolje-data';
 import Innholdslaster from './innholdslaster/innholdslaster';
 import TourModalLocalStorage from './components/modal/tour-modal/tour-modal-local-storage';
+import { useFeatureSelector } from './hooks/redux/use-feature-selector';
+import { REDESIGN } from './konstanter';
 
 loggBrowserMetrikker();
 
 function Routes() {
     const {enhettiltak, veiledere, portefoljestorrelser} = useFetchPortefoljeData();
+    const erRedesignTogglePa = useFeatureSelector()(REDESIGN);
+
     return (
         <div className="portefolje">
             <div className='maincontent side-innhold'>
                 <Innholdslaster avhengigheter={[enhettiltak, veiledere, portefoljestorrelser]}>
                     <Switch>
-                        <Route
-                            path="/enhet"
-                            component={EnhetSide}
-                        />
-                        <Route
-                            path="/veiledere"
-                            component={VeiledereSide}
-                        />
-                        <Route
-                            path="/portefolje/:ident"
-                            component={MinOversiktSide}
-                        />
-                        <Route
-                            path="/portefolje"
-                            component={MinOversiktSide}
-                        />
+                        {erRedesignTogglePa ?
+                            <>
+                                <Route
+                                    path="/enhet"
+                                    component={EnhetSide}
+                                />
+                                <Route
+                                    path="/veiledere"
+                                    component={VeiledereSide}
+                                />
+                                <Route
+                                    path="/portefolje/:ident"
+                                    component={MinOversiktSide}
+                                />
+                                <Route
+                                    path="/portefolje"
+                                    component={MinOversiktSide}
+                                />
+                            </>
+                            : <>
+                                <Route
+                                    path="/enhet"
+                                    component={EnhetSideGammel}
+                                />
+                                <Route
+                                    path="/veiledere"
+                                    component={VeiledereSideGammel}
+                                />
+                                <Route
+                                    path="/portefolje/:ident"
+                                    component={MinOversiktSideGammel}
+                                />
+                                <Route
+                                    path="/portefolje"
+                                    component={MinOversiktSideGammel}
+                                />
+                            </>
+                        }
                     </Switch>
                     <TilbakemeldingFab/>
                     <TourModalLocalStorage/>
