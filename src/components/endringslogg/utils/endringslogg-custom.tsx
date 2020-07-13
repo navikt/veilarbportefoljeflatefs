e43f6a579daabf86864e93e13e927d8ca52b05a2
taniaholst <holst.tania@gmail.com>
2020-07-13 09:12:27 +0200

    KAIZEN-0 fjernet featuretoggle på cv/jobbprofil

--- src/components/endringslogg/utils/endringslogg-custom.tsx
+++ src/components/endringslogg/utils/endringslogg-custom.tsx
@@ -25,7 +25,6 @@ const endringslogginnhold: EndringsloggInnlegg[] = [
         dato: '10. JUL. 2020',
         tittel: 'Nytt filter på CV/jobbprofil',
         versjonId: '10.07.20',
-        featureToggleName: 'veilarbportefolje.cv-jobbprofil',
         tekst: 'Nå kan du filtrere på brukere som har/ikke har delt CV og jobbprofil med NAV. Da blir det enklere å identifisere og prioritere hvilke brukere som kan ha behov for råd og veiledning i registrering av CV og jobbprofil på arbeidsplassen.no.',
         children:
             <>
