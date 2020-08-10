d84c9e09096511b56ac8bb06847d84f4deb50233
Dragutin Vujovic <dragutin.vujovic@nav.no>
2020-08-10 15:43:25 +0200

    fo-2955: add metric for number of filters per veileder

--- src/middleware/metrics-middleware.ts
+++ src/middleware/metrics-middleware.ts
@@ -87,7 +87,6 @@ function finnSlettetGruppe(store: any, filterId: number) {
     return undefined;
 }
 
-
 export const metricsMiddleWare = (store: any) => (next: any) => (action: any) => {
     const {type, data, kolonne} = action;
     const sideNavn = finnSideNavn();
@@ -149,7 +148,8 @@ export const metricsMiddleWare = (store: any) => (next: any) => (action: any) =>
 
         //lagrede filter
         case HENT_LAGREDEFILTER_OK:
-
+            const veilederIdentTilNonsens = mapVeilederIdentTilNonsens(store.getState().inloggetVeileder.data.ident);
+            loggAntallLagredeFilter(action.data.length, veilederIdentTilNonsens);
             break;
         case NY_LAGREDEFILTER_OK:
             loggAntallBokstaverIFilterNavn(action.data.filterNavn)
@@ -344,3 +344,7 @@ const loggAntallBokstaverIFilterNavn = (filterNavn) => {
         {filterNavn: filterNavn.length})
 };
 
+const loggAntallLagredeFilter = (antallFilter, veilederIdentHash) => {
+    logEvent('portefolje.metrikker.lagredefilter.antall-per-veileder',
+        {antallFilter: antallFilter, veilederhash: veilederIdentHash})
+}
