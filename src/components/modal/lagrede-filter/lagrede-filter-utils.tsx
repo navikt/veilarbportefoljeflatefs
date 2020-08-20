31fed6d36f001563793a331effec07c529daec4c
taniaholst <holst.tania@gmail.com>
2020-08-20 10:40:03 +0200

    KAIZEN-0 småfiks i variabelnavn

--- src/components/modal/lagrede-filter/lagrede-filter-utils.tsx
+++ src/components/modal/lagrede-filter/lagrede-filter-utils.tsx
@@ -6,30 +6,31 @@ export function lagredeFilterListerErLik(lagretFilter: FiltervalgModell, nyttFil
     return objectEquals(lagretFilter, nyttFilter)
 }
 
-function objectEquals(x, y) {
-    // if both are function
-    if (x === null || x === undefined || y === null || y === undefined) {
-        return x === y;
+function objectEquals(lagretFilter, nyttFilter) {
+    // if both are functions
+    if (lagretFilter === null || lagretFilter === undefined || nyttFilter === null || nyttFilter === undefined) {
+        return lagretFilter === nyttFilter;
     }
-    if (x === y || x.valueOf() === y.valueOf()) {
+    if (lagretFilter === nyttFilter || lagretFilter.valueOf() === nyttFilter.valueOf()) {
         return true;
     }
 
-    // if they are not function or strictly equal, they both need to be Objects
-    if (!(x instanceof Object)) {
+    // if they are not functions or strictly equal, they both need to be objects
+    if (!(lagretFilter instanceof Object)) {
         return false;
     }
-    if (!(y instanceof Object)) {
+    if (!(nyttFilter instanceof Object)) {
         return false;
     }
 
-    var p = Object.keys(x);
-    return Object.keys(y).every(function (i) {
+    const p = Object.keys(lagretFilter!);
+    return Object.keys(nyttFilter!).every(function (i) {
         return p.indexOf(i) !== -1;
-    }) ?
-        p.every(function (i) {
-            return objectEquals(x[i], y[i]);
-        }) : false;
+    })
+        ? p.every(function (i) {
+            return objectEquals(lagretFilter[i], nyttFilter[i]);
+        })
+        : false;
 }
 
 export function erTomtObjekt(objekt): boolean {
