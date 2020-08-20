3e037162c36a84dc14b9c1dfcd141e97410d69ac
Dragutin Vujovic <dragutin.vujovic@nav.no>
2020-08-20 13:16:10 +0200

    fo-2955: BugFix for deep object comparison

--- src/components/modal/lagrede-filter/lagrede-filter-utils.tsx
+++ src/components/modal/lagrede-filter/lagrede-filter-utils.tsx
@@ -18,10 +18,7 @@ function deepEqual(object1, object2) {
         const val1 = object1[key];
         const val2 = object2[key];
         const areObjects = isObject(val1) && isObject(val2);
-        if (
-            areObjects && !deepEqual(val1, val2) ||
-            !areObjects && val1 !== val2
-        ) {
+        if ((areObjects && !deepEqual(val1, val2)) || (!areObjects && val1 !== val2)) {
             return false;
         }
     }
