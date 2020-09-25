d1076bdc719c6987692e621fead8b98e981d68b9
stianStensli <stian.horn.stensli@nav.no>
2020-09-25 09:20:23 +0200

    KZ-0 Endret dato

--- src/components/endringslogg/utils/endringslogg-custom.tsx
+++ src/components/endringslogg/utils/endringslogg-custom.tsx
@@ -22,9 +22,9 @@ export interface EndringsloggInnleggMedSettStatus extends EndringsloggInnlegg {
 
 const endringslogginnhold: EndringsloggInnlegg[] = [
     {
-        dato: '24. september 2020',
+        dato: '25. september 2020',
         tittel: 'Endringer i filtrene',
-        versjonId: '24.09.20',
+        versjonId: '25.09.20',
         featureToggleName: 'veilarbportefolje.redesign',
         children:
             <>
