805ce100eae6b4387e73956d3ff38470a60ef71d
taniaholst <tania.holst@nav.no>
2020-11-02 13:10:49 +0100

    tc-159 fikset featuretoggle

--- src/konstanter.ts
+++ src/konstanter.ts
@@ -9,7 +9,7 @@ export const DEFAULT_PAGINERING_STORRELSE = 20;
 export const SPOR_OM_TILBAKEMELDING = 'veilarbportefolje.spor_om_tilbakemelding';
 export const VEDTAKSTOTTE = 'pto.vedtaksstotte.pilot';
 export const GJEM_HOVEDMAL = 'veilarbportfolje.gjem_hovedmal';
-export const DARKMODE = 'veilarbportfolje.darkmode';
-export const ANNEN_VEILEDER = 'veilarbportfolje.annen_veileder';
+export const DARKMODE = 'veilarbportefolje.darkmode';
+export const ANNEN_VEILEDER = 'veilarbportefolje.annen_veileder';
 
 export const PORTEFOLJE_FEATURES = [SPOR_OM_TILBAKEMELDING, VEDTAKSTOTTE, GJEM_HOVEDMAL, DARKMODE];
