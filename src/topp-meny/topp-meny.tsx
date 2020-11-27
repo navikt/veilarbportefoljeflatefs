dec77ce8f3dfa72b109bc14d1e10a1e412f3e67e
stianStensli <stian.horn.stensli@nav.no>
2020-11-27 15:28:28 +0100

    debug jaws

--- src/topp-meny/topp-meny.tsx
+++ src/topp-meny/topp-meny.tsx
@@ -23,7 +23,6 @@ function ToppMeny(props: {erPaloggetVeileder?: boolean}) {
     return (
         <div
             className={classNames('topp-meny', erAlertstripeFeilmeldingFeatureTogglePa && 'topp-meny__alertstripe')}
-            role="navigation"
             aria-label="Naviger mellom de forskjellige oversiktene."
         >
             <Lenker erPaloggetVeileder={!!props.erPaloggetVeileder} />
