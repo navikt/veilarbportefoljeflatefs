c7ca463057c975f0f7df71745b774bf089064a5d
taniaholst <tania.holst@nav.no>
2021-01-05 11:53:44 +0100

    fikset design på alertstripemelding

--- src/enhetsportefolje/enhet-side.tsx
+++ src/enhetsportefolje/enhet-side.tsx
@@ -36,6 +36,10 @@ import FiltreringNavnellerfnr from '../filtrering/filtrering-navnellerfnr';
 import Alertstripe from 'nav-frontend-alertstriper';
 import LagredeFilterUIController from '../filtrering/lagrede-filter-controller';
 import {selectValgteAlternativer} from '../ducks/ui/listevisning-selectors';
+import AlertStripe from 'nav-frontend-alertstriper';
+import Lenke from 'nav-frontend-lenker';
+import {useFeatureSelector} from '../hooks/redux/use-feature-selector';
+import {ALERTSTRIPE_FEILMELDING} from '../konstanter';
 
 function antallFilter(filtervalg) {
     function mapAktivitetFilter(value) {
@@ -111,6 +115,7 @@ export default function EnhetSide() {
     };
 
     const [scrolling, setScrolling] = useState(false);
+    const erAlertstripeFeilmeldingFeatureTogglePa = useFeatureSelector()(ALERTSTRIPE_FEILMELDING);
 
     useEffect(() => {
         function onScroll() {
@@ -130,6 +135,15 @@ export default function EnhetSide() {
         <DocumentTitle title="Enhetens oversikt">
             <div className="side-storrelse" id={`side-storrelse_${id}`}>
                 <ToppMeny />
+                    {erAlertstripeFeilmeldingFeatureTogglePa && (
+                        <AlertStripe type="feil" className="stor-feil-modal">
+                            Vi har dessverre tekniske problemer som kan medføre ustabilitet og/eller feil med
+                            filtreringer. Feilretting pågår.{' '}
+                            <Lenke href="https://navno.sharepoint.com/sites/intranett-driftsmeldinger/" target="_blank">
+                                <b>Følg med på driftsmeldinger på Navet.</b>
+                            </Lenke>
+                        </AlertStripe>
+                    )}
                 <Innholdslaster avhengigheter={[statustall]}>
                     <div
                         className={classNames(
