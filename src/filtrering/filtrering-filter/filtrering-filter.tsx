b6cd8f54f8c6e4e4dfc95d45593097e1194c65ed
taniaholst <tania.holst@nav.no>
2021-03-08 16:26:14 +0100

    tc-275 featuretoggle klar

--- src/filtrering/filtrering-filter/filtrering-filter.tsx
+++ src/filtrering/filtrering-filter/filtrering-filter.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {Element, Normaltekst} from 'nav-frontend-typografi';
 import CheckboxFilterform from './filterform/checkbox-filterform';
 import {
+    aktiviteter,
     alder,
     cvJobbprofil,
     fodselsdagIMnd,
@@ -21,7 +22,7 @@ import Dropdown from '../../components/dropdown/dropdown';
 import './filterform/filterform.less';
 import FodselsdatoFilterform from './filterform/fodselsdato-filterform';
 import {useFeatureSelector} from '../../hooks/redux/use-feature-selector';
-import {GJEM_HOVEDMAL, SISTE_ENDRING} from '../../konstanter';
+import {AKTIVITETER, GJEM_HOVEDMAL, SISTE_ENDRING} from '../../konstanter';
 import '../filtrering-skjema.less';
 import '../../components/sidebar/sidebar.less';
 import {PopoverOrientering} from 'nav-frontend-popover';
@@ -32,6 +33,7 @@ import {ReactComponent as InfoIkon} from '../../components/ikoner/info-ikon.svg'
 import {HendelserFilterform} from './filterform/hendelser-filterform';
 import {OversiktType} from '../../ducks/ui/listevisning';
 import AktivitetFilterformController from './filterform/aktiviteter-filterform/aktivitet-filterform-controller';
+import AktivitetFilterform from './filterform/aktivitet-filterform';
 
 interface FiltreringFilterProps {
     filtervalg: any;
@@ -43,6 +45,7 @@ interface FiltreringFilterProps {
 function FiltreringFilter({filtervalg, endreFiltervalg, enhettiltak, oversiktType}: FiltreringFilterProps) {
     const erGjemHovedmalFeatureTogglePa = useFeatureSelector()(GJEM_HOVEDMAL);
     const erSisteEndringFeatureTogglePa = useFeatureSelector()(SISTE_ENDRING);
+    const erAktivitetFeatureTogglePa = useFeatureSelector()(AKTIVITETER);
     return (
         <div
             className="filtrering-filter col-sm-12 blokk-xs filtrering-filter__kolonne"
@@ -277,9 +280,17 @@ function FiltreringFilter({filtervalg, endreFiltervalg, enhettiltak, oversiktTyp
                 <Dropdown
                     name="Aktivitet"
                     id="aktivitet"
-                    render={() => (
-                        <AktivitetFilterformController filtervalg={filtervalg} endreFiltervalg={endreFiltervalg} />
-                    )}
+                    render={() =>
+                        erAktivitetFeatureTogglePa ? (
+                            <AktivitetFilterformController filtervalg={filtervalg} endreFiltervalg={endreFiltervalg} />
+                        ) : (
+                            <AktivitetFilterform
+                                valg={aktiviteter}
+                                filtervalg={filtervalg}
+                                endreFiltervalg={endreFiltervalg}
+                            />
+                        )
+                    }
                 />
                 <Dropdown
                     name="Tiltakstype"
