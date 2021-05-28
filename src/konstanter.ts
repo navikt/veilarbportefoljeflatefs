ac5ad85c5f6eb610c9985c3569df9fe47a6cf661
taniaholst <tania.holst@nav.no>
2021-05-28 15:58:51 +0200

    tc-330 fjernet feature toggles

--- src/konstanter.ts
+++ src/konstanter.ts
@@ -11,9 +11,6 @@ export const VEDTAKSTOTTE = 'pto.vedtaksstotte.pilot';
 export const GJEM_HOVEDMAL = 'veilarbportfolje.gjem_hovedmal';
 export const DARKMODE = 'veilarbportefolje.darkmode';
 export const ALERTSTRIPE_FEILMELDING = 'veilarbportefolje.alertstripe_feilmelding';
-export const SISTE_ENDRING = 'veilarbportefolje.siste_endring';
-export const HENDELSE_MEDISINSKBEHANDLING = 'veilarbportefolje.hendelse_medisinskbehandling';
-export const ULESTE_ENDRINGER = 'veilarbportefolje.uleste_endringer';
 export const UTEN_KRR_FILTER = 'veilarbportefolje.uten_krr_filter';
 export const TVUNGEN_STEPPER = 'veilarbportefolje.tvungen_stepper';
 export const SOK_VEILEDER = 'veilarbportefolje.sok-veileder';
@@ -24,9 +21,6 @@ export const PORTEFOLJE_FEATURES = [
     GJEM_HOVEDMAL,
     DARKMODE,
     ALERTSTRIPE_FEILMELDING,
-    SISTE_ENDRING,
-    HENDELSE_MEDISINSKBEHANDLING,
-    ULESTE_ENDRINGER,
     UTEN_KRR_FILTER,
     TVUNGEN_STEPPER,
     SOK_VEILEDER
--- src/routes.tsx
+++ src/routes.tsx
@@ -10,12 +10,9 @@ import Innholdslaster from './innholdslaster/innholdslaster';
 import TvungenStepper from './components/modal/tour-modal/tvungen-stepper';
 import {TilToppenKnapp} from './components/til-toppen-knapp/til-toppen-knapp';
 import './style.less';
-import {useFeatureSelector} from './hooks/redux/use-feature-selector';
-import {SISTE_ENDRING} from './konstanter';
 
 function Routes() {
     const {enhettiltak, veiledere, portefoljestorrelser} = useFetchPortefoljeData();
-    const erSisteEndringFeatureTogglePa = useFeatureSelector()(SISTE_ENDRING);
 
     return (
         <div className="portefolje">
@@ -29,7 +26,7 @@ function Routes() {
                     </Switch>
                     <TilbakemeldingFab />
                     <TilToppenKnapp />
-                    {erSisteEndringFeatureTogglePa && <TvungenStepper/>}
+                    <TvungenStepper/>
                 </Innholdslaster>
             </div>
         </div>
