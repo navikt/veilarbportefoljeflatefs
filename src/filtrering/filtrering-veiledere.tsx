145eda70985041f64e2e8bf835d7add349912716
taniaholst <tania.holst@nav.no>
2021-05-31 10:35:52 +0200

    tc-331 fikset bug i søkingen

--- src/filtrering/filtrering-veiledere.tsx
+++ src/filtrering/filtrering-veiledere.tsx
@@ -1,26 +1,55 @@
 import * as React from 'react';
 import {Input, Label} from 'nav-frontend-skjema';
 import VeilederCheckboxListe from '../components/veileder-checkbox-liste/veileder-checkbox-liste';
-import {useRef, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
+import {FiltervalgModell} from '../model-interfaces';
 
 interface FiltreringVeiledereProps {
-    endreFiltervalg: (filterId: string, filterVerdi: React.ReactNode) => void;
+    filtervalg: FiltervalgModell;
+    endreFiltervalg: (filterId: string, filterVerdi: string) => void;
 }
 
-export default function FiltreringVeiledere({endreFiltervalg}: FiltreringVeiledereProps) {
-    const [veilederNavnQuery, setVeilederNavnQuery] = useState('');
-
+export default function FiltreringVeiledere({endreFiltervalg, filtervalg}: FiltreringVeiledereProps) {
+    const [veilederNavnQuery, setVeilederNavnQuery] = useState(filtervalg.veilederNavnQuery);
     const wrapperRef = useRef<HTMLDivElement>(null);
+    const isInitialMount = useRef(true);
+    const timer = useRef<number | undefined>();
+    const savedCallback = useRef(null);
 
-    const handleChange = event => {
-        const nyQuery = event.target.value;
-        setVeilederNavnQuery(nyQuery);
-        endreFiltervalg('veilederNavnQuery', veilederNavnQuery);
-    };
+    useEffect(() => {
+        // @ts-ignore
+        savedCallback.current = filterverdi => endreFiltervalg('veilederNavnQuery', filterverdi);
+    }, [endreFiltervalg]);
+
+    useEffect(() => {
+        const prevQuery = filtervalg.veilederNavnQuery;
+        if (prevQuery === '') {
+            setVeilederNavnQuery('');
+        }
+    }, [filtervalg.veilederNavnQuery]);
+
+    useEffect(() => {
+        function functionCall() {
+            // @ts-ignore
+            savedCallback.current(veilederNavnQuery);
+        }
+
+        if (isInitialMount.current) {
+            isInitialMount.current = false;
+        } else {
+            if (!timer.current) {
+                timer.current = window.setTimeout(functionCall, 500);
+            }
+            return () => {
+                clearTimeout(timer.current);
+                timer.current = undefined;
+            };
+        }
+    }, [veilederNavnQuery]);
 
     const nullstillInputfelt = () => {
         setVeilederNavnQuery('');
-        endreFiltervalg('veilederNavnQuery', '');
+        endreFiltervalg('veilederNavnQuery', veilederNavnQuery);
     };
 
     return (
@@ -30,7 +59,7 @@ export default function FiltreringVeiledere({endreFiltervalg}: FiltreringVeilede
             </Label>
             <Input
                 placeholder="Navn eller NAV-ident"
-                onChange={e => handleChange(e)}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setVeilederNavnQuery(e.target.value)}
                 value={veilederNavnQuery}
                 data-testid="veilederoversikt_sok-veileder-input"
                 aria-label="Navn eller NAV-ident"
