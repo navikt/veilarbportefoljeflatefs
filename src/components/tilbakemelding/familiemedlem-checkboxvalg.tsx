d8dd73eb6af07bb0660414f7c23b5aee40303460
Sneha Desai <sneha.desai@nav.no>
2021-06-29 16:44:39 +0200

    TK-96: La undersokelse modal om familiemedlem opplysninger

---
+++ src/components/tilbakemelding/familiemedlem-checkboxvalg.tsx
@@ -0,0 +1,172 @@
+import * as React from 'react';
+import {Innholdstittel, Element} from 'nav-frontend-typografi';
+import './tilbakemelding-modal.less';
+import {useState} from 'react';
+import {Checkbox, Textarea} from 'nav-frontend-skjema';
+import {Hovedknapp} from 'nav-frontend-knapper';
+import CheckboxValg from './checkbox-valg';
+import TakkIkon from './takk-ikon.png';
+import classNames from "classnames";
+import TilbakemeldingTakkModal from "./tilbakemelding-takk-modal";
+
+export interface TilbakemeldingCheckboxProps {
+    checkboxIndexListe: number[];
+    kommentar: string;
+}
+
+interface TilbakemeldingModalProps {
+    open: boolean;
+    onTilbakemeldingCheckboxSendt: (tilbakemelding: TilbakemeldingCheckboxProps, checkboxStatusListe: any) => void;
+}
+
+export enum CheckboxVerdier {
+    PARTNER_NAVN = 'Partner/ektefelles navn',
+    PARTNER_ALDER = 'Partner/ektefelles alder',
+    PARTNER_FODSELSDATO = 'Partner/ektefelles fødselsdato',
+    PARTNER_FODSELSNUMMER = 'Partner/ektefelles fulle fødselsnummer',
+    BARNETS_NAVN = 'Barnets navn',
+    BARNETS_ALDER = 'Barnets alder',
+    BARNETS_FODSELSDATO = 'Barnets fødselsdato',
+    BARNETS_FODSELSNUMMER = 'Barnets fulle fødselsnummer',
+    BARNETS_KJONN = 'Barnets kjønn'
+}
+
+function FamiliemedlemCheckboxValg({open, onTilbakemeldingCheckboxSendt}: TilbakemeldingModalProps) {
+    const [kommentar, setKommentar] = useState('');
+    const [harBlittVist, setHarBlittVist] = useState(false);
+    const [harSendt, setHarSendt] = useState(false);
+    const [checkboxIndexListe, setcheckboxIndexListe] = useState<number[]>([]);
+    const [showFadeOutAnimation, setShowFadeOutAnimation] = useState(false);
+
+    const KOMMENTAR_MAX_CHAR = 750;
+    const KOMMENTAR_ROWS = 5;
+
+    const ikkeVisIgjen = false;
+
+    const checkboxStatus: any = {};
+
+    function hentCheckboksStatusListe(value: number[]) {
+        checkboxStatus.partner_navn = value.includes(1);
+        checkboxStatus.partner_alder =  value.includes(2);
+        checkboxStatus.partner_fodselsdato = value.includes(3);
+        checkboxStatus.partner_fodselsnummer = value.includes(4);
+        checkboxStatus.barnets_navn = value.includes(5);
+        checkboxStatus.barnets_alder = value.includes(6);
+        checkboxStatus.barnets_fodselsdato = value.includes(7);
+        checkboxStatus.barnets_fodselsnummer = value.includes(8);
+        checkboxStatus.barnets_kjonn = value.includes(9);
+
+        return checkboxStatus;
+    }
+
+    const handleCheckboxFormSubmitted = e => {
+        e.preventDefault();
+        if ((checkboxIndexListe.length > 0) || (kommentar !== undefined && kommentar !== null && kommentar !== '')) {
+            setHarSendt(true);
+            onTilbakemeldingCheckboxSendt({checkboxIndexListe, kommentar}, hentCheckboksStatusListe(checkboxIndexListe));
+        } else {
+            setShowFadeOutAnimation(true);
+        }
+    };
+
+    const handleCheckboxChanged = (verdi: number, el: any) => {
+        if (el.target.checked) {
+            checkboxIndexListe.push(verdi);
+        } else {
+            let index = checkboxIndexListe.indexOf(verdi);
+            checkboxIndexListe.splice(index,1);
+        }
+        setcheckboxIndexListe([...checkboxIndexListe]);
+    };
+
+    const handleKommentarChanged = (value: string) => {
+        if (value.length <= KOMMENTAR_MAX_CHAR) {
+            setKommentar(value);
+        }
+    };
+
+    if (open && !harBlittVist) {
+        setHarBlittVist(true);
+    }
+
+    // Make sure that the animation will trigger when closing instead of returning null (no animation)
+    if ((!open && !harBlittVist) || ikkeVisIgjen) {
+        return null;
+    }
+
+    const renderTakkMelding = () => {
+        return (
+            TilbakemeldingTakkModal
+        );
+    };
+
+    const CheckboxValg = () => {
+        return (
+            <>
+                {
+                    Object.keys(CheckboxVerdier).map((key, index) => {
+                        return (
+                            <Checkbox
+                                checked={checkboxIndexListe.includes(index+1)}
+                                label={CheckboxVerdier[key]}
+                                value={CheckboxVerdier[key]}
+                                key={index}
+                                onChange={e => handleCheckboxChanged(index+1, e)}
+                                className="tilbakemelding-modal__checkbox-element"
+                            />
+                        )
+                    })
+                }
+            </>
+        );
+    };
+
+    const renderCheckboxValg = () => {
+        return (
+            <div className={classNames({ 'tilbakemelding-modal__innhold-fade-out': showFadeOutAnimation })}>
+                <Innholdstittel className="blokk-xxs tilbakemelding-modal__tittel">Hva trenger du å vite om brukerens familie?</Innholdstittel>
+                <Element className="blokk-xxs">Vi skal alltid prøve å begrense mengden informasjon vi har om brukerne våre. Hvor mye informasjon om brukers familie trenger du minimum for oppfølging mot arbeid?</Element>
+                <form
+                    className="tilbakemelding-modal__ekspander"
+                    onSubmit={handleCheckboxFormSubmitted}
+                    data-widget="accessible-autocomplete"
+                >
+                    <CheckboxValg/>
+                    <div className="tilbakemelding-modal__kommentar">
+                        <Textarea
+                            className="tilbakemelding-modal__kommentar-felt"
+                            label="Hva bruker du disse opplysningene til?"
+                            rows={KOMMENTAR_ROWS}
+                            maxLength={KOMMENTAR_MAX_CHAR}
+                            value={kommentar}
+                            onChange={(e) => handleKommentarChanged(e.target.value)}
+                        />
+                    </div>
+                    <Hovedknapp role="submit" className="knapp--hoved">
+                        Send
+                    </Hovedknapp>
+                </form>
+            </div>
+        );
+    };
+
+    return (
+        <div
+            className={classNames(
+                'tilbakemelding-modal',
+                {'tilbakemelding-modal--slide-in': open},
+                {'tilbakemelding-modal--slide-out': !open}
+            )}
+        >
+            <div
+                className={classNames('tilbakemelding-modal__innhold', {
+                    'tilbakemelding-modal__innhold--takk': harSendt
+                })}
+            >
+                { harSendt ? <TilbakemeldingTakkModal /> : renderCheckboxValg() }
+            </div>
+        </div>
+    );
+}
+
+export default FamiliemedlemCheckboxValg;
--- src/components/tilbakemelding/tilbakemelding-fab.tsx
+++ src/components/tilbakemelding/tilbakemelding-fab.tsx
@@ -4,11 +4,13 @@ import {connect} from 'react-redux';
 import './tilbakemelding-fab.less';
 import {sjekkFeature} from '../../ducks/features';
 import {SPOR_OM_TILBAKEMELDING} from '../../konstanter';
-import TilbakemeldingModal, {Tilbakemelding, TilbakemeldingCheckboxProps} from './tilbakemelding-modal';
+import {Tilbakemelding} from './tilbakemelding-modal';
 import {logEvent} from '../../utils/frontend-logger';
 import {useRef, useState} from 'react';
 import {useEventListener} from '../../hooks/use-event-listener';
-
+import tilbakemeldingBilde from './tilbakemelding.svg';
+import lukkBilde from './lukk.svg';
+import FamiliemedlemCheckboxValg, {TilbakemeldingCheckboxProps} from "./familiemedlem-checkboxvalg";
 // FAB = Floating Action Button
 interface TilbakemeldingFabProps {
     harFeature: (feature: string) => boolean;
@@ -16,10 +18,11 @@ interface TilbakemeldingFabProps {
 
 function TilbakemeldingFab({harFeature}: TilbakemeldingFabProps) {
     const TILBAKEMELDING_PREFIX = 'har_sendt_tilbakemelding';
-    const TILBAKEMELDING_FEATURE_TAG = 'siste_endring'; // TODO: Husk å endre for hver nye feature
+    const TILBAKEMELDING_FEATURE_TAG = 'opplysninger_om_familiemedlemmer'; // TODO: Husk å endre for hver nye feature
+    const TILBAKEMELDING_LOCALSTORAGE_NAME = `${TILBAKEMELDING_PREFIX}__${TILBAKEMELDING_FEATURE_TAG}`;
 
+    const [hideFab, setHideFab] = useState(false);
     const [isModalOpen, setModalOpen] = useState(false);
-    const harSendtTilbakemelding = false;
     const [ikkeVisIgjen, setIkkeVisIgjen] = useState(false);
     const wrapperRef = useRef<HTMLDivElement>(null);
 
@@ -34,10 +37,14 @@ function TilbakemeldingFab({harFeature}: TilbakemeldingFabProps) {
 
     useEventListener('mousedown', handleClickOutside);
 
-    const tilbakemeldingLocalStorageName = `${TILBAKEMELDING_PREFIX}__${TILBAKEMELDING_FEATURE_TAG}`;
+    const startAutoClose = () => {
+        setTimeout(() => {
+            setModalOpen(false);
+        }, 1500);
+    };
 
     const harTidligereSendtTilbakemelding = () => {
-        return window.localStorage.getItem(tilbakemeldingLocalStorageName) != null;
+        return window.localStorage.getItem(TILBAKEMELDING_LOCALSTORAGE_NAME) != null;
     };
 
     const handleFabClicked = () => {
@@ -48,7 +55,7 @@ function TilbakemeldingFab({harFeature}: TilbakemeldingFabProps) {
     };
 
     const handleTilfredshetsTilbakemeldingSendt = (tilbakemelding: Tilbakemelding) => {
-        window.localStorage.setItem(tilbakemeldingLocalStorageName, 'true');
+        window.localStorage.setItem(TILBAKEMELDING_LOCALSTORAGE_NAME, 'true');
 
         logEvent('portefolje.tilbakemelding', {
             feature: TILBAKEMELDING_FEATURE_TAG,
@@ -56,61 +63,46 @@ function TilbakemeldingFab({harFeature}: TilbakemeldingFabProps) {
         });
     };
 
-    const handleCheckboxTilbakemeldingSendt = (tilbakemelding: TilbakemeldingCheckboxProps) => {
-        window.localStorage.setItem(tilbakemeldingLocalStorageName, 'true');
+    const handleCheckboxTilbakemeldingSendt = (tilbakemelding: TilbakemeldingCheckboxProps, checkboxStatusListe: any) => {
+        startAutoClose();
+        setHideFab(true);
+        window.localStorage.setItem(TILBAKEMELDING_LOCALSTORAGE_NAME, 'true');
 
-        logEvent('portefolje.tilbakemelding', {
-            feature: TILBAKEMELDING_FEATURE_TAG,
-            ...tilbakemelding
-        });
-
-        tilbakemelding.checkboxverdi.forEach(verdi => {
-            logEvent('portefolje.tilbakemelding.checkboxverdier', {
-                feature: TILBAKEMELDING_FEATURE_TAG,
-                verdi
-            });
-        });
+        console.log("checkboxStatusListe: ", checkboxStatusListe, "kommentar: ", tilbakemelding.kommentar, "checkboxIndexListe: ", tilbakemelding.checkboxIndexListe);
+        logEvent('detaljer.tilbakemelding', {feature: TILBAKEMELDING_FEATURE_TAG, ...tilbakemelding, ...checkboxStatusListe});
     };
 
     const handleIkkeVisIgjen = () => {
-        window.localStorage.setItem(tilbakemeldingLocalStorageName, 'true');
+        window.localStorage.setItem(TILBAKEMELDING_LOCALSTORAGE_NAME, 'true');
         logEvent('portefolje.ikke_vis_tilbakemelding_igjen');
         setIkkeVisIgjen(true);
     };
 
     const harRiktigFeatures = harFeature(SPOR_OM_TILBAKEMELDING); // NB: Husk å endre for hver feature
-    if (ikkeVisIgjen || !harRiktigFeatures || harSendtTilbakemelding || harTidligereSendtTilbakemelding()) {
-        return null;
-    }
-
-    const lukkeIkon =
-        'data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAQAAAADQ4RFAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAKqNIzIAAAAJcEhZcwAACxIAAAsSAdLdfvwAAAAHdElNRQfjAgQNMjf0s2WWAAAAeElEQVQ4y7WUUQ6AMAhDG+/Sw3H/f/TDJW5RpDZxfxt9hHQAABCEeoaWSCRCQgKJBE9ol7AYSs6XkJAoH3qkx8pojb0mfA62pd8FkkmrSPN1EcrIjH1ALqxENjXPD+UZRhiWG59rtJHRsMZoGENojLu1WKwVZizLA5OtpXPPjPcAAAAAAElFTkSuQmCC';
-    const apneIkon =
-        'data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAkCAQAAABY3hDnAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAKqNIzIAAAAJcEhZcwAACxIAAAsSAdLdfvwAAAAHdElNRQfjAgQQGhbxeetBAAACLklEQVRIx8XWO0xUQRTG8d+urEp8xDc+OjXYiBE0kWBjIsFGYimVaEFprCy00MLORLFweyttxBg1NsTESsBHMEJMMIKxMEQhSkHh7uKOBQtBd+9F2Ztwppib+c75Z+6Zc88dYLusQQWhypE3KKtOyU6YrBq5cExqgzqTgvsOqlGtZTTqEUyqIyu4XzVyofUIsgwKGhMFNwoGyQsyiYIzgnxKQCpRMIF0jLzWE1O6K2rdpjy2Np4eIpSbpfJpKlOaSsqNaGbcjk+W5qNlytxKe3RwHHhXaZ4pU2b+8vhP8FhpHipThv7yiMpHhNIpCHorar2CoDOOGQ2mTYfaikqtjtmOEAVeljquypYBfMx3F2Njz5h2PC4flQ/vkDETDkTG7TXkk8PRzChwyhXTXtpfUd1mXM71iGNfpNxWeizvq1NlCdvtqYJH1sdlIa6OM7J+mPDAaXtssc5Wza56b8YHG+LTGwemxjl9fsr76K033vkmGNVtU0xUEFKKUtKx8I1atdtntbSCKc89NKwY6Z9SFBgR1EvS6gUjaa/RlSi4C69okPPLpYR+qCtd9ktOQwrn3ZKWM6Lwh1NRt3ul5xWuxXSzOcvYZ5WiC27PLrTol69wWXoxv4+ef7699WthYcNcrf6PdOx3x4BmrPFQqx/O+rLIjgtG5BZ7rSOCfmzUJxjXkMgZzIN3GBKM2ZMUdhY8ZlQwbGdy2FlwEAzEfrxLBj+Lv0YtxTb77K5VSw3/DdK0H6HFZvnVAAAAAElFTkSuQmCC';
+    const hide = !harRiktigFeatures || harTidligereSendtTilbakemelding() || hideFab;
 
     return (
         <div ref={wrapperRef}>
-            <div
-                className={classNames('tilbakemelding-fab', {
-                    'tilbakemelding-fab__trykket': isModalOpen
-                })}
-                onClick={handleFabClicked}
-                data-testid={isModalOpen ? 'tilbakemelding_fab_knapp_trykket' : 'tilbakemelding_fab_knapp'}
-            >
-                <img
-                    alt="Åpne/Lukk tilbakemeldingform"
-                    className={classNames({
-                        'tilbakemelding-fab__ikon--lukke': isModalOpen,
-                        'tilbakemelding-fab__ikon--apne': !isModalOpen
-                    })}
-                    src={isModalOpen ? lukkeIkon : apneIkon}
-                />
-            </div>
-            <TilbakemeldingModal
-                open={isModalOpen}
-                onTilbakemeldingSendt={handleTilfredshetsTilbakemeldingSendt}
-                onTilbakemeldingCheckboxSendt={handleCheckboxTilbakemeldingSendt}
-                onIkkeVisIgjen={handleIkkeVisIgjen}
+            {!hide && (
+                <div
+                    aria-label="Åpne tilbakemeldingsmodal"
+                    className={classNames('tilbakemelding-fab', {'tilbakemelding-fab__trykket': isModalOpen})}
+                    onClick={handleFabClicked}
+                    data-testid={isModalOpen ? 'tilbakemelding_fab_knapp_trykket' : 'tilbakemelding_fab_knapp'}
+                >
+                    <img
+                        alt="Åpne/Lukk tilbakemeldingform"
+                        className={classNames({
+                            'tilbakemelding-fab__ikon--lukke': isModalOpen,
+                            'tilbakemelding-fab__ikon--apne': !isModalOpen
+                        })}
+                        src={isModalOpen ? lukkBilde : tilbakemeldingBilde}
+                    />
+                </div>
+            )}
+            <FamiliemedlemCheckboxValg
+               open={isModalOpen}
+               onTilbakemeldingCheckboxSendt={handleCheckboxTilbakemeldingSendt}
             />
         </div>
     );
