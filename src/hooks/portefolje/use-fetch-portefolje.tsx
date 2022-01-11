0d74d5489bec6d2fd6e782dd2749a53da74502e5
emildohlenhansen <emil.dohlen.hansen@bekk.no>
2022-01-11 12:08:18 +0100

    legg til portefolje status sjekk

--- src/hooks/portefolje/use-fetch-portefolje.tsx
+++ src/hooks/portefolje/use-fetch-portefolje.tsx
@@ -7,40 +7,46 @@ import {OversiktType, oppdaterAlternativer} from '../../ducks/ui/listevisning';
 import {useSelectGjeldendeVeileder} from './use-select-gjeldende-veileder';
 import {antallFilter} from '../../enhetsportefolje/enhet-side';
 import {MIN_ARBEIDSLISTE} from '../../filtrering/filter-konstanter';
+import {STATUS} from '../../ducks/utils';
 
 export function useFetchPortefolje(oversiktType: OversiktType) {
     const dispatch = useDispatch();
     const enhet = useEnhetSelector();
     const gjeldendeVeileder = useSelectGjeldendeVeileder();
-    const {sorteringsrekkefolge, filtervalg, sorteringsfelt} = usePortefoljeSelector(oversiktType);
-
-    const fetchPortefoljeVeileder = async () => {
-        await dispatch(
-            hentPortefoljeForVeileder(enhet, gjeldendeVeileder, sorteringsrekkefolge, sorteringsfelt, filtervalg)
-        );
-        if (filtervalg.ferdigfilterListe?.includes(MIN_ARBEIDSLISTE)) {
-            await dispatch(hentArbeidslisteforVeileder(enhet, gjeldendeVeileder));
-        }
-    };
+    const {sorteringsrekkefolge, filtervalg, sorteringsfelt, portefolje} = usePortefoljeSelector(oversiktType);
 
     useEffect(() => {
         if (enhet && sorteringsrekkefolge && sorteringsfelt) {
             if (oversiktType === OversiktType.enhetensOversikt && antallFilter(filtervalg)) {
                 dispatch(hentPortefoljeForEnhet(enhet, sorteringsrekkefolge, sorteringsfelt, filtervalg));
             } else if (oversiktType === OversiktType.minOversikt && gjeldendeVeileder) {
-                fetchPortefoljeVeileder();
+                dispatch(
+                    hentPortefoljeForVeileder(
+                        enhet,
+                        gjeldendeVeileder,
+                        sorteringsrekkefolge,
+                        sorteringsfelt,
+                        filtervalg
+                    )
+                );
+                if (filtervalg.ferdigfilterListe?.includes(MIN_ARBEIDSLISTE)) {
+                    dispatch(hentArbeidslisteforVeileder(enhet, gjeldendeVeileder));
+                }
             }
         }
-    }, [
-        dispatch,
-        enhet,
-        sorteringsfelt,
-        sorteringsrekkefolge,
-        filtervalg,
-        gjeldendeVeileder,
-        oversiktType,
-        fetchPortefoljeVeileder
-    ]);
+    }, [dispatch, enhet, sorteringsfelt, sorteringsrekkefolge, filtervalg, gjeldendeVeileder, oversiktType]);
+
+    useEffect(() => {
+        if (
+            enhet &&
+            oversiktType === OversiktType.minOversikt &&
+            gjeldendeVeileder &&
+            filtervalg.ferdigfilterListe?.includes(MIN_ARBEIDSLISTE) &&
+            portefolje.status === STATUS.OK
+        ) {
+            dispatch(hentArbeidslisteforVeileder(enhet, gjeldendeVeileder));
+        }
+    }, [dispatch, enhet, filtervalg, gjeldendeVeileder, oversiktType, portefolje.status]);
 
     useEffect(() => {
         oppdaterAlternativer(dispatch, filtervalg, oversiktType);
