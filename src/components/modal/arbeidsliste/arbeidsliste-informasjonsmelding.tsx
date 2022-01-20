126ff80ce0113dcb385b114758ff2435fb91e57c
emildohlenhansen <emil.dohlen.hansen@bekk.no>
2022-01-20 11:58:13 +0100

    legg til lenke til Navet for  hvordan bruke arbeidslisten

--- src/components/modal/arbeidsliste/arbeidsliste-informasjonsmelding.tsx
+++ src/components/modal/arbeidsliste/arbeidsliste-informasjonsmelding.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import AlertStripe from 'nav-frontend-alertstriper';
+import Lenke from 'nav-frontend-lenker';
 
 const ArbeidslisteInformasjonsmelding = () => (
     <AlertStripe type="info" className="stor-feil-modal">
-        Arbeidslisten er <b>kun</b> til internt bruk. Informasjon som er relevant for personen eller saksbehandlingen skal
-        alltid deles i dialogen.
+        Arbeidslisten er <b>kun</b> til internt bruk. Informasjon som er relevant for personen eller saksbehandlingen
+        skal alltid deles i dialogen.
+        <br />
+        <Lenke
+            href="https://navno.sharepoint.com/sites/fag-og-ytelser-arbeid-arbeidsrettet-brukeroppfolging/SitePages/Oversikten-i-Modia.aspx#slik-bruker-du-arbeidslisten"
+            target="_blank"
+        >
+            <b>Les mer om hvordan bruke arbeidslisten på Navet.</b>
+        </Lenke>
     </AlertStripe>
 );
 
 export default ArbeidslisteInformasjonsmelding;
-
