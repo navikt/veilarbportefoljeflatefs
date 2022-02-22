5a9b194811105a5a45be907c2ba3346cd3a02bdc
emildohlenhansen <emil.dohlen.hansen@bekk.no>
2022-02-22 11:34:46 +0100

    bruk --ignorepatterns

--- src/topp-meny/topp-meny.tsx
+++ src/topp-meny/topp-meny.tsx
@@ -1,3 +1,4 @@
+import Endringslogg from '@navikt/familie-endringslogg';
 import React from 'react';
 import './lenker.less';
 import Toasts from '../components/toast/toast';
@@ -9,7 +10,6 @@ import DarkModeToggle from '../components/toggle/dark-mode-toggle';
 import {useFeatureSelector} from '../hooks/redux/use-feature-selector';
 import {ALERTSTRIPE_FEILMELDING, DARKMODE} from '../konstanter';
 import classNames from 'classnames';
-import {Endringslogg} from '@navikt/familie-endringslogg';
 
 function ToppMeny(props: {erPaloggetVeileder?: boolean}) {
     //VENTER PÅ ATT HENTE PORTEFOLJESTORRELSER FØR ATT VETA OM VI SKA VISA MIN OVERSIKT LENKEN ELLER EJ
