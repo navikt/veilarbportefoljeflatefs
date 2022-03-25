6f635c44c86132e38de6c2fc898a8bb18198cfcb
Mathias S. Müller <mathias.sundt.muller@nav.no>
2022-03-25 14:20:33 +0100

    Bump og juster datovelger i arbeidslistemodalen

--- src/components/formik/formik-datovelger/formik-datovelger.tsx
+++ src/components/formik/formik-datovelger/formik-datovelger.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Field, FieldProps, getIn} from 'formik';
-import {Datovelger} from 'nav-datovelger';
+import {Datepicker} from 'nav-datovelger';
 import {validerDatoFeldt} from '../../../utils/dato-utils';
 import classNames from 'classnames';
 import './daypicker.less';
@@ -24,16 +24,17 @@ function FormikDatoVelger({name}: FormikDatepickerProps) {
                         <Label className="skjemaelement__label" size="small">
                             Frist
                         </Label>
-                        <Datovelger
-                            id="fristDatovelger"
-                            input={{
-                                id: 'fristInput',
+                        <Datepicker
+                            inputId="fristDatovelger"
+                            inputProps={{
+                                // id: 'fristInput' cypress bruker denne,
                                 name: 'frist',
-                                placeholder: 'dd.mm.åååå',
-                                ariaLabel: 'Frist:'
+                                placeholder: 'dd.mm.åååå'
+                                // ariaLabel: 'Frist:'
                             }}
                             onChange={(date?: string) => setFieldValue(field.name, date)}
-                            valgtDato={field.value}
+                            calendarSettings={{showWeekNumbers: true, position: 'responsive'}}
+                            value={field.value}
                             dayPickerProps={{className: 'datovelger__DayPicker'}}
                         />
                         <SkjemaelementFeilmelding>{error}</SkjemaelementFeilmelding>
