511379f45d6407a60b7cef52a39c43c0bfd4b249
Mathias S. Müller <mathias.sundt.muller@nav.no>
2022-03-25 16:07:16 +0100

    Legg til markering av røde dager i dateovelger

--- src/components/formik/formik-datovelger/formik-datovelger.tsx
+++ src/components/formik/formik-datovelger/formik-datovelger.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import dayjs from 'dayjs';
+import DateHolidays, {HolidaysTypes} from 'date-holidays';
 import {Field, FieldProps, getIn} from 'formik';
 import {Datepicker} from 'nav-datovelger';
 import {validerDatoFeldt} from '../../../utils/dato-utils';
@@ -6,6 +8,45 @@ import classNames from 'classnames';
 import './daypicker.less';
 import SkjemaelementFeilmelding from 'nav-frontend-skjema/lib/skjemaelement-feilmelding';
 import {Label} from '@navikt/ds-react';
+
+const norwegianHolidays = new DateHolidays('no');
+
+const getPublicHolidays = (from: Date, to: Date): HolidaysTypes.Holiday[] => {
+    let days = [] as HolidaysTypes.Holiday[];
+    const fromYear = from.getFullYear();
+    const toYear = to.getFullYear();
+    if (fromYear === toYear) {
+        days = norwegianHolidays.getHolidays(fromYear);
+    } else {
+        let år = fromYear;
+        while (år <= toYear) {
+            days = [...days, ...norwegianHolidays.getHolidays(år)];
+            år++;
+        }
+    }
+    const start = dayjs(from).subtract(24, 'hour');
+    const slutt = dayjs(to).add(24, 'hour');
+    return days
+        .filter(d => d.type === 'public')
+        .map(d => ({
+            ...d,
+            date: d.date
+        }))
+        .filter(d => dayjs(d.date).isAfter(start, 'day') && dayjs(d.date).isBefore(slutt, 'day'));
+};
+
+const holidays = getPublicHolidays(
+    dayjs()
+        .subtract(4, 'year')
+        .toDate(),
+    dayjs()
+        .add(4, 'year')
+        .toDate()
+);
+
+const isPublicHoliday = (d: Date): boolean => {
+    return holidays.some(d2 => dayjs(d2.date).isSame(d, 'day'));
+};
 
 interface FormikDatepickerProps {
     name: string;
@@ -35,7 +76,8 @@ function FormikDatoVelger({name}: FormikDatepickerProps) {
                             calendarSettings={{showWeekNumbers: true, position: 'responsive'}}
                             value={field.value}
                             dayPickerProps={{
-                                className: 'datovelger__DayPicker'
+                                className: 'datovelger__DayPicker',
+                                modifiers: {isPublicHoliday}
                             }}
                         />
                         <SkjemaelementFeilmelding>{error}</SkjemaelementFeilmelding>
