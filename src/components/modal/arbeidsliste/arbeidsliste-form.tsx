444976b751a94366bcd6c6af5369e9eee5026c5f
emildohlenhansen <emil.dohlen.hansen@bekk.no>
2022-04-05 14:09:54 +0200

    fjern debug

--- src/components/modal/arbeidsliste/arbeidsliste-form.tsx
+++ src/components/modal/arbeidsliste/arbeidsliste-form.tsx
@@ -12,6 +12,7 @@ function label(bruker: BrukerModell): React.ReactNode {
 }
 
 function ArbeidslisteForm({arbeidsliste, valgteBrukere}) {
+    console.log(arbeidsliste);
     return (
         <>
             {arbeidsliste.map((bruker, index) => (
