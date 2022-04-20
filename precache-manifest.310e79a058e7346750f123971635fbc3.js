a46e5f71e18c83970e38399e929d1f7193e53bff
dragutin-nav <dragutin-nav@users.noreply.github.com>
2022-04-20 20:20:21 +0000

    deploy: e2015389d68d9c4f1b75d024eccbee9df8d7b511

---
+++ precache-manifest.310e79a058e7346750f123971635fbc3.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e79f17dfe2a6a2d136699cf9f03c531",
+    "url": "/veilarbportefoljeflatefs/index.html"
+  },
+  {
+    "revision": "20668c8fecf90af28c02",
+    "url": "/veilarbportefoljeflatefs/static/css/2.chunk.css"
+  },
+  {
+    "revision": "c3a206bbe9380f2d8579",
+    "url": "/veilarbportefoljeflatefs/static/css/main.chunk.css"
+  },
+  {
+    "revision": "20668c8fecf90af28c02",
+    "url": "/veilarbportefoljeflatefs/static/js/2.chunk.js"
+  },
+  {
+    "revision": "9b6cc9c04821a6a978a6ac0cce4c5c7b",
+    "url": "/veilarbportefoljeflatefs/static/js/2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3a206bbe9380f2d8579",
+    "url": "/veilarbportefoljeflatefs/static/js/main.chunk.js"
+  },
+  {
+    "revision": "8a57895437ea18171cfd",
+    "url": "/veilarbportefoljeflatefs/static/js/runtime-main.js"
+  },
+  {
+    "revision": "066de983925e8fcf50042d2bd2cc9ceb",
+    "url": "/veilarbportefoljeflatefs/static/media/arbeidslisteikon_bla.066de983.svg"
+  },
+  {
+    "revision": "828d482c83510806e9bafa234bbbfdb7",
+    "url": "/veilarbportefoljeflatefs/static/media/arbeidslisteikon_gronn.828d482c.svg"
+  },
+  {
+    "revision": "6eba3d4a289feb42f43899ca6aa48236",
+    "url": "/veilarbportefoljeflatefs/static/media/arbeidslisteikon_gul.6eba3d4a.svg"
+  },
+  {
+    "revision": "f619ae0237791a11597e8e722e9c2fb9",
+    "url": "/veilarbportefoljeflatefs/static/media/arbeidslisteikon_lilla.f619ae02.svg"
+  },
+  {
+    "revision": "a9dd540e89dd63621305eb7061213b1a",
+    "url": "/veilarbportefoljeflatefs/static/media/dragIcon.a9dd540e.svg"
+  },
+  {
+    "revision": "e20551002c154ba3f102aa186c84db1a",
+    "url": "/veilarbportefoljeflatefs/static/media/filter-ikon.e2055100.svg"
+  },
+  {
+    "revision": "65d6b4acd3bac90cedc355df572c07c7",
+    "url": "/veilarbportefoljeflatefs/static/media/icon-v3.65d6b4ac.svg"
+  },
+  {
+    "revision": "3d95ca9cec78896264a8df610cebfd6b",
+    "url": "/veilarbportefoljeflatefs/static/media/mine-filter1.3d95ca9c.png"
+  },
+  {
+    "revision": "6f239861c77e8c67ed71efd57a9a4a4e",
+    "url": "/veilarbportefoljeflatefs/static/media/mine-filter2.6f239861.png"
+  },
+  {
+    "revision": "e6138a3570f265e3880fb3368bca8440",
+    "url": "/veilarbportefoljeflatefs/static/media/mine-filter3.e6138a35.png"
+  },
+  {
+    "revision": "0561452606bb08c50764ab60fea76973",
+    "url": "/veilarbportefoljeflatefs/static/media/settings.05614526.svg"
+  },
+  {
+    "revision": "efa9dd7fe477cb93b4bf62dbf32563f0",
+    "url": "/veilarbportefoljeflatefs/static/media/siste-endring1.efa9dd7f.png"
+  },
+  {
+    "revision": "b7f91305945fe12a2bd22b865f2b8f9e",
+    "url": "/veilarbportefoljeflatefs/static/media/siste-endring2.b7f91305.png"
+  },
+  {
+    "revision": "b4f9bb36178c8231d97a5b7d044d704e",
+    "url": "/veilarbportefoljeflatefs/static/media/siste-endring3.b4f9bb36.png"
+  },
+  {
+    "revision": "91f3b622bdd45a3e876301e154ec087f",
+    "url": "/veilarbportefoljeflatefs/static/media/tab_filter.91f3b622.svg"
+  },
+  {
+    "revision": "12f547a3475dbd5c376cb8c768633180",
+    "url": "/veilarbportefoljeflatefs/static/media/tab_mine-filter.12f547a3.svg"
+  },
+  {
+    "revision": "7becfdccd95f6c54892930bbdd68e939",
+    "url": "/veilarbportefoljeflatefs/static/media/tab_status.7becfdcc.svg"
+  },
+  {
+    "revision": "4e78fdf04ce65943b6a72fb5323c0e1c",
+    "url": "/veilarbportefoljeflatefs/static/media/tab_veiledergrupper.4e78fdf0.svg"
+  },
+  {
+    "revision": "eef4ad900d86d24d528a75cba5e3fedf",
+    "url": "/veilarbportefoljeflatefs/static/media/tilrettelegging1.eef4ad90.jpg"
+  },
+  {
+    "revision": "d139b6f0bf592ae8c8a21422c6baae4f",
+    "url": "/veilarbportefoljeflatefs/static/media/tilrettelegging2.d139b6f0.jpg"
+  },
+  {
+    "revision": "eaa1fa470dcc47fcc7f8234bfdeb9828",
+    "url": "/veilarbportefoljeflatefs/static/media/tilrettelegging3.eaa1fa47.jpg"
+  }
+]);
