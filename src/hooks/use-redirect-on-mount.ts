ee42d9bf844794a9253f8e35297fd025f33b4f32
Dragutin Vujovic <dragutin.vujovic@nav.no>
2022-05-08 21:00:03 +0200

    Fix github pages

--- src/hooks/use-redirect-on-mount.ts
+++ src/hooks/use-redirect-on-mount.ts
@@ -31,7 +31,6 @@ export function useRedirectOnMount() {
         }
         else if (erGithubPages()){
             history.push('/enhet');
-            dispatch(settSortering('ikke_satt', 'ikke_satt'));
             return;
         } else if (lastPath && location.pathname === '/tilbake') {
             history.replace({pathname: lastPath, search: lastSearch});
