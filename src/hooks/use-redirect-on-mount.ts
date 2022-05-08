4a2139041520fcff7dab216f88c7ed8a7e07f5e1
Dragutin Vujovic <dragutin.vujovic@nav.no>
2022-05-08 20:42:05 +0200

    Fix github pages

--- src/hooks/use-redirect-on-mount.ts
+++ src/hooks/use-redirect-on-mount.ts
@@ -30,10 +30,10 @@ export function useRedirectOnMount() {
             return;
         }
         else if (erGithubPages()){
-            history.push('/');
+            //history.push('/');
             //DEKORATORN VIKER IKKE PÅ HEROKU SÅ DETTA ER EN HAKS FOR ATT FÅ ENHET SATT
-            //dispatch(oppdaterValgtEnhet('1234'));
-            //dispatch(settSortering('ikke_satt', 'ikke_satt'));
+            dispatch(oppdaterValgtEnhet('1234'));
+            dispatch(settSortering('ikke_satt', 'ikke_satt'));
             return;
         } else if (lastPath && location.pathname === '/tilbake') {
             history.replace({pathname: lastPath, search: lastSearch});
