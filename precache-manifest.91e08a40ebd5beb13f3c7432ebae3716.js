ce6ca7b859e0bc202e8c02ab93241bf76c989e6e
stianStensli <stianStensli@users.noreply.github.com>
2022-05-12 10:40:09 +0000

    deploy: 219bafb160315aa7e0713d391865448cf602d22c

--- precache-manifest.91e08a40ebd5beb13f3c7432ebae3716.js
+++ precache-manifest.91e08a40ebd5beb13f3c7432ebae3716.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/veilarbportefoljeflatefs/static/css/2.chunk.css"
   },
   {
-    "revision": "313f5e5ccea37ed5aca9",
+    "revision": "8de829cb8a9649c2491d",
     "url": "/veilarbportefoljeflatefs/static/css/main.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/veilarbportefoljeflatefs/static/js/2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "313f5e5ccea37ed5aca9",
+    "revision": "8de829cb8a9649c2491d",
     "url": "/veilarbportefoljeflatefs/static/js/main.chunk.js"
   },
   {
