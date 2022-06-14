9f7995d79dd7c8eeb707535f3f79fe8c7425b5ca
Mathias S. Müller <mathias.sundt.muller@nav.no>
2022-06-14 16:07:36 +0200

    Fiks cannot update compnt while rndering a diffrnt

--- src/hooks/portefolje/use-set-enhet-hvis-enhet-i-url.ts
+++ src/hooks/portefolje/use-set-enhet-hvis-enhet-i-url.ts
@@ -2,12 +2,21 @@ import {useQueryParams} from '../use-query-params';
 import {velgEnhetForVeileder} from '../../ducks/valgt-enhet';
 import {useDispatch, useSelector} from 'react-redux';
 import {AppState} from '../../reducer';
+import {useEffect} from 'react';
 
 export function useSetInitalEnhet() {
     const innloggetVeilederInfo = useSelector((state: AppState) => state.innloggetVeileder.data);
 
     const dispatch = useDispatch();
     const enhetId = useQueryParams().enhet;
+
+    useEffect(() => {
+        if (enhetId && innloggetVeilederInfo && enheter.length !== 0) {
+            if (enheter.findIndex(enhet => enhet.enhetId === enhetId) >= 0) {
+                dispatch(velgEnhetForVeileder(enhetId));
+            }
+        }
+    }, []);
 
     if (!innloggetVeilederInfo) {
         return {hasError: true};
@@ -17,12 +26,6 @@ export function useSetInitalEnhet() {
 
     if (enheter.length === 0) {
         return {hasError: true};
-    }
-
-    if (enhetId) {
-        if (enheter.findIndex(enhet => enhet.enhetId === enhetId) >= 0) {
-            dispatch(velgEnhetForVeileder(enhetId));
-        }
     }
 
     return {hasError: false};
