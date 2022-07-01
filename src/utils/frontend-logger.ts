e42cd7d9fc32de0297728572825ddd1a2912cedc
stianStensli <stian.horn.stensli@nav.no>
2022-07-01 11:05:47 +0200

    bedre typer

--- src/utils/frontend-logger.ts
+++ src/utils/frontend-logger.ts
@@ -4,9 +4,10 @@ import {
     DEFAULT_FRONTENDLOGGER_API_URL
 } from '@navikt/frontendlogger/lib';
 
-export const logger = process.env.REACT_APP_MOCK
-    ? createMockFrontendLogger('veilarbportefoljeflatefs')
-    : createFrontendLogger('veilarbportefoljeflatefs', DEFAULT_FRONTENDLOGGER_API_URL);
+export const logger =
+    process.env.REACT_APP_MOCK === 'true'
+        ? createMockFrontendLogger('veilarbportefoljeflatefs')
+        : createFrontendLogger('veilarbportefoljeflatefs', DEFAULT_FRONTENDLOGGER_API_URL);
 
 export const logEvent = (logTag: string, fields?: {}, tags?: {}): void => {
     console.log('Er mock: ' + process.env.REACT_APP_MOCK);
