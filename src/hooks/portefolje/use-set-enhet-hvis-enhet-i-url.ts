d8079d27de97609b8590819ed57daf34b4ffb641
Mathias S. Müller <mathias.sundt.muller@nav.no>
2022-07-01 15:37:26 +0200

    Legg til types/jest

--- src/hooks/portefolje/use-set-enhet-hvis-enhet-i-url.ts
+++ src/hooks/portefolje/use-set-enhet-hvis-enhet-i-url.ts
@@ -2,29 +2,31 @@ import {useQueryParams} from '../use-query-params';
 import {velgEnhetForVeileder} from '../../ducks/valgt-enhet';
 import {useDispatch, useSelector} from 'react-redux';
 import {AppState} from '../../reducer';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
 export function useSetInitalEnhet() {
     const innloggetVeilederInfo = useSelector((state: AppState) => state.innloggetVeileder.data);
 
     const dispatch = useDispatch();
     const enhetId = useQueryParams().enhet;
+    const enheter = innloggetVeilederInfo?.enheter;
+
+    const [isTriggered, setTriggered] = useState<boolean>(false);
 
     useEffect(() => {
-        if (enhetId && innloggetVeilederInfo && enheter.length !== 0) {
+        if (!isTriggered && enhetId && innloggetVeilederInfo && enheter && enheter.length !== 0) {
             if (enheter.findIndex(enhet => enhet.enhetId === enhetId) >= 0) {
+                setTriggered(true);
                 dispatch(velgEnhetForVeileder(enhetId));
             }
         }
-    }, []);
+    }, [dispatch, enhetId, enheter, innloggetVeilederInfo, isTriggered]);
 
     if (!innloggetVeilederInfo) {
         return {hasError: true};
     }
 
-    const enheter = innloggetVeilederInfo.enheter;
-
-    if (enheter.length === 0) {
+    if (enheter?.length === 0) {
         return {hasError: true};
     }
 
