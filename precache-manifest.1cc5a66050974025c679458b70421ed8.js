dd4a280ad91eb768964317dde59642fa748dd992
stianStensli <stianStensli@users.noreply.github.com>
2022-07-08 07:47:25 +0000

    deploy: 70e1b4f2cb13b02340bf4b3d46c9ef0cea609574

---
+++ precache-manifest.1cc5a66050974025c679458b70421ed8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f38a39184f9e222beabc3d817645e7",
+    "url": "/veilarbportefoljeflatefs/index.html"
+  },
+  {
+    "revision": "6f468f8e9eae538cfb9c",
+    "url": "/veilarbportefoljeflatefs/static/css/2.chunk.css"
+  },
+  {
+    "revision": "d482e2e2c936a37f4313",
+    "url": "/veilarbportefoljeflatefs/static/css/main.chunk.css"
+  },
+  {
+    "revision": "6f468f8e9eae538cfb9c",
+    "url": "/veilarbportefoljeflatefs/static/js/2.chunk.js"
+  },
+  {
+    "revision": "027481246f8f023536b5b95d66a44ccd",
+    "url": "/veilarbportefoljeflatefs/static/js/2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d482e2e2c936a37f4313",
+    "url": "/veilarbportefoljeflatefs/static/js/main.chunk.js"
+  },
+  {
+    "revision": "8a57895437ea18171cfd",
+    "url": "/veilarbportefoljeflatefs/static/js/runtime-main.js"
+  },
+  {
+    "revision": "066de983925e8fcf50042d2bd2cc9ceb",
+    "url": "/veilarbportefoljeflatefs/static/media/arbeidslisteikon_bla.066de983.svg"
+  },
+  {
+    "revision": "828d482c83510806e9bafa234bbbfdb7",
+    "url": "/veilarbportefoljeflatefs/static/media/arbeidslisteikon_gronn.828d482c.svg"
+  },
+  {
+    "revision": "6eba3d4a289feb42f43899ca6aa48236",
+    "url": "/veilarbportefoljeflatefs/static/media/arbeidslisteikon_gul.6eba3d4a.svg"
+  },
+  {
+    "revision": "f619ae0237791a11597e8e722e9c2fb9",
+    "url": "/veilarbportefoljeflatefs/static/media/arbeidslisteikon_lilla.f619ae02.svg"
+  },
+  {
+    "revision": "a9dd540e89dd63621305eb7061213b1a",
+    "url": "/veilarbportefoljeflatefs/static/media/dragIcon.a9dd540e.svg"
+  },
+  {
+    "revision": "e20551002c154ba3f102aa186c84db1a",
+    "url": "/veilarbportefoljeflatefs/static/media/filter-ikon.e2055100.svg"
+  },
+  {
+    "revision": "0561452606bb08c50764ab60fea76973",
+    "url": "/veilarbportefoljeflatefs/static/media/settings.05614526.svg"
+  },
+  {
+    "revision": "91f3b622bdd45a3e876301e154ec087f",
+    "url": "/veilarbportefoljeflatefs/static/media/tab_filter.91f3b622.svg"
+  },
+  {
+    "revision": "12f547a3475dbd5c376cb8c768633180",
+    "url": "/veilarbportefoljeflatefs/static/media/tab_mine-filter.12f547a3.svg"
+  },
+  {
+    "revision": "7becfdccd95f6c54892930bbdd68e939",
+    "url": "/veilarbportefoljeflatefs/static/media/tab_status.7becfdcc.svg"
+  },
+  {
+    "revision": "4e78fdf04ce65943b6a72fb5323c0e1c",
+    "url": "/veilarbportefoljeflatefs/static/media/tab_veiledergrupper.4e78fdf0.svg"
+  }
+]);
