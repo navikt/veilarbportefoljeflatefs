19458ebafdc1cd0db19247165f6e32d53d7b3862
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2022-08-11 12:52:11 +0200

    TC-140 Legg til kopiknapp på fnr

--- src/components/tabell/brukerfnr.tsx
+++ src/components/tabell/brukerfnr.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {BrukerModell} from '../../model-interfaces';
 import {BodyShort} from '@navikt/ds-react';
+import Kopiknapp from '../knapper/kopiknapp';
 
 interface BrukerFnrProps {
     className?: string;
@@ -11,6 +12,8 @@ function BrukerFnr({className, bruker}: BrukerFnrProps) {
     return (
         <BodyShort size="small" className={className}>
             {bruker.fnr}
+            {bruker.fnr && (
+            <Kopiknapp kopitekst={bruker.fnr} type="fødselsnummer" />)}
         </BodyShort>
     );
 }
