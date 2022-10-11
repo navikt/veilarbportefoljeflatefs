6884f0cd2121a0f8769fa9f33c9c6f4cb3a79e1d
taniaholst <tania.holst@nav.no>
2022-10-11 15:07:14 +0200

    la til venting på loader-modalene

--- cypress/integration/arbeidslistestatus_spec.js
+++ cypress/integration/arbeidslistestatus_spec.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'cypress-wait-until';
 
 before('Start server', () => {
     cy.configure();
@@ -26,10 +27,7 @@ describe('Filter min arbeidsliste', () => {
         cy.getByTestId('filter_checkboks-label_minArbeidslisteLilla').then($tall => {
             antallFor = $tall.text();
         });
-
     });
-
-
 
     it('Åpne Min arbeidsliste', () => {
         cy.getByTestId('legg-i-arbeidsliste_knapp').should('be.disabled');
@@ -43,23 +41,15 @@ describe('Filter min arbeidsliste', () => {
         cy.getByTestId('modal_arbeidslistekategori_LILLA').click();
         cy.getByTestId('modal_arbeidsliste_lagre-knapp').click();
 
-
+        cy.get('.veilarbportefoljeflatefs-laster-modal').should('be.visible');
+        cy.get('.veilarbportefoljeflatefs-laster-modal').should('not.exist');
 
         cy.getByTestId('filter_checkboks-label_minArbeidslisteLilla').then($tall => {
-            antallEtter = $tall.text();
-
+            expect(antallFor).not.to.eq($tall.text());
         });
-        console.log('antall før', antallFor.valueOf());
-        console.log('antall etter', antallEtter.valueOf());
-        cy.getByTestId('filter_checkboks-label_minArbeidslisteLilla')
-
-          .then($tall => {
-              expect(antallFor).not.to.eq($tall.text());
-          });
-
-
-}); });
-        /*
+    });
+});
+/*
         cy.getByTestId('antall-tiltak').then($navn => {
   expect(antallTiltak).not.to.eq($navn.text());
 });
