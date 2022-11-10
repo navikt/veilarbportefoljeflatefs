e0b2ec9d45c8081e300436f818e93ace380dfee8
Dragutin Vujovic <dragutin.vujovic@nav.no>
2022-11-10 15:16:00 +0100

    Small fixes based on code review

--- src/filtrering/filtrering-filter/filterform/geografiskbosted-filterform.tsx
+++ src/filtrering/filtrering-filter/filterform/geografiskbosted-filterform.tsx
@@ -8,6 +8,7 @@ import NullstillKnapp from '../../../components/nullstill-valg-knapp/nullstill-k
 import {useGeografiskbostedSelector} from '../../../hooks/redux/use-geografiskbosted-selector';
 import {GeografiskBostedOptions} from '../../../ducks/geografiskBosted';
 import {MultiSelect} from 'react-multi-select-component';
+import {isEmptyArray} from 'formik';
 
 interface GeografiskBostedFilterformProps {
     endreFiltervalg: (form: string, filterVerdi: string[]) => void;
@@ -25,13 +26,18 @@ function GeografiskBostedFilterform({endreFiltervalg, filtervalg, gridColumns = 
     useEffect(() => {
         setVisGeografiskBosted(filtervalg.visGeografiskBosted);
 
-        if (geografiskbostedListData.size > 0) {
+        if (!isEmptyArray(geografiskbostedListData)) {
             let selectedValues: GeografiskBostedOptions[] = [];
-            filtervalg.geografiskBosted.forEach(x => {
-                if (geografiskbostedListData.get(x) != null && geografiskbostedListData.get(x) !== undefined) {
-                    selectedValues.push({label: geografiskbostedListData.get(x)!, value: x, checked: true});
-                }
-            });
+            filtervalg.geografiskBosted
+                .filter(bostedKode => geografiskbostedListData.get(bostedKode) != null)
+                .filter(bostedKode => geografiskbostedListData.get(bostedKode) !== undefined)
+                .forEach(bostedKode => {
+                    selectedValues.push({
+                        label: geografiskbostedListData.get(bostedKode)!,
+                        value: bostedKode,
+                        checked: true
+                    });
+                });
             setSelectedGeografiskBosted(selectedValues);
         }
     }, [filtervalg, geografiskbostedListData]);
@@ -44,13 +50,13 @@ function GeografiskBostedFilterform({endreFiltervalg, filtervalg, gridColumns = 
     };
 
     useEffect(() => {
-        if (geografiskbostedListData != null && geografiskbostedListData.size > 0) {
-            let result: GeografiskBostedOptions[] = [];
-
-            geografiskbostedListData.forEach((value, key) => {
-                result.push({label: value, value: key});
-            });
-            setGeografiskBostedOptions(result);
+        if (geografiskbostedListData.size > 0) {
+            setGeografiskBostedOptions(
+                Array.from(geografiskbostedListData).map((bostedKodeNavn: [string, string]) => ({
+                    label: bostedKodeNavn[0],
+                    value: bostedKodeNavn[1]
+                }))
+            );
         }
     }, [geografiskbostedListData]);
 
@@ -60,79 +66,73 @@ function GeografiskBostedFilterform({endreFiltervalg, filtervalg, gridColumns = 
     };
 
     return (
-        <>
-            <form className="skjema checkbox-filterform" data-testid="checkbox-filterform">
-                <div className={classNames('checkbox-filterform__valg', 'landgruppe')}>
-                    <Grid columns={gridColumns}>
-                        <CheckboxGroup
-                            hideLegend
-                            legend=""
-                            onChange={(filtre: string[]) => {
-                                endreFiltervalg('visGeografiskBosted', filtre);
-                            }}
-                            size="small"
-                            value={visGeografiskBosted}
-                        >
-                            <div>
-                                <Checkbox
-                                    key="visGeografiskBosted"
-                                    value="1"
-                                    data-testid={`filter_vis_geografisk_bosted`}
-                                >
-                                    Vis geografisk bosted
-                                </Checkbox>
-                            </div>
-                        </CheckboxGroup>
-                    </Grid>
-                </div>
-                <hr />
-                <label className="skjemaelement__label">Velg en eller flere</label>
-                <MultiSelect
-                    className="utvalgsliste"
-                    options={geografiskBostedOptions}
-                    value={selectedGeografiskBosted}
-                    onChange={velgGeografiskBosted}
-                    labelledBy="Select"
-                    hasSelectAll={false}
-                    overrideStrings={{
-                        allItemsAreSelected: 'Alle bosteder er valgt.',
-                        clearSearch: 'Fjern søk',
-                        clearSelected: 'Fjern valgt',
-                        noOptions: 'Ingen resultater ved søk',
-                        search: 'Søk',
-                        selectSomeItems: 'Velg bosted...'
-                    }}
-                    ItemRenderer={({checked, option, onClick, disabled}) => (
-                        <div className={'navds-checkbox navds-checkbox--small'}>
-                            <input
-                                type="checkbox"
-                                onChange={e => {
-                                    e.stopPropagation();
-                                    onClick();
-                                }}
-                                checked={checked}
-                                tabIndex={-1}
-                                className={'navds-checkbox__input'}
-                                value={option.value}
-                                aria-checked={checked}
-                                id={`checkbox-bosted-${option.value}`}
-                            />
-                            <label className={'navds-checkbox__label'} htmlFor={`checkbox-bosted-${option.value}`}>
-                                <div className="navds-checkbox__content">
-                                    <div className="navds-body-short navds-body-short--small">{option.label}</div>
-                                </div>
-                            </label>
+        <form className="skjema checkbox-filterform" data-testid="checkbox-filterform">
+            <div className={classNames('checkbox-filterform__valg', 'landgruppe')}>
+                <Grid columns={gridColumns}>
+                    <CheckboxGroup
+                        hideLegend
+                        legend=""
+                        onChange={(filtre: string[]) => {
+                            endreFiltervalg('visGeografiskBosted', filtre);
+                        }}
+                        size="small"
+                        value={visGeografiskBosted}
+                    >
+                        <div>
+                            <Checkbox key="visGeografiskBosted" value="1" data-testid={`filter_vis_geografisk_bosted`}>
+                                Vis geografisk bosted
+                            </Checkbox>
                         </div>
-                    )}
-                />
-                <NullstillKnapp
-                    dataTestId="checkbox-filterform"
-                    nullstillValg={nullstillValg}
-                    form={'landgruppe'}
-                    disabled={visGeografiskBosted.length <= 0 && selectedGeografiskBosted.length <= 0}
-                />
-            </form>
-        </>
+                    </CheckboxGroup>
+                </Grid>
+            </div>
+            <hr />
+            <label className="skjemaelement__label">Velg en eller flere</label>
+            <MultiSelect
+                className="utvalgsliste"
+                options={geografiskBostedOptions}
+                value={selectedGeografiskBosted}
+                onChange={velgGeografiskBosted}
+                labelledBy="Select"
+                hasSelectAll={false}
+                overrideStrings={{
+                    allItemsAreSelected: 'Alle bosteder er valgt.',
+                    clearSearch: 'Fjern søk',
+                    clearSelected: 'Fjern valgt',
+                    noOptions: 'Ingen resultater ved søk',
+                    search: 'Søk',
+                    selectSomeItems: 'Velg bosted...'
+                }}
+                ItemRenderer={({checked, option, onClick, disabled}) => (
+                    <div className={'navds-checkbox navds-checkbox--small'}>
+                        <input
+                            type="checkbox"
+                            onChange={e => {
+                                e.stopPropagation();
+                                onClick();
+                            }}
+                            checked={checked}
+                            tabIndex={-1}
+                            className={'navds-checkbox__input'}
+                            value={option.value}
+                            aria-checked={checked}
+                            id={`checkbox-bosted-${option.value}`}
+                        />
+                        <label className={'navds-checkbox__label'} htmlFor={`checkbox-bosted-${option.value}`}>
+                            <div className="navds-checkbox__content">
+                                <div className="navds-body-short navds-body-short--small">{option.label}</div>
+                            </div>
+                        </label>
+                    </div>
+                )}
+            />
+            <NullstillKnapp
+                dataTestId="checkbox-filterform"
+                nullstillValg={nullstillValg}
+                form={'landgruppe'}
+                disabled={visGeografiskBosted.length <= 0 && selectedGeografiskBosted.length <= 0}
+            />
+        </form>
     );
 }
 
