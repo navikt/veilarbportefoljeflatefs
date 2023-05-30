510e1c429a1e407a4a91f61dd103c7bf486cfd4c
Dragutin Vujovic <dragutin.vujovic@nav.no>
2023-05-30 14:22:40 +0200

    Add feature toggle for filter for personer med barn under 18

--- src/konstanter.ts
+++ src/konstanter.ts
@@ -14,7 +14,7 @@ export const ALERTSTRIPE_FEILMELDING = 'veilarbportefolje.alertstripe_feilmeldin
 export const VIS_MELDING_OM_BRUKERE_MED_ADRESSEBESKYTTELSE_ELLER_SKJERMING =
     'veilarbportefolje.vis_melding_om_brukere_med_adressebeskyttelse_eller_skjerming';
 
-export const FILTER_FOR_PERSONER_MED_BARN_UNDER_18 = 'pto.veilarbportefolje.filter_for_personer_med_barn_under_18';
+export const FILTER_FOR_PERSONER_MED_BARN_UNDER_18 = 'veilarbportefolje.filter_for_personer_med_barn_under_18';
 
 //HUSK Å LEGG TIL FEATURE-TOGGLE HER OGSÅ!!!!!!!!!
 export const PORTEFOLJE_FEATURES = [
