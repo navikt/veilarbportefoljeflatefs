e3b4815312a3991604217bad1123ad4da6f864a6
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2023-08-28 10:46:03 +0200

    Fjern ubrukt funksjon

--- src/utils/url-utils.ts
+++ src/utils/url-utils.ts
@@ -6,22 +6,6 @@ export function getFraBrukerFraUrl(): string {
     return queryString.parse(window.location.search).fraBruker as string;
 }
 
-export function setFraBrukerIUrl(bruker: string) {
-    const parsed = queryString.parse(window.location.search);
-    parsed.fraBruker = bruker;
-
-    const lastSearch = localStorage.getItem('lastsearch');
-    const fnrRegex = new RegExp('[0-9]{11}');
-
-    if (lastSearch && fnrRegex.test(lastSearch)) {
-        localStorage.setItem('lastsearch', lastSearch.replace(fnrRegex, bruker));
-    } else if (lastSearch && !fnrRegex.test(lastSearch)) {
-        localStorage.setItem('lastsearch', lastSearch.concat(`&fraBruker=${bruker}`));
-    }
-    localStorage.setItem('xPos', window.pageXOffset.toString());
-    localStorage.setItem('yPos', window.pageYOffset.toString());
-}
-
 export function getSideFromUrl() {
     return parseInt((queryString.parse(window.location.search).side as string) || '1', 10);
 }
