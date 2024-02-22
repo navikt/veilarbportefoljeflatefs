d0b9566af853d494377254e40b1b4db37cc36395
Ingrid Fosså <ingrid.fossa@nav.no>
2024-02-22 15:16:26 +0100

    Gjer at månadsnamn i møteplanen kjem på norsk sjølv om brukaren har engelsk operativsystem
    Dette gjer at språket vert litt meir konsekvent, før hadde vi overskrifter som "Mandag 23. march".
    
    Co-authored-by: Mathias Sundt Müller <mathias.sundt.muller@nav.no>

--- src/utils/dato-utils.ts
+++ src/utils/dato-utils.ts
@@ -177,28 +177,6 @@ export function oppfolgingStartetDato(dato: string): Maybe<Date> {
     return oppfolgingStartetDato < tidligsteDato ? tidligsteDato : oppfolgingStartetDato;
 }
 
-export function dagFraDato(dato: Date): string {
-    const i = dato.getDay();
-    switch (i) {
-        case 0:
-            return 'Søndag';
-        case 1:
-            return 'Mandag';
-        case 2:
-            return 'Tirsdag';
-        case 3:
-            return 'Onsdag';
-        case 4:
-            return 'Torsdag';
-        case 5:
-            return 'Fredag';
-        case 6:
-            return 'Lørdag';
-        default:
-            return '...';
-    }
-}
-
 export function hentSkjermetInfo(
     egenAnsatt: boolean | undefined,
     skjermetTil: string | undefined
