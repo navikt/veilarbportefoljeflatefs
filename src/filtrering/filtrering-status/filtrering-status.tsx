e60ef8c193dd942b33d93ec553cf9cbe8e20985b
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2024-03-11 12:41:36 +0100

    Legg til egen feature-toggle for fargekategorier

--- src/filtrering/filtrering-status/filtrering-status.tsx
+++ src/filtrering/filtrering-status/filtrering-status.tsx
@@ -17,7 +17,12 @@ import BarInputCheckbox from '../../components/barinput/barinput-checkbox';
 import {BarInputRadio} from '../../components/barinput/barinput-radio';
 import {tekstAntallBrukere} from '../../utils/tekst-utils';
 import {useFeatureSelector} from '../../hooks/redux/use-feature-selector';
-import {HUSKELAPP, VEDTAKSTOTTE, VIS_MELDING_OM_BRUKERE_MED_ADRESSEBESKYTTELSE_ELLER_SKJERMING} from '../../konstanter';
+import {
+    FARGEKATEGORIER,
+    HUSKELAPP,
+    VEDTAKSTOTTE,
+    VIS_MELDING_OM_BRUKERE_MED_ADRESSEBESKYTTELSE_ELLER_SKJERMING
+} from '../../konstanter';
 import {Detail, Label, RadioGroup, ReadMore} from '@navikt/ds-react';
 import './filtrering-status.css';
 import FilterStatusMineFargekategorier from './fargekategori';
@@ -63,6 +68,7 @@ export function FiltreringStatus({filtervalg, oversiktType, statustall}: Filtrer
     const statustallTotalt = statustallMedBrukerinnsyn.totalt + (statustallUtenBrukerinnsyn?.totalt ?? 0);
     const erVedtaksStotteFeatureTogglePa = useFeatureSelector()(VEDTAKSTOTTE);
     const erHuskelappFeatureTogglePa = useFeatureSelector()(HUSKELAPP);
+    const erFargekategorierFeatureTogglePa = useFeatureSelector()(FARGEKATEGORIER);
     const visBrukereMedAdressebeskyttelseEllerSkjermingStatus =
         useFeatureSelector()(VIS_MELDING_OM_BRUKERE_MED_ADRESSEBESKYTTELSE_ELLER_SKJERMING) &&
         oversiktType === OversiktType.enhetensOversikt &&
@@ -251,30 +257,30 @@ export function FiltreringStatus({filtervalg, oversiktType, statustall}: Filtrer
                     checked={ferdigfilterListe.includes(MIN_ARBEIDSLISTE)}
                 />
                 {erHuskelappFeatureTogglePa && oversiktType === OversiktType.minOversikt && (
-                    <>
-                        <div className="forsteBarlabelIGruppe">
-                            <BarInputRadio
-                                filterNavn="huskelapp"
-                                antall={statustallMedBrukerinnsyn.mineHuskelapper}
-                                handleChange={handleRadioButtonChange}
-                            />
-                        </div>
-                        <FilterStatusMineFargekategorier
-                            ferdigfilterListe={fargekategoriListe}
-                            handleChange={handleCheckboxChange}
-                            handleChangeCheckbox={dispatchFargekategorierChange}
-                            hidden={oversiktType !== OversiktType.minOversikt}
-                            filtervalg={filtervalg}
-                            endreFiltervalg={dispatchFiltreringStatusChanged}
-                            checked={ferdigfilterListe.includes(MINE_FARGEKATEGORIER)}
-                            indeterminate={
-                                ferdigfilterListe.includes(MINE_FARGEKATEGORIER) &&
-                                !alleFargekategoriFilterAlternativer.every(fargekategoriFilterAlternativ =>
-                                    fargekategoriListe.includes(fargekategoriFilterAlternativ)
-                                )
-                            }
+                    <div className="forsteBarlabelIGruppe">
+                        <BarInputRadio
+                            filterNavn="huskelapp"
+                            antall={statustallMedBrukerinnsyn.mineHuskelapper}
+                            handleChange={handleRadioButtonChange}
                         />
-                    </>
+                    </div>
+                )}
+                {erFargekategorierFeatureTogglePa && oversiktType === OversiktType.minOversikt && (
+                    <FilterStatusMineFargekategorier
+                        ferdigfilterListe={fargekategoriListe}
+                        handleChange={handleCheckboxChange}
+                        handleChangeCheckbox={dispatchFargekategorierChange}
+                        hidden={oversiktType !== OversiktType.minOversikt}
+                        filtervalg={filtervalg}
+                        endreFiltervalg={dispatchFiltreringStatusChanged}
+                        checked={ferdigfilterListe.includes(MINE_FARGEKATEGORIER)}
+                        indeterminate={
+                            ferdigfilterListe.includes(MINE_FARGEKATEGORIER) &&
+                            !alleFargekategoriFilterAlternativer.every(fargekategoriFilterAlternativ =>
+                                fargekategoriListe.includes(fargekategoriFilterAlternativ)
+                            )
+                        }
+                    />
                 )}
             </RadioGroup>
         </div>
