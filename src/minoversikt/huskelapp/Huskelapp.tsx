5fcbfa5f29dcc9aeb86301700420472cfd96db52
Ingrid Fosså <ingrid.fossa@nav.no>
2024-03-19 10:10:04 +0100

    Gjer at alle filene fungerer igjen

--- src/minoversikt/huskelapp/Huskelapp.tsx
+++ src/minoversikt/huskelapp/Huskelapp.tsx
@@ -4,7 +4,7 @@ import {toDatePrettyPrint} from '../../utils/dato-utils';
 import {HuskelappModell} from '../../model-interfaces';
 import {BodyShort, Detail, Heading} from '@navikt/ds-react';
 
-interface Pourops {
+interface Props {
     huskelapp: HuskelappModell;
     className?: string;
     children?: React.ReactNode;
