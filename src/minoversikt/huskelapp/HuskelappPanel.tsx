c9519696b0b64fe6929f08fc46058a6bc80117d4
JulieHillRoa <julie.hill.roa@nav.no>
2024-04-10 16:00:17 +0200

    Kunne slette arbeidsliste uten å slette fargekategori ved huskelapp

--- src/minoversikt/huskelapp/HuskelappPanel.tsx
+++ src/minoversikt/huskelapp/HuskelappPanel.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import {LagHuskelappInngang} from './LagHuskelappInngang';
 import {EksisterendeArbeidslisteVisning} from './EksisterendeArbeidslisteVisning';
-import {ArbeidslisteDataModell, BrukerModell, HuskelappModell} from '../../model-interfaces';
+import {BrukerModell, HuskelappModell} from '../../model-interfaces';
 import './huskelapp.css';
 import {HuskelappPanelvisning} from './HuskelappPanelvisning';
-import {slettArbeidslisteAction} from '../../ducks/arbeidsliste';
 import {useDispatch} from 'react-redux';
-import {oppdaterStateVedSlettArbeidsliste} from './slettEksisterendeArbeidsliste';
+import {slettArbeidslisteUtenFargekategoriOgOppdaterRedux} from './slettEksisterendeArbeidsliste';
 
 export const HuskelappPanel = ({bruker}: {bruker: BrukerModell}) => {
     const dispatch = useDispatch();
     const onSlettArbeidsliste = () => {
-        const arbeidsliste: ArbeidslisteDataModell[] = [bruker].map(bruker => ({
-            fnr: bruker.fnr,
-            kommentar: bruker.arbeidsliste.kommentar ?? null,
-            frist: bruker.arbeidsliste.frist,
-            kategori: bruker.arbeidsliste.kategori
-        }));
-        slettArbeidslisteAction(arbeidsliste)(dispatch).then(res =>
-            oppdaterStateVedSlettArbeidsliste(res, arbeidsliste, dispatch)
-        );
+        slettArbeidslisteUtenFargekategoriOgOppdaterRedux(bruker, dispatch);
     };
 
     return (
--- src/minoversikt/huskelapp/slettEksisterendeArbeidsliste.ts
+++ src/minoversikt/huskelapp/slettEksisterendeArbeidsliste.ts
@@ -1,8 +1,26 @@
-import {ArbeidslisteDataModell} from '../../model-interfaces';
+import {ArbeidslisteDataModell, BrukerModell} from '../../model-interfaces';
 import {visServerfeilModal} from '../../ducks/modal-serverfeil';
 import {FJERN_FRA_ARBEIDSLISTE_FEILET, visFeiletModal} from '../../ducks/modal-feilmelding-brukere';
 import {leggTilStatustall} from '../../ducks/statustall-veileder';
 import {oppdaterArbeidslisteForBruker} from '../../ducks/portefolje';
+import {slettArbeidslisteUtenFargekategori} from '../../middleware/api';
+import {ThunkDispatch} from 'redux-thunk';
+import {AppState} from '../../reducer';
+import {AnyAction} from 'redux';
+
+export const slettArbeidslisteUtenFargekategoriOgOppdaterRedux = async (
+    bruker: BrukerModell,
+    dispatch: ThunkDispatch<AppState, any, AnyAction>
+) => {
+    try {
+        const arbeidslisteUtenFargekategori = await slettArbeidslisteUtenFargekategori(bruker.fnr);
+        const ikkeAktivArbeidsliste = {...arbeidslisteUtenFargekategori, arbeidslisteAktiv: false, fnr: bruker.fnr};
+        leggTilStatustall('minArbeidsliste', -1)(dispatch);
+        oppdaterArbeidslisteForBruker([ikkeAktivArbeidsliste])(dispatch);
+    } catch (error) {
+        return visServerfeilModal()(dispatch);
+    }
+};
 
 export const oppdaterStateVedSlettArbeidsliste = (res, arbeidsliste: ArbeidslisteDataModell[], dispatch) => {
     if (!res) {
