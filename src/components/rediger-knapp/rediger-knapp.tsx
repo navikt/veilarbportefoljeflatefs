92994df59567f355bebe6726360fe1f2f01d6335
Ingrid Fosså <ingrid.fossa@nav.no>
2024-05-15 16:20:06 +0200

    Bytt til aksel-ikon i redigeringsknapp

--- src/components/rediger-knapp/rediger-knapp.tsx
+++ src/components/rediger-knapp/rediger-knapp.tsx
@@ -1,8 +1,7 @@
 import React, {useState} from 'react';
-import hiddenIf from '../hidden-if/hidden-if';
-import './knapper.css';
-import {Edit, EditFilled} from '@navikt/ds-icons';
 import {Button} from '@navikt/ds-react';
+import {PencilFillIcon, PencilIcon} from '@navikt/aksel-icons';
+import hiddenIf from '../hidden-if/hidden-if';
 
 interface Props {
     aria: string;
@@ -23,7 +22,7 @@ function RedigerKnapp({aria, onClick, dataTestid}: Props) {
             onClick={onClick}
             data-testid={dataTestid}
             size="small"
-            icon={hover ? <EditFilled /> : <Edit />}
+            icon={hover ? <PencilFillIcon title="Rediger" /> : <PencilIcon title="Rediger" />}
         ></Button>
     );
 }
