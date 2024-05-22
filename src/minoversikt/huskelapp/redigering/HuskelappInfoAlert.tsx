710db25aea03953d49a2033a9efb4a9986d346a3
Ingrid Fosså <ingrid.fossa@nav.no>
2024-05-22 08:55:11 +0200

    Fiks skrivefeil i infomelding

--- src/minoversikt/huskelapp/redigering/HuskelappInfoAlert.tsx
+++ src/minoversikt/huskelapp/redigering/HuskelappInfoAlert.tsx
@@ -5,7 +5,7 @@ export const HuskelappInfoAlert = () => (
     <Alert variant="info" className="blokk-xs">
         Du skal ikke skrive sensitive opplysninger eller annen informasjon som personen skal ha innsyn i her.{' '}
         <Link>
-            <b>Mer informasjon om huskelap på Navet.</b>
+            <b>Mer informasjon om huskelapp på Navet.</b>
         </Link>
     </Alert>
 );
