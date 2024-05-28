7cebb87cc4ee4f1e1f64ec5c5967bbaeb38add83
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2024-05-28 10:52:52 +0200

    Viser info om filter hvis veileder har ferdigfilter MIN_ARBEIDSLISTE

--- src/filtrering/filtrering-mine-filter/mine-filter_innhold.tsx
+++ src/filtrering/filtrering-mine-filter/mine-filter_innhold.tsx
@@ -44,6 +44,13 @@ function MineFilterInnhold({
     const inaktiveFilter = () => {
         return filtrertListe().filter(elem => !elem.aktiv);
     };
+    const alertArbeidslisteEllerKategori = () => {
+        if (filtrertListe().filter(elem => elem.filterValg.ferdigfilterListe.includes('MIN_ARBEIDSLISTE')).length > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    };
 
     const dispatch = useDispatch();
 
@@ -57,17 +64,19 @@ function MineFilterInnhold({
     const hentFiltrertListeinnhold = () => {
         return (
             <>
-                <Alert
-                    variant="info"
-                    className="mine-filter_alertstripe"
-                    data-testid="mine-filter_alertstripe-arbeidsliste"
-                    size="small"
-                >
-                    <b>Du har filter med arbeidsliste</b>
-                    <br />
-                    Disse kan vise færre brukere etterhvert som du migrerer til huskelapp. Det kan være lurt å lage nye
-                    filter som også henter inn brukere med fargekategorier.
-                </Alert>
+                {alertArbeidslisteEllerKategori() && (
+                    <Alert
+                        variant="info"
+                        className="mine-filter_alertstripe"
+                        data-testid="mine-filter_alertstripe-arbeidsliste"
+                        size="small"
+                    >
+                        <b>Du har filter med arbeidsliste</b>
+                        <br />
+                        Disse kan vise færre brukere etterhvert som du migrerer til huskelapp. Det kan være lurt å lage
+                        nye filter som også henter inn brukere med fargekategorier.
+                    </Alert>
+                )}
                 {inaktiveFilter().length !== 0 && (
                     <Alert
                         variant="info"
