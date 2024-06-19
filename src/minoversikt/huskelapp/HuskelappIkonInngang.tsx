2f44af5cb3199121097498b60b1e5fc7bd7ca9f1
Sneha Desai <sneha.desai@gmail.com>
2024-06-19 11:18:13 +0200

    Fjernet ubrukt importer

--- src/minoversikt/huskelapp/HuskelappIkonInngang.tsx
+++ src/minoversikt/huskelapp/HuskelappIkonInngang.tsx
@@ -4,7 +4,6 @@ import {BrukerModell, HuskelappModell, VeilederModell} from '../../model-interfa
 import {RedigerHuskelappModal} from './redigering/RedigerHuskelappModal';
 import {ReactComponent as HuskelappIkon} from '../../components/ikoner/huskelapp/Huskelappikon_bakgrunnsfarge.svg';
 import {ReactComponent as HuskelappIkonTomt} from '../../components/ikoner/huskelapp/Huskelappikon_stiplet.svg';
-import {HuskelappModal} from './modalvisning/HuskelappModal';
 import {OrNothing} from '../../utils/types/types';
 import './huskelapp-inngang.css';
 
@@ -15,7 +14,6 @@ interface Props {
 
 export const HuskelappIkonInngang = ({bruker, innloggetVeileder}: Props) => {
     const [skalViseRedigerHuskelappModal, setSkalViseRedigerHuskelappModal] = useState<boolean>(false);
-    const [skalViseHuskelappModal, setSkalViseHuskelappModal] = useState<boolean>(false);
 
     const arbeidslisteAktiv = bruker.arbeidsliste?.arbeidslisteAktiv;
     const harHuskelappEllerArbeidsliste = !!bruker.huskelapp || arbeidslisteAktiv;
@@ -30,13 +28,9 @@ export const HuskelappIkonInngang = ({bruker, innloggetVeileder}: Props) => {
 
     function lukkRedigeringsmodal() {
         setSkalViseRedigerHuskelappModal(false);
-        /*if (bruker.huskelapp) {
-            setSkalViseHuskelappModal(true);
-        }*/
     }
 
     function apneRedigeringsmodal() {
-        //setSkalViseHuskelappModal(false);
         setSkalViseRedigerHuskelappModal(true);
     }
 
@@ -54,7 +48,7 @@ export const HuskelappIkonInngang = ({bruker, innloggetVeileder}: Props) => {
                 variant="tertiary"
                 icon={harHuskelappEllerArbeidsliste ? <HuskelappIkon /> : <HuskelappIkonTomt />}
                 title={titletekst()}
-                onClick={() => setSkalViseRedigerHuskelappModal(true)}
+                onClick={apneRedigeringsmodal}
             />
             {skalViseRedigerHuskelappModal && (
                 <RedigerHuskelappModal
@@ -63,7 +57,7 @@ export const HuskelappIkonInngang = ({bruker, innloggetVeileder}: Props) => {
                     huskelapp={bruker.huskelapp as HuskelappModell}
                     arbeidsliste={bruker.arbeidsliste.arbeidslisteAktiv ? bruker.arbeidsliste : null}
                     bruker={bruker}
-                    lukkVisHuskelappModal={() => setSkalViseRedigerHuskelappModal(false)}
+                    lukkVisHuskelappModal={lukkRedigeringsmodal}
                 />
             )}
         </>
