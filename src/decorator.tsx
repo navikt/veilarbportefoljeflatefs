70097dc16de2500af66a084f44f4f6e66531044a
Ingrid Fosså <ingrid.fossa@nav.no>
2024-09-27 13:33:07 +0200

    Dokumenterer edge-case og feil i fiksen

--- src/decorator.tsx
+++ src/decorator.tsx
@@ -47,7 +47,33 @@ function getConfig(enhet: string | null, settValgtEnhet: (enhet) => void): Decor
 }
 
 const onFnrChangedMedFeatureToggle = fnr => {
-    /* TODO dokumenter smutthol/edge-case der ein søkar på brukar som allereie er i context */
+    /* Skildring av edge-case og kjende problem med denne fiksen.*/
+
+    /* Oppførsel med fiks:
+     *
+     * Case: Berre ei open fane
+     * Oppsummert: forventa oppførsel. Opning i ny fane: opnar ny fane og blir ståande på oversikten. Resten navigerer til brukarside.
+     *
+     * Case: To opne vindauge med oversikten, begge er "heilt ferske". Fane 1 er den du står på, fane 2 er open i bakgrunnen/anna vindauge.
+     *   1) Du navigerer direkte til person i den eine. -> Begge oversiktsfaner navigerer til personen.
+     *   2) Du opnar ein person i ny fane. -> Fane 1 blir verande på oversikten, fane 2 navigerer til personen.
+     *   3) Du søkar på ein person i personsøket. -> Begge oversiktsfaner navigerer til personen.
+     * Oppsummert: Fane 1 har forventa oppførsel. Fane 2 navigerer til personen i alle tilfelle (i staden for å halde seg på oversikten i alle tilfelle).
+     *
+     * Case: To opne vindauge med oversikten. Fane 1 er den du står på, fane 2 har tidlegare opna brukar med ctrl+klikk
+     *   1) Du navigerer direkte til person i den eine (frå fane 1). -> Fane navigerer til personen. Fane 2 forblir på oversikten, men får melding om endring av bruker i kontekst.
+     *   2) Du opnar ein person i ny fane (frå fane 1). -> Fane 1 forblir på oversikten. Fane 2 forblir på oversikten, men får melding om endring av bruker i kontekst.
+     *   3) Du søkar på ein person i personsøket (frå fane 1). -> Fane 1 navigerer til personen. Fane 2 forblir på oversikten, men får melding om endring av bruker i kontekst.
+     * Oppsummert: Fane 1 har forventa oppførsel. Fane 2 får melding om endring av brukar i kontekst (og skulle helst ikkje hatt det).
+     *
+     *
+     * Case: Søkar på same person ein rett før opna i ny fane
+     *   - Opnar person i ny fane. Personnummeret dukkar opp i personsøk.
+     *   - Trykkar søk. Ingenting skjer (anna enn at feltet vert tomt).
+     *   - Skriv inn personnummer på nytt, prøvar å søke igjen. Skjer framleis ingenting.
+     * Dette er eit edge-case vi visste om då vi valde fiksen. Vi trur ikkje det vil skje ofte, fiksen i sum er betre enn ingenting.
+     *
+     * */
     const fnrForSidenavigeringMidlertidigFiks = store.getState().fnrForSidenavigeringMidlertidigFiks.fnr;
 
     if (fnr) {
