95a006e134a1cd88b661d2142ccbb74707f96f1e
Ingrid Fosså <ingrid.fossa@nav.no>
2024-09-27 10:21:52 +0200

    Hentar fnr frå lokal state direkte i onFnrChanged-funksjonen

--- src/decorator.tsx
+++ src/decorator.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import NAVSPA from '@navikt/navspa';
-import {useDispatch, useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import {oppdaterValgtEnhet} from './ducks/valgt-enhet';
 import {useEnhetSelector} from './hooks/redux/use-enhet-selector';
 import {useBrukerIKontekstSelector} from './hooks/redux/use-bruker-i-kontekst-selector';
@@ -9,7 +9,7 @@ import {fjernBrukerIKontekst} from './ducks/bruker-i-kontekst';
 import {DecoratorPropsV3, Environment} from './utils/types/decorator-props-v3';
 import {useFeatureSelector} from './hooks/redux/use-feature-selector';
 import {MIDLERTIDIG_FIKS_FNR_I_KONTEKST} from './konstanter';
-import {AppState} from './reducer';
+import {store} from './application';
 
 const InternflateDecorator = NAVSPA.importer<DecoratorPropsV3>('internarbeidsflate-decorator-v3');
 
@@ -46,8 +46,10 @@ function getConfig(enhet: string | null, settValgtEnhet: (enhet) => void): Decor
     };
 }
 
-const onFnrChangedMedFeatureToggle = (fnr, fnrForSidenavigeringMidlertidigFiks: string | null) => {
+const onFnrChangedMedFeatureToggle = fnr => {
     /* TODO dokumenter smutthol/edge-case der ein søkar på brukar som allereie er i context */
+    const fnrForSidenavigeringMidlertidigFiks = store.getState().fnrForSidenavigeringMidlertidigFiks.fnr;
+
     if (fnr) {
         if (fnr !== fnrForSidenavigeringMidlertidigFiks) {
             // eslint-disable-next-line no-alert
@@ -69,35 +71,8 @@ const onFnrChangedMedFeatureToggle = (fnr, fnrForSidenavigeringMidlertidigFiks: 
     }
 };
 
-function getConfigMedFeatureToggle(
-    enhet: string | null,
-    settValgtEnhet: (enhet) => void,
-    fnrForSidenavigeringMidlertidigFiks: string | null
-): DecoratorPropsV3 {
-    return {
-        appName: 'Arbeidsrettet oppfølging',
-        fnr: undefined,
-        onFnrChanged: fnr => onFnrChangedMedFeatureToggle(fnr, fnrForSidenavigeringMidlertidigFiks),
-        showSearchArea: true,
-        enhet: enhet ?? undefined,
-        showEnheter: true,
-        onEnhetChanged: value => {
-            if (value) {
-                settValgtEnhet(value);
-            }
-        },
-        proxy: '/modiacontextholder',
-        environment: getDecoratorEnv(),
-        showHotkeys: false,
-        urlFormat: getEnv().ingressType === 'ansatt' ? 'ANSATT' : 'NAV_NO'
-    };
-}
-
 export function Decorator() {
     const erMidlertidigFiksFnrIKontekstFeatureTogglePa = useFeatureSelector()(MIDLERTIDIG_FIKS_FNR_I_KONTEKST);
-    const fnrForSidenavigeringMidlertidigFiks = useSelector(
-        (state: AppState) => state.fnrForSidenavigeringMidlertidigFiks.fnr
-    );
 
     const dispatch = useDispatch();
     const enhetId = useEnhetSelector();
@@ -113,14 +88,10 @@ export function Decorator() {
         dispatch(oppdaterValgtEnhet(enhet));
     }
 
-    const configToggle = getConfigMedFeatureToggle(enhetId, velgEnhet, fnrForSidenavigeringMidlertidigFiks);
-    const configVanlig = getConfig(enhetId, velgEnhet);
-    const onFnrChangedToggle = fnr => onFnrChangedMedFeatureToggle(fnr, fnrForSidenavigeringMidlertidigFiks);
-
-    const valgtConfig = erMidlertidigFiksFnrIKontekstFeatureTogglePa ? configToggle : configVanlig;
+    const config = getConfig(enhetId, velgEnhet);
 
     if (erMidlertidigFiksFnrIKontekstFeatureTogglePa) {
-        return <InternflateDecorator {...valgtConfig} onFnrChanged={value => onFnrChangedToggle(value)} />;
+        return <InternflateDecorator {...config} onFnrChanged={fnr => onFnrChangedMedFeatureToggle(fnr)} />;
     }
-    return <InternflateDecorator {...valgtConfig} />;
+    return <InternflateDecorator {...config} />;
 }
