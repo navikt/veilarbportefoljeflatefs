b47e14e435f541f2f34991506d878b42b30bb2dc
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2024-10-17 16:18:29 +0200

    Legger inn Amplitude track for filter innsatsgruppe

--- src/utils/frontend-logger.tsx
+++ src/utils/frontend-logger.tsx
@@ -1,4 +1,8 @@
 import {erMock} from './url-utils';
+import {trackAmplitude} from '../amplitude/amplitude';
+import {string} from 'prop-types';
+import {filter} from 'cypress/types/minimatch';
+
 // import {sendEventTilPortefolje} from '../middleware/api';
 
 export interface FrontendEvent {
@@ -8,10 +12,22 @@ export interface FrontendEvent {
 }
 
 export const logEvent = (logTag: string, fields?: {}, tags?: {}): void => {
+    let filter,
+        verdi = '';
+    if (fields) {
+        const keys = Object.keys(fields);
+        filter = fields[keys[1]];
+        verdi = fields[keys[2]];
+    }
     if (erMock()) {
         // eslint-disable-next-line no-console
         console.log('Event', logTag, 'Fields:', fields, 'Tags:', tags);
     } else {
+        if (filter === 'innsatsgruppe') {
+            // eslint-disable-next-line no-console
+            console.log('Amplitude data ', filter, verdi);
+            trackAmplitude({name: 'filtervalg', data: {filter, verdi}});
+        }
         // Funker ikke etter vi flytta til GCP på grunn av influx som bare er i fss?
         // sendEventTilPortefolje({name: logTag, fields: fields, tags: tags});
     }
