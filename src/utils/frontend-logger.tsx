f4c3d41ddaecbde29b0bec1b688e67358a29fed1
JulieHillRoa <julie.hill.roa@nav.no>
2024-10-21 09:02:06 +0200

    oversetter innsatsgruppe, definere en type for filterendring m.m

--- src/utils/frontend-logger.tsx
+++ src/utils/frontend-logger.tsx
@@ -1,33 +1,39 @@
 import {erMock} from './url-utils';
 import {trackAmplitude} from '../amplitude/amplitude';
-import {string} from 'prop-types';
-import {filter} from 'cypress/types/minimatch';
+import {innsatsgruppe} from '../filtrering/filter-konstanter';
 
-// import {sendEventTilPortefolje} from '../middleware/api';
+export type FilterFields = {sideNavn: string; filter: string; verdi: string; veilederIdent: string};
+
+type Fields = FilterFields | {} | undefined;
+
+function isFilterFields(fields: Fields): fields is FilterFields {
+    return (fields as FilterFields).filter !== undefined;
+}
 
 export interface FrontendEvent {
     name: string;
     fields?: {};
     tags?: {};
 }
-
-export const logEvent = (logTag: string, fields?: {}, tags?: {}): void => {
-    let filter,
-        verdi = '';
-    if (fields) {
-        const keys = Object.keys(fields);
-        filter = fields[keys[1]];
-        verdi = fields[keys[2]];
-    }
+export const logEvent = (logTag: string, fields?: Fields, tags?: {}): void => {
     if (erMock()) {
         // eslint-disable-next-line no-console
         console.log('Event', logTag, 'Fields:', fields, 'Tags:', tags);
     } else {
-        if (filter === 'innsatsgruppe') {
-            // eslint-disable-next-line no-console
-            console.log('Amplitude data ', filter, verdi);
-            trackAmplitude({name: 'filtervalg', data: {filter, verdi}});
+        if (isFilterFields(fields)) {
+            if (fields?.filter === 'innsatsgruppe') {
+                // eslint-disable-next-line no-console
+                console.log('Amplitude data ', fields.filter, fields.verdi);
+                trackAmplitude({
+                    name: 'filtervalg',
+                    data: {
+                        filternavn: fields?.filter,
+                        kategori: innsatsgruppe[fields.verdi]?.label
+                    }
+                });
+            }
         }
+
         // Funker ikke etter vi flytta til GCP på grunn av influx som bare er i fss?
         // sendEventTilPortefolje({name: logTag, fields: fields, tags: tags});
     }
