a061c50488f322c30f3796199e46b796b3599055
Ingrid Fosså <ingrid.fossa@nav.no>
2024-11-05 15:26:22 +0100

    Fjern arbeidslistefilter

--- cypress/e2e/arbeidslistestatus_spec.js
+++ cypress/e2e/arbeidslistestatus_spec.js
@@ -6,28 +6,10 @@
 });
 
 describe('Arbeidslistestatus', () => {
-
-    it('Sjekk tekst på legg til i / fjern fra arbeidslisteknapp', () => {
-
-        // Vel "Min arbeidsliste"-filteret under Status i sidemenyen
-        cy.getByTestId('filter_checkboks-container_minArbeidsliste').click();
-
-        // Nullstill filter etter test
-        cy.getByTestId('filtreringlabel_min-arbeidsliste').click();
-    });
-
     it('Legg til person i lilla arbeidsliste', () => {
-        // Vel "Min arbeidsliste"-filteret under Status i sidemenyen
-        cy.getByTestId('filter_checkboks-container_minArbeidsliste').click();
 
         // Hentar ut kor mange som er i Lilla arbeidsliste i starten av testen
-        cy.getByTestId('filter_checkboks-label_minArbeidslisteLilla').as('lillaArbeidslistetall').then(antallILillaArbeidslisteFor => {
-            // Nullstill valg av filter "min arbeidsliste"
-            cy.scrollTo('top');
             cy.getByTestId('filtreringlabel_min-arbeidsliste').click();
-
-            // Sjekk at det no er ein meir person i lilla arbeidsliste
-            cy.getByTestId('filter_checkboks-container_minArbeidsliste').click();
         });
     });
 });*/
