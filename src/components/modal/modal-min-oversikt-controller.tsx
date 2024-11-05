37c6c89cc186bf67b4ab28b3a9f21e724569b3b3
Ingrid Fosså <ingrid.fossa@nav.no>
2024-11-05 14:47:01 +0100

    Fjern kode for Legg i / fjern arbeidsliste feilet-modal

--- src/components/modal/modal-min-oversikt-controller.tsx
+++ src/components/modal/modal-min-oversikt-controller.tsx
@@ -1,10 +1,4 @@
-import React from 'react';
-import FeilmeldingBrukereModal from './feilmelding-brukere-modal';
-import {
-    FJERN_FRA_ARBEIDSLISTE_FEILET,
-    LEGG_TIL_ARBEIDSLISTE_FEILET,
-    TILDELING_FEILET
-} from '../../ducks/modal-feilmelding-brukere';
+import {TILDELING_FEILET} from '../../ducks/modal-feilmelding-brukere';
 import ServerFeilModal from './server-feil-modal';
 import {VIS_TILDELING_SUKSESS_MODAL} from '../../ducks/modal';
 import {useModalControllerSelector} from '../../hooks/redux/use-modal-controller.selector';
@@ -41,20 +35,6 @@ export function MinOversiktModalController() {
                 fnrFeil={feilmeldingModal.brukereError}
                 fnrSuksess={feilmeldingModal.brukereOk}
                 onClose={lukkTildelingFeiletModal}
-            />
-            <FeilmeldingBrukereModal
-                isOpen={feilmeldingModal.aarsak === LEGG_TIL_ARBEIDSLISTE_FEILET}
-                fnrFeil={feilmeldingModal.brukereError}
-                onClose={closeFeilmeldingModal}
-                tittelTekst="Handlingen kan ikke utføres"
-                infotekstTekst="Kunne ikke opprette arbeidsliste for følgende bruker(e):"
-            />
-            <FeilmeldingBrukereModal
-                isOpen={feilmeldingModal.aarsak === FJERN_FRA_ARBEIDSLISTE_FEILET}
-                fnrFeil={feilmeldingModal.brukereError}
-                onClose={closeFeilmeldingModal}
-                tittelTekst="Handlingen kan ikke utføres"
-                infotekstTekst="Kunne ikke slette arbeidsliste for følgende bruker(e):"
             />
         </>
     );
