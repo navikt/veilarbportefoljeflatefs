8155820a17e72c5aa7a2ed7bf4e4f6312cba4d7f
Ingrid Fosså <ingrid.fossa@nav.no>
2025-02-04 17:28:23 +0100

    Skriv om heimelaga cypress-kommandoar til TypeScript

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -37,41 +37,60 @@ Cypress.Commands.add('configure', () => {
     cy.getByTestId('enhetens-oversikt').contains('Enhetens oversikt').should('exist');
 });
 
-Cypress.Commands.add('getByTestId', (selector, ...args) => {
-    return cy.get(`[data-testid=${selector}]`, ...args);
+Cypress.Commands.add('getByTestId', (selector) => {
+    return cy.get(`[data-testid=${selector}]`);
 });
 
+/* Ting eg ikkje forstår med denne funksjonen (2025-02-05, Ingrid)
+* - Kvifor forventer vi loader når vi allereie er på ei side? Vi har ikkje trykka på noko her.
+* - Kvifor returnerer vi kalla, i staden for å berre kalle dei? Vi bruker ikkje funksjonen i nøsting uansett.
+*   (Det ville dessutan vore meir fornuftig/lettlest å returnere berre oversikt-elementet.)
+* - Prøver vi nokon gong å gå til ei side vi ikkje er på? Treng vi den koden i det heile? Og om vi prøver, er ikkje
+*   rett oppførsel at vi trykkar på ting?
+* */
 Cypress.Commands.add('gaTilOversikt', side => {
     if (side === 'min-oversikt') {
+        // Om vi ikkje er på den valgte oversiktsida frå før
         if (cy.getByTestId(side).should('not.have.class', '.oversiktslenke--valgt')) {
             return (
+                // trykk på den og sjekk at vi får rett url
                 cy.getByTestId(side).click({force: true}) &&
                 cy.url().should('include', '/veilarbportefoljeflatefs/portefolje')
             );
         }
+        // Om vi er på sida allereie
         return (
+            // Sjekker at vi ser loader og at url er rett
             cy.get('.navds-loader').should('be.visible') &&
             cy.url().should('include', '/veilarbportefoljeflatefs/portefolje')
         );
     } else if (side === 'enhetens-oversikt') {
+        // Om vi ikkje er på den valgte oversiktsida frå før
         if (cy.getByTestId(side).should('not.have.class', '.oversiktslenke--valgt')) {
             return (
+                // trykk på den og sjekk at vi får rett url
                 cy.getByTestId(side).click({force: true}) &&
                 cy.url().should('include', '/veilarbportefoljeflatefs/enhet')
             );
         }
+        // Om vi er på sida allereie
         return (
+            // Sjekker at vi ser loader og at url er rett
             cy.get('.navds-loader').should('be.visible') &&
             cy.url().should('include', '/veilarbportefoljeflatefs/enhet')
         );
     } else if (side === 'veileder-oversikt') {
+        // Om vi ikkje er på den valgte oversiktsida frå før
         if (cy.getByTestId(side).should('not.have.class', '.oversiktslenke--valgt')) {
+            // trykk på den og sjekk at vi får rett url
             return (
                 cy.getByTestId(side).click({force: true}) &&
                 cy.url().should('include', '/veilarbportefoljeflatefs/veiledere')
             );
         }
+        // Om vi er på sida allereie
         return (
+            // Sjekker at vi ser loader og at url er rett
             cy.get('.navds-loader').should('be.visible') &&
             cy.url().should('include', '/veilarbportefoljeflatefs/veiledere')
         );
@@ -91,7 +110,7 @@ Cypress.Commands.add('klikkPaSidebarTab', tab => {
     cy.getByTestId(`sidebar-tab_${tab}`).click({force: true})
 })
 
-Cypress.Commands.add('klikkTab', tab => {
+Cypress.Commands.add('klikkTab', (tab) => {
     if (tab === 'VEILEDERGRUPPER') {
         if (cy.faneErLukket('VEILEDERGRUPPER')) {
             return (
