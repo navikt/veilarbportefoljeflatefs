4b6f224165e208f06b78461ac2f57b9ca73b863b
JulieHillRoa <julie.hill.roa@nav.no>
2025-02-25 10:25:11 +0100

    Fjerner konstanten som refererer til ubrukt bryter og som blir overskrevet

--- src/konstanter.ts
+++ src/konstanter.ts
@@ -7,8 +7,9 @@ export const IKKE_SATT = 'ikke_satt';
 export const DEFAULT_PAGINERING_STORRELSE = 50;
 export const SE_FLERE_PAGINERING_STORRELSE = 200;
 
+export const VEDTAKSTOTTE = 'VEDTAKSTOTTE';
+
 export const SPOR_OM_TILBAKEMELDING = 'veilarbportefoljeflatefs.spor_om_tilbakemelding';
-export const VEDTAKSTOTTE = 'pto.vedtaksstotte.pilot';
 export const DARKMODE = 'veilarbportefoljeflatefs.darkmode';
 export const ALERTSTRIPE_FEILMELDING = 'veilarbportefoljeflatefs.alertstripe_feilmelding';
 export const VIS_MELDING_OM_BRUKERE_MED_ADRESSEBESKYTTELSE_ELLER_SKJERMING =
