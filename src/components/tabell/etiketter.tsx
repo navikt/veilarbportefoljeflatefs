e902a35d4f174d7cc661cbe7df0f532cde97286c
Ingrid Fosså <ingrid.fossa@nav.no>
2025-04-02 15:35:34 +0200

    Gjer det lettare å sjå frå koden kva etikettar som skal visast ved å snu logikken frå "skjul dersom dette er sant" til "vis dersom dette er sant"

--- src/components/tabell/etiketter.tsx
+++ src/components/tabell/etiketter.tsx
@@ -1,6 +1,5 @@
 import {Tag} from '@navikt/ds-react';
 import {BrukerModell, VurderingsBehov} from '../../model-interfaces';
-import hiddenIf from '../hidden-if/hidden-if';
 import {hentSkjermetInfo} from '../../utils/dato-utils';
 
 interface EtiketterProps {
@@ -8,80 +7,60 @@ interface EtiketterProps {
 }
 
 function Etiketter({bruker}: EtiketterProps) {
-    const HiddenEtikett = hiddenIf(Tag);
     const skjermetInfo = hentSkjermetInfo(bruker.egenAnsatt, bruker.skjermetTil);
 
     return (
         <>
-            <HiddenEtikett variant="info" size="small" hidden={!bruker.erDoed} className="tabell-etikett etikett--doed">
-                Død
-            </HiddenEtikett>
-            <HiddenEtikett
-                variant="warning"
-                size="small"
-                hidden={!bruker.sikkerhetstiltak || bruker.sikkerhetstiltak.length === 0}
-                className="tabell-etikett"
-            >
-                Sikkerhetstiltak
-            </HiddenEtikett>
-            <HiddenEtikett variant="warning" size="small" hidden={!bruker.diskresjonskode} className="tabell-etikett">
-                {`Kode ${bruker.diskresjonskode}`}
-            </HiddenEtikett>
-            <HiddenEtikett
-                variant={skjermetInfo.type}
-                size="small"
-                hidden={skjermetInfo.hidden}
-            >{`${skjermetInfo.tittel}`}</HiddenEtikett>
-            <HiddenEtikett
-                variant="info"
-                size="small"
-                hidden={bruker.vurderingsBehov !== VurderingsBehov.IKKE_VURDERT}
-                className="tabell-etikett"
-            >
-                Trenger vurdering
-            </HiddenEtikett>
-            <HiddenEtikett
-                variant="info"
-                size="small"
-                hidden={bruker.vurderingsBehov !== VurderingsBehov.ARBEIDSEVNE_VURDERING}
-                className="tabell-etikett"
-            >
-                Behov for AEV
-            </HiddenEtikett>
-            <HiddenEtikett
-                variant="info"
-                size="small"
-                hidden={!bruker.trengerVurdering || bruker.vurderingsBehov !== VurderingsBehov.OPPGITT_HINDRINGER}
-                className="tabell-etikett"
-            >
-                Oppgitt hindringer
-            </HiddenEtikett>
-            <HiddenEtikett
-                variant="info"
-                size="small"
-                hidden={!bruker.trengerVurdering || bruker.vurderingsBehov !== VurderingsBehov.ANTATT_GODE_MULIGHETER}
-                className="tabell-etikett"
-            >
-                Antatt gode muligheter
-            </HiddenEtikett>
-            <HiddenEtikett
-                variant="info"
-                size="small"
-                hidden={
-                    !bruker.trengerVurdering || bruker.vurderingsBehov !== VurderingsBehov.ANTATT_BEHOV_FOR_VEILEDNING
-                }
-                className="tabell-etikett"
-            >
-                Antatt behov for veiledning
-            </HiddenEtikett>
-            <HiddenEtikett
-                variant="info"
-                size="small"
-                hidden={!bruker.erSykmeldtMedArbeidsgiver}
-                className="tabell-etikett"
-            >
-                Sykmeldt
-            </HiddenEtikett>
+            {bruker.erDoed && (
+                <Tag variant="info" size="small" className="tabell-etikett etikett--doed">
+                    Død
+                </Tag>
+            )}
+            {bruker.sikkerhetstiltak && bruker.sikkerhetstiltak.length !== 0 && (
+                <Tag variant="warning" size="small" className="tabell-etikett">
+                    Sikkerhetstiltak
+                </Tag>
+            )}
+            {bruker.diskresjonskode && (
+                <Tag variant="warning" size="small" className="tabell-etikett">
+                    {`Kode ${bruker.diskresjonskode}`}
+                </Tag>
+            )}
+            {!skjermetInfo.hidden && (
+                <Tag variant={skjermetInfo.type} size="small">
+                    {`${skjermetInfo.tittel}`}
+                </Tag>
+            )}
+            {bruker.vurderingsBehov === VurderingsBehov.IKKE_VURDERT && (
+                <Tag variant="info" size="small" className="tabell-etikett">
+                    Trenger vurdering
+                </Tag>
+            )}
+            {bruker.vurderingsBehov === VurderingsBehov.ARBEIDSEVNE_VURDERING && (
+                <Tag variant="info" size="small" className="tabell-etikett">
+                    Behov for AEV
+                </Tag>
+            )}
+            {bruker.trengerVurdering && bruker.vurderingsBehov === VurderingsBehov.OPPGITT_HINDRINGER && (
+                <Tag variant="info" size="small" className="tabell-etikett">
+                    Oppgitt hindringer
+                </Tag>
+            )}
+            {bruker.trengerVurdering && bruker.vurderingsBehov === VurderingsBehov.ANTATT_GODE_MULIGHETER && (
+                <Tag variant="info" size="small" className="tabell-etikett">
+                    Antatt gode muligheter
+                </Tag>
+            )}
+            {bruker.trengerVurdering && bruker.vurderingsBehov === VurderingsBehov.ANTATT_BEHOV_FOR_VEILEDNING && (
+                <Tag variant="info" size="small" className="tabell-etikett">
+                    Antatt behov for veiledning
+                </Tag>
+            )}
+            {bruker.erSykmeldtMedArbeidsgiver && (
+                <Tag variant="info" size="small" className="tabell-etikett">
+                    Sykmeldt
+                </Tag>
+            )}
         </>
     );
 }
