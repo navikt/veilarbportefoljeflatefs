cf0fdf97056a67dce52f189b78173a50d84b9873
Ingrid Fosså <ingrid.fossa@nav.no>
2025-04-10 11:03:55 +0200

    Bytt til Aksel-tabell i veilederoversikten
    Betre UU, mindre kode og vedlikehald.

--- src/veilederoversikt/veilederoversikt-tabell.tsx
+++ src/veilederoversikt/veilederoversikt-tabell.tsx
@@ -1,8 +1,8 @@
 import classNames from 'classnames';
-import {Button} from '@navikt/ds-react';
+import {Button, Table} from '@navikt/ds-react';
 import {ArrowDownIcon, ArrowUpIcon} from '@navikt/aksel-icons';
 import {VeilederoversiktTabellrad} from './veilederoversikt-tabellrad';
-import './veilederoversikt.css';
+import './veilederoversikt-tabell.css';
 
 interface VeiledereTabellProps {
     veiledere: any;
@@ -45,70 +45,59 @@ export function VeilederoversiktTabell({
     };
 
     return (
-        <div className="veilederoversikt__toolbar-container">
-            <span className="veilederoversikt__sticky-skygge">
-                <table className="veileder-tabell">
-                    <thead>
-                        <tr>
-                            <th scope="col" className="tabellheader">
-                                <div className="tabellheader__lenke tabellheader__tekst">
-                                    <Button
-                                        size="xsmall"
-                                        variant="tertiary"
-                                        onClick={sorterPaaEtternavn}
-                                        className={classNames('lenke lenke--frittstaende', {
-                                            'valgt-sortering': sorterEtternavn
-                                        })}
-                                        aria-pressed={sorterEtternavn}
-                                        aria-label={
-                                            sorterEtternavn
-                                                ? `Etternavn, ${currentSortering.direction} rekkefølge`
-                                                : 'Etternavn, ingen sortering'
-                                        }
-                                    >
-                                        Etternavn
-                                    </Button>
-                                    , Fornavn
-                                    {sorteringspil(sorterEtternavn)}
-                                </div>
-                            </th>
-                            <th scope="col" className="tabellheader__tekst">
-                                Nav-ident
-                            </th>
-                            <th className="tabellheader tabell-element-center" scope="col">
-                                <div
-                                    className="tabellheader__lenke"
-                                    data-testid="veilederoversikt_sortering_antall-brukere"
-                                >
-                                    <Button
-                                        size="xsmall"
-                                        variant="tertiary"
-                                        onClick={sorterPaaPortefoljestorrelse}
-                                        className={classNames('lenke lenke--frittstaende tabellheader__tekst', {
-                                            'valgt-sortering': sorterPaaPortefoljeStr
-                                        })}
-                                        aria-pressed={sorterPaaPortefoljeStr}
-                                        aria-label={
-                                            sorterPaaPortefoljeStr
-                                                ? `Antall brukere, ${currentSortering.direction} rekkefølge`
-                                                : 'Antall brukere, ingen sortering'
-                                        }
-                                    >
-                                        Antall brukere
-                                    </Button>
-                                    {sorteringspil(sorterPaaPortefoljeStr)}
-                                </div>
-                            </th>
-                            <th />
-                        </tr>
-                    </thead>
-                    <tbody data-testid="veilederoversikt_veilederliste_tbody">
-                        {veiledere.map((veileder: any) => (
-                            <VeilederoversiktTabellrad veileder={veileder} key={veileder.ident} />
-                        ))}
-                    </tbody>
-                </table>
-            </span>
-        </div>
+        <Table className="veileder-tabell" zebraStripes={true}>
+            <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell>
+                        <div className="veiledertabell__sorteringskolonne">
+                            <Button
+                                size="xsmall"
+                                variant="tertiary"
+                                onClick={sorterPaaEtternavn}
+                                className={classNames({'valgt-sortering': sorterEtternavn})}
+                                aria-pressed={sorterEtternavn}
+                                aria-label={
+                                    sorterEtternavn
+                                        ? `Etternavn, ${currentSortering.direction} rekkefølge`
+                                        : 'Etternavn, ingen sortering'
+                                }
+                            >
+                                Etternavn
+                            </Button>
+                            , Fornavn
+                            {sorteringspil(sorterEtternavn)}
+                        </div>
+                    </Table.HeaderCell>
+                    <Table.HeaderCell>Nav-ident</Table.HeaderCell>
+                    <Table.HeaderCell>
+                        <div
+                            className="veiledertabell__sorteringskolonne"
+                            data-testid="veilederoversikt_sortering_antall-brukere"
+                        >
+                            <Button
+                                size="xsmall"
+                                variant="tertiary"
+                                onClick={sorterPaaPortefoljestorrelse}
+                                className={classNames({'valgt-sortering': sorterPaaPortefoljeStr})}
+                                aria-pressed={sorterPaaPortefoljeStr}
+                                aria-label={
+                                    sorterPaaPortefoljeStr
+                                        ? `Antall brukere, ${currentSortering.direction} rekkefølge`
+                                        : 'Antall brukere, ingen sortering'
+                                }
+                            >
+                                Antall brukere
+                            </Button>
+                            {sorteringspil(sorterPaaPortefoljeStr)}
+                        </div>
+                    </Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
+            <Table.Body data-testid="veilederoversikt_veilederliste_tbody">
+                {veiledere.map((veileder: any) => (
+                    <VeilederoversiktTabellrad veileder={veileder} key={veileder.ident} />
+                ))}
+            </Table.Body>
+        </Table>
     );
 }
