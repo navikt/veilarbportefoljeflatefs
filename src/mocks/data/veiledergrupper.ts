16612b4d19524cf2abac007d097c8e05260daad6
Ingrid Fosså <ingrid.fossa@nav.no>
2025-05-27 13:32:09 +0200

    Unngå å caste mockdata til typar for Mine filter og Veiledergrupper til LagretFilter-typen og legg til felt som mangla då vi casta til typen.

--- src/mocks/data/veiledergrupper.ts
+++ src/mocks/data/veiledergrupper.ts
@@ -3,7 +3,7 @@ import {initialState} from '../../ducks/filtrering';
 import {veiledere} from './veiledere';
 import {LagretFilter} from '../../ducks/lagret-filter';
 
-export const veiledergrupper = () => {
+export const veiledergrupper = (): LagretFilter[] => {
     const veilederGruppe1 = veiledere.slice(0, 4).map(v => v.ident);
     const veilederGruppe2 = veiledere.slice(5, 10).map(v => v.ident);
     const veilederGruppe3 = veiledere.slice(11, 15).map(v => v.ident);
@@ -21,42 +21,60 @@ export const veiledergrupper = () => {
             filterId: 12,
             filterValg: {...initialState, veiledere: veilederGruppe1},
             opprettetDato: faker.date.between(datointervall),
-            filterCleanup: false
+            sortOrder: null,
+            filterCleanup: false,
+            aktiv: true,
+            note: ''
         },
         {
             filterNavn: 'Prinsessegruppen',
             filterId: 13,
             filterValg: {...initialState, veiledere: veilederGruppe2},
             opprettetDato: faker.date.between(datointervall),
-            filterCleanup: false
+            sortOrder: null,
+            filterCleanup: false,
+            aktiv: true,
+            note: ''
         },
         {
             filterNavn: 'Team Awesome',
             filterId: 14,
             filterValg: {...initialState, veiledere: veilederGruppe3},
             opprettetDato: faker.date.between(datointervall),
-            filterCleanup: false
+            sortOrder: null,
+            filterCleanup: false,
+            aktiv: true,
+            note: ''
         },
         {
             filterNavn: 'Team VOFF',
             filterId: 15,
             filterValg: {...initialState, veiledere: veilederGruppe4},
             opprettetDato: faker.date.between(datointervall),
-            filterCleanup: false
+            sortOrder: null,
+            filterCleanup: false,
+            aktiv: true,
+            note: ''
         },
         {
             filterNavn: 'Ungdomsavdelingen',
             filterId: 16,
             filterValg: {...initialState, veiledere: veilederGruppe5},
             opprettetDato: faker.date.between(datointervall),
-            filterCleanup: false
+            sortOrder: null,
+            filterCleanup: false,
+            aktiv: true,
+            note: ''
         },
         {
             filterNavn: 'Gruppen brukes til test la stå',
             filterId: 17,
             filterValg: {...initialState, veiledere: veilederGruppe5},
             opprettetDato: faker.date.between(datointervall),
-            filterCleanup: true
+            sortOrder: null,
+            filterCleanup: true,
+            aktiv: true,
+            note: ''
         }
-    ] as LagretFilter[];
+    ];
 };
