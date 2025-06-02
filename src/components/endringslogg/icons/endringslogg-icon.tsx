a9a4d6dd03c66d17bd619cfb343061734eb3d63b
Ingrid Fosså <ingrid.fossa@nav.no>
2025-06-02 15:57:28 +0200

    Bytt til designsystemfarge for endringslogg-ikon
    Og fjern siste spor av fargetema frå komponenten

--- src/components/endringslogg/icons/endringslogg-icon.tsx
+++ src/components/endringslogg/icons/endringslogg-icon.tsx
@@ -6,10 +6,7 @@ interface Props {
     width?: string;
 }
 
-const farger = {
-    mørk: '#3e3832',
-    lys: '#f1f1f1'
-};
+const ikonfarge = '--a-gray-800';
 
 export const EndringsloggIkon: FC<Props> = ({className, height, width}) => (
     <svg
@@ -24,7 +21,7 @@ export const EndringsloggIkon: FC<Props> = ({className, height, width}) => (
         <path
             d="M28 3.79a3.93 3.93 0 0 1 4 4v14a3.92 3.92 0 0 1-4 4H17.08v2h5.06a1.07 1.07 0 1 1 0 2.13h-12a1.07 1.07 0 1 1 0-2.13h5.06v-2H4a3.92 3.92 0 0 1-4-4v-5.93a1 1 0 0 1 1-1.2 1.17 1.17 0 0 1 1 1.2v3.92h28v-12a2.05 2.05 0 0 0-2-2H15.21a1.07 1.07 0 0 1 0-2.13Zm0 20a2 2 0 0 0 2-2H2a2 2 0 0 0 2 2Z"
             style={{
-                fill: farger.mørk
+                fill: ikonfarge
             }}
             data-name="Outline/16-transfers/transfer-computers-2"
         />
@@ -32,10 +29,10 @@ export const EndringsloggIkon: FC<Props> = ({className, height, width}) => (
             data-name="Combined-Shape"
             d="M1.31 9.11H1.1a.62.62 0 0 1-.63-.61 6.21 6.21 0 0 1 4-6 6.81 6.81 0 0 1 5.44.33L9.57 2a.64.64 0 0 1 .36-.81.63.63 0 0 1 .8.37l1.1 2.91a.62.62 0 0 1-.36.8l-2.9 1.1A.63.63 0 0 1 7.76 6a.62.62 0 0 1 .36-.8l2-.74a5.43 5.43 0 0 0-5.15-.71 5 5 0 0 0-3.26 4.76.63.63 0 0 1-.4.6Zm11.52-1.7a.62.62 0 0 1 .63.61 6.22 6.22 0 0 1-4 6A6.77 6.77 0 0 1 4 13.64l.35.93A.62.62 0 0 1 3.2 15l-1.11-2.91a.62.62 0 0 1 .37-.8l2.9-1.1a.63.63 0 0 1 .44 1.17l-1.94.73a5.46 5.46 0 0 0 5.14.72A5 5 0 0 0 12.22 8a.61.61 0 0 1 .61-.59Z"
             style={{
-                stroke: farger.mørk,
+                stroke: ikonfarge,
                 strokeMiterlimit: 10,
                 strokeWidth: '.74px',
-                fill: farger.mørk
+                fill: ikonfarge
             }}
         />
     </svg>
