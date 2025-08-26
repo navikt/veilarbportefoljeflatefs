46c0838005cb004cb33e9aae7f0529ca43e919ef
Ingrid Fosså <ingrid.fossa@nav.no>
2025-08-26 16:20:40 +0200

    Lagar gjenbrukbar hjelpefunksjon for å formatere varigheit

--- src/test/dato-utils.test.ts
+++ src/test/dato-utils.test.ts
@@ -1,4 +1,4 @@
-import {klokkeslettTilMinutter, minuttDifferanse} from '../utils/dato-utils';
+import {formaterVarighetSomTimerOgMinutt, klokkeslettTilMinutter, minuttDifferanse} from '../utils/dato-utils';
 
 describe('dato utils test', () => {
     describe('omvandler allt til minutter dersom tidskolonne skall kunne vare mer generisk', () => {
@@ -15,6 +15,20 @@ describe('dato utils test', () => {
             date2.setMinutes(date.getMinutes() + 30);
             const minutter = minuttDifferanse(date2, date);
             expect(minutter).toBe(30);
+        });
+
+        it('skal formattere varighet rett som timer og minutt', () => {
+            const bareMinutt = 15;
+            const forventetTekstBareMinutt = '15min';
+            expect(formaterVarighetSomTimerOgMinutt(bareMinutt)).toBe(forventetTekstBareMinutt);
+
+            const helTime = 60;
+            const forventetTekstHelTime = '1t';
+            expect(formaterVarighetSomTimerOgMinutt(helTime)).toBe(forventetTekstHelTime);
+
+            const timeOgMinutt = 75;
+            const forventetTekstTimeOgMinutt = '1t 15min';
+            expect(formaterVarighetSomTimerOgMinutt(timeOgMinutt)).toBe(forventetTekstTimeOgMinutt);
         });
     });
 });
