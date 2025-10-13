9905a29156d58625517fec50ec3124860aeb19c4
Ingrid Fosså <ingrid.fossa@nav.no>
2025-10-13 12:13:05 +0200

    Gjer eit par av concat-ane for Enhetens oversikt bittelitt lettare å lese
    ved å gje tydelegare namn på konstantar

--- src/ducks/ui/listevisning-selectors.ts
+++ src/ducks/ui/listevisning-selectors.ts
@@ -121,7 +121,9 @@ export function getMuligeKolonner(filtervalg: FiltervalgModell, oversiktType: Ov
     const filtrertPaYtelseMedRettighetsperiode =
         filtervalg.ytelse === YTELSE_ARENA_AAP || filtervalg.ytelse === YTELSE_ARENA_AAP_ORDINAR;
 
-    const ikkeFiltrertPaMoterIDagIMinOversikt = oversiktType === OversiktType.enhetensOversikt && !filtrertPaMoterIDag;
+    const erPaEnhetensOversikt = oversiktType === OversiktType.enhetensOversikt;
+
+    const erPaEnhetensOversiktOgIkkeFiltrertPaMoterIDag = erPaEnhetensOversikt && !filtrertPaMoterIDag;
 
     const filtrertPaEnsligForsorger = !!filtervalg.ensligeForsorgere.length;
 
@@ -162,7 +164,7 @@ export function getMuligeKolonner(filtervalg: FiltervalgModell, oversiktType: Ov
         .concat(addHvis(Kolonne.SISTE_ENDRING, filtrertPaSisteEndringKategori))
         .concat(addHvis(Kolonne.SISTE_ENDRING_DATO, filtrertPaSisteEndringKategori))
         .concat(addHvis(Kolonne.MOTER_IDAG, filtrertPaMoterIDag))
-        .concat(addHvis(Kolonne.VEILEDER, filtrertPaMoterIDag && oversiktType === OversiktType.enhetensOversikt))
+        .concat(addHvis(Kolonne.VEILEDER, filtrertPaMoterIDag && erPaEnhetensOversikt))
         .concat(addHvis(Kolonne.MOTER_VARIGHET, filtrertPaMoterIDag))
         .concat(addHvis(Kolonne.MOTE_ER_AVTALT, filtrertPaMoterIDag))
         .concat(addHvis(Kolonne.UTLOPTE_AKTIVITETER, filtervalg.ferdigfilterListe.includes(UTLOPTE_AKTIVITETER)))
@@ -197,7 +199,7 @@ export function getMuligeKolonner(filtervalg: FiltervalgModell, oversiktType: Ov
         .concat(addHvis(Kolonne.YTELSE_ARENA_YTELSESTYPE_AAP, filtrertPaAAPYtelse))
         .concat(addHvis(Kolonne.YTELSE_ARENA_VEDTAKSPERIODE_AAP, filtrertPaYtelseMedVedtaksperiode))
         .concat(addHvis(Kolonne.YTELSE_ARENA_RETTIGHETSPERIODE_AAP, filtrertPaYtelseMedRettighetsperiode))
-        .concat(addHvis(Kolonne.VEILEDER, ikkeFiltrertPaMoterIDagIMinOversikt))
+        .concat(addHvis(Kolonne.VEILEDER, erPaEnhetensOversiktOgIkkeFiltrertPaMoterIDag))
         .concat(addHvis(Kolonne.YTELSE_ARENA_VURDERINGSFRIST_AAP, filtrertPaAAPYtelse))
         .concat(addHvis(Kolonne.YTELSE_ARENA_VEDTAKSPERIODE_AAP, filtervalg.ytelse === YTELSE_ARENA_AAP_ORDINAR))
         .concat(addHvis(Kolonne.YTELSE_ARENA_RETTIGHETSPERIODE_AAP, filtervalg.ytelse === YTELSE_ARENA_AAP_UNNTAK))
@@ -205,7 +207,7 @@ export function getMuligeKolonner(filtervalg: FiltervalgModell, oversiktType: Ov
         .concat(addHvis(Kolonne.ENSLIGE_FORSORGERE_VEDTAKSPERIODE, filtrertPaEnsligForsorger))
         .concat(addHvis(Kolonne.ENSLIGE_FORSORGERE_AKIVITETSPLIKT, filtrertPaEnsligForsorger))
         .concat(addHvis(Kolonne.ENSLIGE_FORSORGERE_OM_BARNET, filtrertPaEnsligForsorger))
-        .concat(addHvis(Kolonne.NAVIDENT, oversiktType === OversiktType.enhetensOversikt))
+        .concat(addHvis(Kolonne.NAVIDENT, erPaEnhetensOversikt))
         .concat(addHvis(Kolonne.CV_SVARFRIST, filtervalg.stillingFraNavFilter.length !== 0))
         .concat(addHvis(Kolonne.BOSTED_SIST_OPPDATERT, filtrertPaGeografiskBosted))
         .concat(addHvis(Kolonne.BARN_UNDER_18_AAR, filtrertPaBarnUnder18Ar))
