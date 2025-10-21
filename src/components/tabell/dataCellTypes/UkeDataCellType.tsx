280718c81e956abadf44d3ea82515d6f3cc9faef
Ingrid Fosså <ingrid.fossa@nav.no>
2025-10-21 15:51:19 +0200

    Flytt tekst-logikk for uker inn i komponenten

--- src/components/tabell/dataCellTypes/UkeDataCellType.tsx
+++ src/components/tabell/dataCellTypes/UkeDataCellType.tsx
@@ -1,15 +1,6 @@
 import {BodyShort} from '@navikt/ds-react';
 import {DataCellTypeProps} from './DataCellTypeProps';
 
-function lagUkerTekst(ukerIgjen, minVal) {
-    if (ukerIgjen < 0 || ukerIgjen === undefined) {
-        return null;
-    } else if (ukerIgjen < minVal) {
-        return `Under ${minVal} uker`;
-    }
-    return `${ukerIgjen} uker`;
-}
-
 interface Props extends DataCellTypeProps {
     ukerIgjen?: number;
     minVal: number;
@@ -19,6 +10,16 @@ export function UkeDataCellType({ukerIgjen, minVal, skalVises, className}: Props
     if (!skalVises) {
         return null;
     }
+
+    const lagUkerTekst = (ukerIgjen, minVal) => {
+        if (ukerIgjen < 0 || ukerIgjen === undefined) {
+            return null;
+        } else if (ukerIgjen < minVal) {
+            return `Under ${minVal} uker`;
+        }
+        return `${ukerIgjen} uker`;
+    };
+
     return (
         <BodyShort size="small" className={className}>
             {lagUkerTekst(ukerIgjen, minVal)}
