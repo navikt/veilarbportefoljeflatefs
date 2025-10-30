f4b74645fd4fa5e4ee4f67f215aad3962f72a104
Ingrid Fosså <ingrid.fossa@nav.no>
2025-10-30 16:51:43 +0100

    Flytt fleire filter til switch

--- src/filtrering/filtrering-label/LagLabelForFiltervalg.tsx
+++ src/filtrering/filtrering-label/LagLabelForFiltervalg.tsx
@@ -5,9 +5,12 @@ import {
     aapIKelvinFilter,
     aktiviteter,
     AktiviteterValg,
+    alder,
+    fargekategorier,
     filterKonstanter,
     HAR_AVVIK,
     hendelserEtikett,
+    registreringstype,
     tiltakspengerFilter,
     TiltakspengerFilter,
     utdanning,
@@ -90,82 +93,88 @@ export const LagLabelForFiltervalg = ({
                 );
             });
         }
+        case Filtervalg.registreringstype: {
+            return valgteFilteralternativer.map(valgtAlternativ => {
+                if (valgtAlternativ === 'INGEN_DATA') {
+                    return (
+                        <FiltreringLabel
+                            key={`situasjon-${valgtAlternativ}`}
+                            label={`Situasjon: ${registreringstype[valgtAlternativ].label}`}
+                            slettFilter={() => slettEnkeltfilter(valgtFilter, valgtAlternativ)}
+                        />
+                    );
+                }
+                return (
+                    <FiltreringLabel
+                        key={`situasjon-${valgtAlternativ}`}
+                        label={registreringstype[valgtAlternativ]}
+                        slettFilter={() => slettEnkeltfilter(valgtFilter, valgtAlternativ)}
+                    />
+                );
+            });
+        }
+        case Filtervalg.sisteEndringKategori: {
+            return valgteFilteralternativer.map(valgtAlternativ => {
+                return (
+                    <FiltreringLabel
+                        key={`hendelser-${valgtAlternativ}`}
+                        label={hendelserEtikett[valgtAlternativ]}
+                        slettFilter={() => slettEnkeltfilter(valgtFilter, valgtAlternativ)}
+                    />
+                );
+            });
+        }
+        case Filtervalg.fodselsdagIMnd: {
+            return valgteFilteralternativer.map(valgtAlternativ => {
+                return (
+                    <FiltreringLabel
+                        key={`fodselsdagIMnd-${valgtAlternativ}`}
+                        label={`Fødselsdato: ${valgtAlternativ}`}
+                        slettFilter={() => slettEnkeltfilter(valgtFilter, valgtAlternativ)}
+                    />
+                );
+            });
+        }
+        case Filtervalg.alder: {
+            return valgteFilteralternativer.map(valgtAlternativ => {
+                return (
+                    <FiltreringLabel
+                        key={`${valgtFilter}--${valgtAlternativ.key || valgtAlternativ}`}
+                        label={alder[valgtAlternativ] || valgtAlternativ + ' år'}
+                        slettFilter={() => slettEnkeltfilter(valgtFilter, valgtAlternativ.key || valgtAlternativ)}
+                    />
+                );
+            });
+        }
+        case Filtervalg.fargekategorier: {
+            return valgteFilteralternativer.map(valgtAlternativ => {
+                return (
+                    <FiltreringLabelMedIkon
+                        key={valgtAlternativ}
+                        label={fargekategorier[valgtAlternativ]}
+                        slettFilter={() => slettEnkeltfilter(valgtFilter, valgtAlternativ)}
+                        ikon={fargekategoriIkonMapper(valgtAlternativ, 'fargekategoriikon')}
+                        tittel={`Fjern filtervalg "Kategori ${fargekategorier[valgtAlternativ]}"`}
+                    />
+                );
+            });
+        }
+        case Filtervalg.aktiviteterForenklet: {
+            return valgteFilteralternativer.map(valgtAlternativ => {
+                return (
+                    <FiltreringLabel
+                        key={valgtAlternativ}
+                        label={aktiviteter[valgtAlternativ]}
+                        slettFilter={() => slettEnkeltfilter(valgtFilter, valgtAlternativ)}
+                    />
+                );
+            });
+        }
         default:
             break;
     }
 
-    if (valgtFilter === Filtervalg.registreringstype) {
-        return valgteFilteralternativer.map(valgtAlternativ => {
-            if (valgtAlternativ === 'INGEN_DATA') {
-                return (
-                    <FiltreringLabel
-                        key={`situasjon-${valgtAlternativ}`}
-                        label={`Situasjon: ${filterKonstanter[valgtFilter][valgtAlternativ].label}`}
-                        slettFilter={() => slettEnkeltfilter(valgtFilter, valgtAlternativ)}
-                    />
-                );
-            }
-            return (
-                <FiltreringLabel
-                    key={`situasjon-${valgtAlternativ}`}
-                    label={filterKonstanter[valgtFilter][valgtAlternativ]}
-                    slettFilter={() => slettEnkeltfilter(valgtFilter, valgtAlternativ)}
-                />
-            );
-        });
-    } else if (valgtFilter === Filtervalg.sisteEndringKategori) {
-        return valgteFilteralternativer.map(valgtAlternativ => {
-            return (
-                <FiltreringLabel
-                    key={`hendelser-${valgtAlternativ}`}
-                    label={hendelserEtikett[valgtAlternativ]}
-                    slettFilter={() => slettEnkeltfilter(valgtFilter, valgtAlternativ)}
-                />
-            );
-        });
-    } else if (valgtFilter === Filtervalg.fodselsdagIMnd) {
-        return valgteFilteralternativer.map(valgtAlternativ => {
-            return (
-                <FiltreringLabel
-                    key={`fodselsdagIMnd-${valgtAlternativ}`}
-                    label={`Fødselsdato: ${valgtAlternativ}`}
-                    slettFilter={() => slettEnkeltfilter(valgtFilter, valgtAlternativ)}
-                />
-            );
-        });
-    } else if (valgtFilter === Filtervalg.alder) {
-        return valgteFilteralternativer.map(valgtAlternativ => {
-            return (
-                <FiltreringLabel
-                    key={`${valgtFilter}--${valgtAlternativ.key || valgtAlternativ}`}
-                    label={filterKonstanter[valgtFilter][valgtAlternativ] || valgtAlternativ + ' år'}
-                    slettFilter={() => slettEnkeltfilter(valgtFilter, valgtAlternativ.key || valgtAlternativ)}
-                />
-            );
-        });
-    } else if (valgtFilter === Filtervalg.fargekategorier) {
-        return valgteFilteralternativer.map(valgtAlternativ => {
-            return (
-                <FiltreringLabelMedIkon
-                    key={valgtAlternativ}
-                    label={filterKonstanter.fargekategorier[valgtAlternativ]}
-                    slettFilter={() => slettEnkeltfilter(valgtFilter, valgtAlternativ)}
-                    ikon={fargekategoriIkonMapper(valgtAlternativ, 'fargekategoriikon')}
-                    tittel={`Fjern filtervalg "Kategori ${filterKonstanter.fargekategorier[valgtAlternativ]}"`}
-                />
-            );
-        });
-    } else if (valgtFilter === Filtervalg.aktiviteterForenklet) {
-        return valgteFilteralternativer.map(valgtAlternativ => {
-            return (
-                <FiltreringLabel
-                    key={valgtAlternativ}
-                    label={aktiviteter[valgtAlternativ]}
-                    slettFilter={() => slettEnkeltfilter(valgtFilter, valgtAlternativ)}
-                />
-            );
-        });
-    } else if (valgtFilter === Filtervalg.ulesteEndringer && valgteFilteralternativer === 'ULESTE_ENDRINGER') {
+    if (valgtFilter === Filtervalg.ulesteEndringer && valgteFilteralternativer === 'ULESTE_ENDRINGER') {
         return [
             <FiltreringLabel
                 key={valgtFilter}
