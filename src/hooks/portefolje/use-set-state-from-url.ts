4a9e9946914389d5242fc3c1714b6f46b242fb51
Ingrid Fosså <ingrid.fossa@nav.no>
2025-11-13 16:38:20 +0100

    Fjern ubrukt logging av side
    Inkludert logging av "inne på annan veileder si side" og logikk for å kryptere identane til desse veiledarane.

--- src/hooks/portefolje/use-set-state-from-url.ts
+++ src/hooks/portefolje/use-set-state-from-url.ts
@@ -1,24 +1,15 @@
 import {useCallback} from 'react';
 import {useDispatch} from 'react-redux';
-import {useLocation, useParams} from 'react-router';
-import {Side} from '../../utils/metrikker/skjerm-metrikker';
-import {loggSideVisning} from '../../utils/metrikker/side-visning-metrikker';
 import {getInitialStateFromUrl} from '../../utils/url-utils';
 import {pagineringSetup} from '../../ducks/paginering';
 import {settSortering} from '../../ducks/portefolje';
 import {useIdentSelector} from '../redux/use-innlogget-ident';
 import {useOnMount} from '../use-on-mount';
 import {logBrowserMetrikker} from '../../utils/metrikker/browser-metrikker';
-import {IdentParam} from '../../model-interfaces';
 
 export function useSetStateFromUrl() {
     const innloggetVeilederIdent = useIdentSelector();
     const dispatch = useDispatch();
-
-    const location = useLocation();
-    const {ident} = useParams<IdentParam>();
-
-    const pathname = location.pathname;
 
     const settInitalStateFraUrl = useCallback(() => {
         const {side, sidestorrelse, sorteringsfelt, sorteringsrekkefolge} = getInitialStateFromUrl();
@@ -26,23 +17,7 @@ export function useSetStateFromUrl() {
         dispatch(settSortering(sorteringsrekkefolge, sorteringsfelt));
     }, [dispatch]);
 
-    function getSideFromPathName(pathName) {
-        switch (pathName) {
-            case `/portefolje/${ident}`:
-                return Side.VEILEDER_PORTEFOLJE_OVERSIKT;
-            case '/portefolje':
-                return Side.MIN_OVERSIKT;
-            case '/enhet':
-                return Side.ENHETENS_OVERSIKT;
-            default:
-                return Side.MIN_OVERSIKT;
-        }
-    }
-    /* eslint-disable react-hooks/exhaustive-deps */
-    const oversiktsside = useCallback(getSideFromPathName, [pathname])(pathname);
-
     useOnMount(() => {
-        loggSideVisning(innloggetVeilederIdent, oversiktsside);
         logBrowserMetrikker(innloggetVeilederIdent);
         settInitalStateFraUrl();
     });
