e2f464662fe29a403fd8b50ceef051302133c4fe
Ingrid Fosså <ingrid.fossa@nav.no>
2025-11-17 16:34:47 +0100

    Vis permittering-data når vi berre filtrerer på dagpenger med permittering også når vi filtrerer på begge typane dette gjeld samstundes

--- src/utils/dagpengerArenaKolonneUtils.ts
+++ src/utils/dagpengerArenaKolonneUtils.ts
@@ -1,27 +1,18 @@
 import {DagpengerFilterArena} from '../filtrering/filter-konstanter';
+import {inneholderBareElementerViSerEtter, inneholderMinstEtAvElementeneViSerEtter} from './listesammenligning';
 
-/** Filtrert på dagpengeytelse med permittering i filteret "ytelseDagpengerArena"
- * (men det ikkje er filtrert på alle dagpengeytelser) */
+/** Filtrert på kun dagpengeytelsar med permittering i filteret "ytelseDagpengerArena" */
 export const filtrertPaDagpengerArenaFilterMedPermittering = (ytelseDagpengerArena: string[]) => {
-    const filtrertPaBareEtPermitteringsfilter =
-        ytelseDagpengerArena.length === 1 &&
-        (ytelseDagpengerArena[0] === DagpengerFilterArena.HAR_DAGPENGER_MED_PERMITTERING_ARENA ||
-            ytelseDagpengerArena[0] === DagpengerFilterArena.HAR_DAGPENGER_MED_PERMITTERING_FISKEINDUSTRI_ARENA);
-
-    const filtrerPaBeggePermitteringsfilter =
-        ytelseDagpengerArena.length === 2 &&
-        ytelseDagpengerArena[0] === DagpengerFilterArena.HAR_DAGPENGER_MED_PERMITTERING_ARENA &&
-        ytelseDagpengerArena[1] === DagpengerFilterArena.HAR_DAGPENGER_MED_PERMITTERING_FISKEINDUSTRI_ARENA;
-
-    return filtrertPaBareEtPermitteringsfilter || filtrerPaBeggePermitteringsfilter;
+    return inneholderBareElementerViSerEtter(ytelseDagpengerArena, [
+        DagpengerFilterArena.HAR_DAGPENGER_MED_PERMITTERING_ARENA,
+        DagpengerFilterArena.HAR_DAGPENGER_MED_PERMITTERING_FISKEINDUSTRI_ARENA
+    ]);
 };
 
-/** Filtrert på dagpengeytelser utan permittering (eller "alle med dagpenger"), i filteret "ytelseDagpengerArena" */
+/** Filtrert på minst ein dagpengeytelse utan permittering */
 export const filtrertPaDagpengerArenaFilterUtenPermittering = (ytelseDagpengerArena: string[]) => {
-    // TODO heller sjekk på "inneheld eit av desse to filtera" her, og "inneheld berre denne lista av filter" på permittering-greia.
-    return (
-        ytelseDagpengerArena.length > 1 ||
-        ytelseDagpengerArena[0] === DagpengerFilterArena.HAR_DAGPENGER_ORDINAR_ARENA ||
-        ytelseDagpengerArena[0] === DagpengerFilterArena.HAR_DAGPENGER_LONNSGARANTIMIDLER_ARENA
-    );
+    return inneholderMinstEtAvElementeneViSerEtter(ytelseDagpengerArena, [
+        DagpengerFilterArena.HAR_DAGPENGER_ORDINAR_ARENA,
+        DagpengerFilterArena.HAR_DAGPENGER_LONNSGARANTIMIDLER_ARENA
+    ]);
 };
