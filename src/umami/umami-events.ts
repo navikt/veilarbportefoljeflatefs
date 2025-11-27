68ec2ab6dc11fc676fce0133e062d5daf9e9b9b5
Sneha Desai <sneha.desai@nav.no>
2025-11-27 11:05:56 +0100

    La til maskereFodselsnummer funksjonen

--- src/umami/umami-events.ts
+++ src/umami/umami-events.ts
@@ -21,6 +21,20 @@ export type AlertVistEvent = {
     tekst: string;
 };
 
+//Man må maskere fødselsnummer med funksjonen nedenfor hvis payloaden(data som skal sendes til umami) inneholder det
+const maskereFodselsnummer = (data?: Record<string, unknown>) => {
+    const maskertObjekt = JSON.stringify(data).replace(/\d{11}/g, (_, indexOfMatch, fullString) =>
+        fullString.charAt(indexOfMatch - 1) === '"' ? '***********' : '"***********"'
+    );
+
+    try {
+        return JSON.parse(maskertObjekt);
+    } catch (e) {
+        console.error('kunne ikke maskere data korrekt før sending til amplitude');
+    }
+    return {};
+};
+
 export const trackFilterValgEvent = ({sidenavn, filternavn, kategori}: FilterValgEvent) => {
     if (!globalThis.umami) {
         // eslint-disable-next-line no-console
