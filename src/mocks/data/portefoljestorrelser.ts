0515a99aec9347addc076611c2dbd13738879cd6
Ingrid Fosså <ingrid.fossa@nav.no>
2025-12-08 15:13:15 +0100

    Lag betre mock-data for veileder-porteføljestorleikar
    Unngå at alle utanom éin veiledar får porteføljestorleik 0.
    
    Også: typesikre bittelitt.

--- src/mocks/data/portefoljestorrelser.ts
+++ src/mocks/data/portefoljestorrelser.ts
@@ -1,17 +1,9 @@
-import {innloggetVeileder, lagTilfeldigVeilederId} from './veiledere';
+import {FacetResults} from '../../ducks/portefoljestorrelser';
+import {veiledere} from './veiledere';
 import {rnd} from '../utils';
 
-function lagPortefoljeStorrelse() {
+export function lagPortefoljestorrelserForVeiledere(): {facetResults: FacetResults[]} {
     return {
-        value: lagTilfeldigVeilederId(),
-        count: rnd(0, 50)
-    };
-}
-
-export function lagPortefoljeStorrelser() {
-    return {
-        facetResults: new Array(40)
-            .fill(lagPortefoljeStorrelse())
-            .concat(new Array(1).fill({value: innloggetVeileder.ident, count: rnd(0, 50)}))
+        facetResults: veiledere.map(veileder => ({value: veileder.ident, count: rnd(0, 120)}))
     };
 }
