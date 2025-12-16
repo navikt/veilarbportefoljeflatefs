82175c3fb343fb364f9872abf9f189dc0b21203f
Ingrid Fosså <ingrid.fossa@nav.no>
2025-12-16 15:50:09 +0100

    Flytt finn tolkebehov-logikk inn i komponenten som brukar den

--- src/components/tabell/dataCells/TolkebehovData.tsx
+++ src/components/tabell/dataCells/TolkebehovData.tsx
@@ -1,7 +1,8 @@
-import {tolkBehov} from '../../../utils/utils';
 import {Kolonne} from '../../../ducks/ui/listevisning';
 import {TekstDataCellType} from '../dataCellTypes/TekstDataCellType';
 import {DataCellMedInnholdBasertPaFiltervalgProps} from './DataCellProps';
+import {FiltervalgModell} from '../../../typer/filtervalg-modell';
+import {BrukerModell} from '../../../typer/bruker-modell';
 
 export const TolkebehovData = ({bruker, valgteKolonner, filtervalg}: DataCellMedInnholdBasertPaFiltervalgProps) => (
     <TekstDataCellType
@@ -10,3 +11,31 @@ export const TolkebehovData = ({bruker, valgteKolonner, filtervalg}: DataCellMed
         className="col col-xs-2"
     />
 );
+
+function tolkBehov(filtervalg: FiltervalgModell, bruker: BrukerModell) {
+    const behov: string[] = [];
+
+    if (
+        (filtervalg.tolkebehov.includes('TALESPRAAKTOLK') && bruker.tolkebehov.talespraaktolk.length > 0) ||
+        filtervalg.tolkBehovSpraak.includes(bruker.tolkebehov.talespraaktolk)
+    ) {
+        behov.push('Talespråktolk');
+    }
+
+    if (
+        (filtervalg.tolkebehov.includes('TEGNSPRAAKTOLK') && bruker.tolkebehov.tegnspraaktolk.length > 0) ||
+        filtervalg.tolkBehovSpraak.includes(bruker.tolkebehov.tegnspraaktolk)
+    ) {
+        if (behov.length > 0) {
+            behov.push('tegnspråktolk');
+        } else {
+            behov.push('Tegnspråktolk');
+        }
+    }
+
+    if (behov.length === 0) {
+        return '-';
+    }
+
+    return behov.join(', ');
+}
