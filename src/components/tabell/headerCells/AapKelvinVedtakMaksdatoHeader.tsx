48ff9444c59d9f79e9d54c4a475f04f1ec8aff3a
Kristina Skatun <kristinaskatun@gmail.com>
2026-05-22 10:39:50 +0200

    Legg til featuretoggle på kolonnen

--- src/components/tabell/headerCells/AapKelvinVedtakMaksdatoHeader.tsx
+++ src/components/tabell/headerCells/AapKelvinVedtakMaksdatoHeader.tsx
@@ -2,21 +2,27 @@ import {HeaderCellProps} from './HeaderCellProps';
 import {SorteringHeader} from '../sortering-header';
 import {Kolonne} from '../../../ducks/ui/listevisning';
 import {Sorteringsfelt} from '../../../typer/kolonnesortering';
+import {useFeatureSelector} from '../../../hooks/redux/use-feature-selector';
+import {VIS_AAP_MAKSDATO} from '../../../konstanter';
 
 export const AapKelvinVedtakMaksdatoHeader = ({
     gjeldendeSorteringsfelt,
     valgteKolonner,
     rekkefolge,
     onClick
-}: HeaderCellProps) => (
-    <SorteringHeader
-        skalVises={valgteKolonner.includes(Kolonne.AAP_KELVIN_MAKSDATO)}
-        sortering={Sorteringsfelt.AAP_KELVIN_MAKSDATO}
-        erValgt={gjeldendeSorteringsfelt === Sorteringsfelt.AAP_KELVIN_MAKSDATO}
-        rekkefolge={rekkefolge}
-        onClick={onClick}
-        tekst="Maksdato AAP (Kelvin)"
-        title="Maksdato er siste dag med mulig rett til AAP"
-        className="col col-xs-2"
-    />
-);
+}: HeaderCellProps) => {
+    const harFeature = useFeatureSelector();
+    const visAapMaksdato = harFeature(VIS_AAP_MAKSDATO);
+    return (
+        <SorteringHeader
+            skalVises={valgteKolonner.includes(Kolonne.AAP_KELVIN_MAKSDATO) && visAapMaksdato}
+            sortering={Sorteringsfelt.AAP_KELVIN_MAKSDATO}
+            erValgt={gjeldendeSorteringsfelt === Sorteringsfelt.AAP_KELVIN_MAKSDATO}
+            rekkefolge={rekkefolge}
+            onClick={onClick}
+            tekst="Maksdato AAP (Kelvin)"
+            title="Maksdato er siste dag med mulig rett til AAP"
+            className="col col-xs-2"
+        />
+    );
+};
