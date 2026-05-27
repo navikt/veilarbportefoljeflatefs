e59ecb5743ac32248702f33099853fbf8fe97107
Kristina Skatun <36476370+kcs85@users.noreply.github.com>
2026-05-27 10:32:44 +0200

    Legg til maksdato for AAP i ny kolonne (#1459)

---
+++ src/components/tabell/headerCells/AapKelvinVedtakMaksdatoHeader.tsx
@@ -0,0 +1,22 @@
+import {HeaderCellProps} from './HeaderCellProps';
+import {SorteringHeader} from '../sortering-header';
+import {Kolonne} from '../../../ducks/ui/listevisning';
+import {Sorteringsfelt} from '../../../typer/kolonnesortering';
+
+export const AapKelvinVedtakMaksdatoHeader = ({
+    gjeldendeSorteringsfelt,
+    valgteKolonner,
+    rekkefolge,
+    onClick
+}: HeaderCellProps) => (
+    <SorteringHeader
+        skalVises={valgteKolonner.includes(Kolonne.AAP_KELVIN_MAKSDATO)}
+        sortering={Sorteringsfelt.AAP_KELVIN_MAKSDATO}
+        erValgt={gjeldendeSorteringsfelt === Sorteringsfelt.AAP_KELVIN_MAKSDATO}
+        rekkefolge={rekkefolge}
+        onClick={onClick}
+        tekst="Maksdato AAP (Kelvin)"
+        title="Maksdato er siste dag med mulig rett til AAP"
+        className="col col-xs-2"
+    />
+);
