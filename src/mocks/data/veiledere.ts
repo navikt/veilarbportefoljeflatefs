2bde3c95ac6d2e3ed20d3ffffcf7cc8dd9c26979
Klara Margrethe Helgemo <klara.margrethe.helgemo@nav.no>
2026-06-30 09:44:58 +0200

    Endringer på grunn av endringer i Faker fra versjon 8 til 10

--- src/mocks/data/veiledere.ts
+++ src/mocks/data/veiledere.ts
@@ -2,6 +2,7 @@ import {fakerNB_NO as faker} from '@faker-js/faker';
 import {rnd} from '../utils';
 import {MOCK_CONFIG} from '../constants';
 import {VeilederePaEnhetModell, VeilederModell} from '../../typer/enhet-og-veiledere-modeller';
+import {FASTE_VEILEDERE} from './faste-veiledere';
 
 faker.seed(MOCK_CONFIG.seed);
 
@@ -24,33 +25,14 @@ function lagVeileder(): VeilederModell {
 }
 
 function lagVeiledere(): VeilederModell[] {
-    const veiledere = new Array(37).fill(0).map(() => lagVeileder());
-    veiledere.push({
+    const tilfeldige = new Array(37).fill(0).map(() => lagVeileder());
+    const faste = FASTE_VEILEDERE.map(({fornavn, etternavn}) => ({
         ident: lagTilfeldigVeilederId(),
-        navn: 'Testesen, Testias',
-        fornavn: 'Testias',
-        etternavn: 'Testesen'
-    });
-    veiledere.push({
-        ident: lagTilfeldigVeilederId(),
-        navn: 'Aasen, Test',
-        fornavn: 'Test',
-        etternavn: 'Aasen'
-    });
-    veiledere.push({
-        ident: lagTilfeldigVeilederId(),
-        navn: 'Aalerud, Test',
-        fornavn: 'Test',
-        etternavn: 'Aalerud'
-    });
-    veiledere.push({
-        ident: lagTilfeldigVeilederId(),
-        navn: 'Olstad, Test',
-        fornavn: 'Test',
-        etternavn: 'Olstad'
-    });
-
-    return veiledere;
+        navn: `${etternavn}, ${fornavn}`,
+        fornavn,
+        etternavn
+    }));
+    return [...tilfeldige, ...faste];
 }
 
 export const veiledere = lagVeiledere();
