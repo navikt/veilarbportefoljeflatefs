f6aabbf37b1629036d12a153794069859a9c30d4
Kristina Skatun <36476370+kcs85@users.noreply.github.com>
2026-09-02 09:00:30 +0200

    Endre systemmelding til å være infoboks og kun en linje (#1548)

--- src/components/informasjonsmeldinger/systemmeldinger.tsx
+++ src/components/informasjonsmeldinger/systemmeldinger.tsx
@@ -1,19 +1,19 @@
-import {Alert, Heading} from '@navikt/ds-react';
+import {InfoCard} from '@navikt/ds-react';
 import {useSystemmeldingerSelector} from '../../hooks/redux/use-systemmeldinger';
 import '../modal/feilmelding-brukere.css';
-import {PortableText} from '@portabletext/react';
+import {toPlainText} from '@portabletext/react';
+import {ExclamationmarkTriangleIcon} from '@navikt/aksel-icons';
 
 export const Systemmeldinger = () => {
     const systemmeldinger = useSystemmeldingerSelector();
     return (
         <>
-            {systemmeldinger.map(systemmelding => (
-                <Alert key={`tittel_${systemmelding.tittel}`} variant={systemmelding.type} size="medium" fullWidth>
-                    <Heading spacing size="small" level="3">
-                        {systemmelding.tittel}
-                    </Heading>
-                    <PortableText value={systemmelding.beskrivelse} />
-                </Alert>
+            {systemmeldinger.map((systemmelding, index) => (
+                <InfoCard key={`infocard_${index}`} data-color="warning" size={'small'}>
+                    <InfoCard.Message icon={<ExclamationmarkTriangleIcon aria-hidden />}>
+                        {toPlainText(systemmelding.beskrivelse)}
+                    </InfoCard.Message>
+                </InfoCard>
             ))}
         </>
     );
