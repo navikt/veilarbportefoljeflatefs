392b8557684929a84a916123e72335534976d1d5
Kristina Skatun <kristinaskatun@gmail.com>
2026-09-21 14:09:11 +0200

    Fiks sortering

--- src/components/sidebar/mine-filter-tab.tsx
+++ src/components/sidebar/mine-filter-tab.tsx
@@ -12,17 +12,9 @@ import {Tiltak} from '../../ducks/enhettiltak';
 import {SidebarTabs} from '../../store/sidebar/sidebar-view-store';
 import {trackKnappKlikketEvent} from '../../umami/umami-events';
 
-function sortMineFilter(a: LagretFilter, b: LagretFilter) {
-    if (a.sortOrder !== null) {
-        if (b.sortOrder !== null) {
-            return a.sortOrder - b.sortOrder;
-        }
-        return -1;
-    }
-    if (b.sortOrder !== null) {
-        return 1;
-    }
-    return a.filterNavn.toLowerCase().localeCompare(b.filterNavn.toLowerCase(), undefined, {numeric: true});
+function sortMineFilter(a, b) {
+    if (a.sortOrder !== b.sortOrder) return a.sortOrder - b.sortOrder;
+    return b.filterId - a.filterId;
 }
 
 interface Props {
